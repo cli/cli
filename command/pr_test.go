401671e1e09ef936670973600ee66d26b1618a5e
Corey Johnson <probablycorey@gmail.com>
2019-10-11 10:07:40 -0700

    Clean up tests

--- command/pr_test.go
+++ command/pr_test.go
@@ -4,13 +4,13 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
-	"strings"
+	"regexp"
 	"testing"
 
 	"github.com/github/gh-cli/api"
 )
 
-func TestPR(t *testing.T) {
+func TestPRList(t *testing.T) {
 	teardown := mockGraphQL("pr.json")
 	defer teardown()
 
@@ -21,8 +21,17 @@ func TestPR(t *testing.T) {
 		}
 	})
 
-	if !strings.Contains(output, "#9 Pull in the GraphQL API functionality [graphql]") {
-		t.Errorf("expected to list PR #8 but did not")
+	expectedPrs := []*regexp.Regexp{
+		regexp.MustCompile(`#8.*\[strawberries\]`),
+		regexp.MustCompile(`#9.*\[apples\]`),
+		regexp.MustCompile(`#10.*\[blueberries\]`),
+		regexp.MustCompile(`#11.*\[figs\]`),
+	}
+
+	for _, r := range expectedPrs {
+		if !r.MatchString(output) {
+			t.Errorf("output did not match regexp /%s/", r)
+		}
 	}
 }
 
