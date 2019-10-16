8b74c3f576d844c87a2c30623429596da217f492
Corey Johnson <probablycorey@gmail.com>
2019-10-16 14:47:47 -0700

    Add PRView tests

--- command/pr_test.go
+++ command/pr_test.go
@@ -4,10 +4,12 @@ import (
 	"regexp"
 	"testing"
 
+	"github.com/github/gh-cli/utils"
+
 	"github.com/github/gh-cli/test"
 )
 
-func TestPRList(t *testing.T) {
+func FTestPRList(t *testing.T) {
 	teardown := test.MockGraphQLResponse("test/fixtures/pr.json")
 	defer teardown()
 
@@ -30,5 +32,32 @@ func TestPRList(t *testing.T) {
 		if !r.MatchString(output) {
 			t.Errorf("output did not match regexp /%s/", r)
 		}
+	}
+}
+
+func TestPRView(t *testing.T) {
+	teardown := test.MockGraphQLResponse("test/fixtures/prView.json")
+	defer teardown()
+
+	gitRepo := test.UseTempGitRepo()
+	defer gitRepo.TearDown()
+
+	openInBrowserCalls := 0
+	utils.OpenInBrowser = func(_ string) error {
+		openInBrowserCalls++
+		return nil
+	}
+
+	output, err := test.RunCommand(RootCmd, "pr view")
+	if err != nil {
+		t.Errorf("error running command `pr view`: %v", err)
+	}
+
+	if output == "" {
+		t.Errorf("command output expected got an empty string")
+	}
+
+	if openInBrowserCalls != 1 {
+		t.Errorf("OpenInBrowser should be called 1 time but was called %d time(s)", openInBrowserCalls)
 	}
 }
