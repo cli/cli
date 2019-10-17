74c637fec8ab39e5ed992cebd6d223fe85ee4be9
Mislav Marohnić <mislav@github.com>
2019-10-17 14:34:55 +0200

    Improve config file parsing

--- context/config_file_test.go
+++ context/config_file_test.go
@@ -1,24 +1,54 @@
 package context
 
 import (
+	"errors"
+	"reflect"
 	"strings"
 	"testing"
 )
+
+func eq(t *testing.T, got interface{}, expected interface{}) {
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("expected: %v, got: %v", expected, got)
+	}
+}
 
 func Test_parseConfig(t *testing.T) {
 	c := strings.NewReader(`---
 github.com:
 - user: monalisa
   oauth_token: OTOKEN
+  protocol: https
+- user: wronguser
+  oauth_token: NOTTHIS
 `)
 	entry, err := parseConfig(c)
-	if err != nil {
-		t.Error(err)
-	}
-	if entry.User != "monalisa" {
-		t.Errorf("got User: %q", entry.User)
-	}
-	if entry.Token != "OTOKEN" {
-		t.Errorf("got User: %q", entry.Token)
-	}
+	eq(t, err, nil)
+	eq(t, entry.User, "monalisa")
+	eq(t, entry.Token, "OTOKEN")
+}
+
+func Test_parseConfig_multipleHosts(t *testing.T) {
+	c := strings.NewReader(`---
+example.com:
+- user: wronguser
+  oauth_token: NOTTHIS
+github.com:
+- user: monalisa
+  oauth_token: OTOKEN
+`)
+	entry, err := parseConfig(c)
+	eq(t, err, nil)
+	eq(t, entry.User, "monalisa")
+	eq(t, entry.Token, "OTOKEN")
+}
+
+func Test_parseConfig_notFound(t *testing.T) {
+	c := strings.NewReader(`---
+example.com:
+- user: wronguser
+  oauth_token: NOTTHIS
+`)
+	_, err := parseConfig(c)
+	eq(t, err, errors.New(`could not find config entry for "github.com"`))
 }
