d334d56f09ae4e6e4f5fa9c3358ecff390f09b6f
Mislav Marohnić <mislav@github.com>
2019-10-17 16:10:45 +0200

    Test FindByName

--- context/remote_test.go
+++ context/remote_test.go
@@ -57,3 +57,22 @@ func Test_parseRemotes(t *testing.T) {
 	eq(t, r[1], &Remote{Name: "origin", Owner: "monalisa", Repo: "octo-cat"})
 	eq(t, r[2], &Remote{Name: "upstream", Owner: "hubot", Repo: "tools"})
 }
+
+func Test_Remotes_FindByName(t *testing.T) {
+	list := Remotes{
+		&Remote{Name: "mona", Owner: "monalisa", Repo: "myfork"},
+		&Remote{Name: "origin", Owner: "monalisa", Repo: "octo-cat"},
+		&Remote{Name: "upstream", Owner: "hubot", Repo: "tools"},
+	}
+
+	r, err := list.FindByName("upstream", "origin")
+	eq(t, err, nil)
+	eq(t, r.Name, "upstream")
+
+	r, err = list.FindByName("nonexist", "*")
+	eq(t, err, nil)
+	eq(t, r.Name, "mona")
+
+	_, err = list.FindByName("nonexist")
+	eq(t, err, errors.New(`no GitHub remotes found`))
+}
