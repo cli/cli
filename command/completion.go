4f03370aa52fba72693002e88d9d732f655b0a8d
Mislav Marohnić <mislav@github.com>
2019-10-31 23:43:05 +0100

    Add tests

--- command/completion.go
+++ command/completion.go
@@ -2,16 +2,13 @@ package command
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
 
-var shellType string
-
 func init() {
 	RootCmd.AddCommand(completionCmd)
-	completionCmd.Flags().StringVarP(&shellType, "shell", "s", "bash", "The type of shell")
+	completionCmd.Flags().StringP("shell", "s", "bash", "The type of shell")
 }
 
 var completionCmd = &cobra.Command{
@@ -28,11 +25,16 @@ start a new shell.
 When installing with Homebrew, see https://docs.brew.sh/Shell-Completion
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		shellType, err := cmd.Flags().GetString("shell")
+		if err != nil {
+			return err
+		}
+
 		switch shellType {
 		case "bash":
-			RootCmd.GenBashCompletion(os.Stdout)
+			RootCmd.GenBashCompletion(cmd.OutOrStdout())
 		case "zsh":
-			RootCmd.GenZshCompletion(os.Stdout)
+			RootCmd.GenZshCompletion(cmd.OutOrStdout())
 		default:
 			return fmt.Errorf("unsupported shell type: %s", shellType)
 		}
