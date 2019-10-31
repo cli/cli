cf1feb847e65c17dacf99bc5b0340e88a97c59b4
Corey Johnson <probablycorey@gmail.com>
2019-10-31 11:02:27 -0700

    Add `gh issue list` and `gh issue view ISSUE_NUMBER`

--- command/pr_test.go
+++ command/pr_test.go
@@ -5,28 +5,8 @@ import (
 	"regexp"
 	"testing"
 
-	"github.com/github/gh-cli/api"
-	"github.com/github/gh-cli/context"
 	"github.com/github/gh-cli/test"
-	"github.com/github/gh-cli/utils"
 )
-
-func initBlankContext(repo, branch string) {
-	initContext = func() context.Context {
-		ctx := context.NewBlank()
-		ctx.SetBaseRepo(repo)
-		ctx.SetBranch(branch)
-		return ctx
-	}
-}
-
-func initFakeHTTP() *api.FakeHTTP {
-	http := &api.FakeHTTP{}
-	apiClientForContext = func(context.Context) (*api.Client, error) {
-		return api.NewClient(api.ReplaceTripper(http)), nil
-	}
-	return http
-}
 
 func TestPRList(t *testing.T) {
 	initBlankContext("OWNER/REPO", "master")
@@ -113,19 +93,4 @@ func TestPRView_NoActiveBranch(t *testing.T) {
 	if *callCount != 1 {
 		t.Errorf("OpenInBrowser should be called once but was called %d time(s)", *callCount)
 	}
-}
-
-func mockOpenInBrowser() (func(), *int) {
-	callCount := 0
-	originalOpenInBrowser := utils.OpenInBrowser
-	teardown := func() {
-		utils.OpenInBrowser = originalOpenInBrowser
-	}
-
-	utils.OpenInBrowser = func(_ string) error {
-		callCount++
-		return nil
-	}
-
-	return teardown, &callCount
 }
