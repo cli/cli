e5c649f9a6719d8da54e746c6ec2762f94bea6d9
Mislav Marohnić <mislav.marohnic@gmail.com>
2019-11-08 15:20:24 +0100

    Add `issue create` test

--- api/queries_issue.go
+++ api/queries_issue.go
@@ -1,9 +1,7 @@
 package api
 
-import "fmt"
-
 func IssueCreate(client *Client, ghRepo Repo, params map[string]interface{}) (*Issue, error) {
-	repoId, err := GitHubRepoId(client, ghRepo)
+	repoID, err := GitHubRepoId(client, ghRepo)
 	if err != nil {
 		return nil, err
 	}
@@ -18,7 +16,7 @@ func IssueCreate(client *Client, ghRepo Repo, params map[string]interface{}) (*I
 	}`
 
 	inputParams := map[string]interface{}{
-		"repositoryId": repoId,
+		"repositoryId": repoID,
 	}
 	for key, val := range params {
 		inputParams[key] = val
@@ -39,32 +37,4 @@ func IssueCreate(client *Client, ghRepo Repo, params map[string]interface{}) (*I
 	}
 
 	return &result.CreateIssue.Issue, nil
-}
-
-func GitHubRepoId(client *Client, ghRepo Repo) (string, error) {
-	owner := ghRepo.RepoOwner()
-	repo := ghRepo.RepoName()
-
-	query := `
-		query FindRepoID($owner:String!, $name:String!) {
-			repository(owner:$owner, name:$name) {
-				id
-			}
-	}`
-	variables := map[string]interface{}{
-		"owner": owner,
-		"name":  repo,
-	}
-
-	result := struct {
-		Repository struct {
-			Id string
-		}
-	}{}
-	err := client.GraphQL(query, variables, &result)
-	if err != nil || result.Repository.Id == "" {
-		return "", fmt.Errorf("failed to determine GH repo ID: %s", err)
-	}
-
-	return result.Repository.Id, nil
 }
