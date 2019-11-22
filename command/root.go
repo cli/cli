977d47b263180e3f7bedd2270652959eef41d2e9
Amanda Pinsker <ampinsk@github.com>
2019-11-22 11:07:59 -0800

    Shorten top summary

--- command/root.go
+++ command/root.go
@@ -37,11 +37,7 @@ type FlagError struct {
 var RootCmd = &cobra.Command{
 	Use:   "gh",
 	Short: "GitHub CLI",
-	Long: `Work seamlessly with GitHub from your command-line, starting
-	with issues and pull requests.
-	
-	(Note: gh is in a very alpha phase.)`,
-
+	Long: `Work seamlessly with GitHub from the command line`,
 	SilenceErrors: true,
 	SilenceUsage:  true,
 }
