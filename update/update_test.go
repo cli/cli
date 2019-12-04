a373158bf6f1bf7f1817d8863d940f039c630f4e
Mislav Marohnić <mislav.marohnic@gmail.com>
2019-12-04 14:34:31 +0100

    Improvements to update notifier
    - Only report an update available if the version number of the release
      is greater than the current version
    
    - Removes `command` dependency from `update` package; instead, pass
      current version as an argument
    
    - Remove `brew upgrade` instructions since we can't be certain that gh
      was installed via Homebrew in the first place.
    
    - Does not check for updates unless stderr is a tty
    
    - Preserve stderr color output even if stdout is not a tty
    
    - Fixes stderr color output on Windows

--- update/update_test.go
+++ update/update_test.go
@@ -1,34 +1,89 @@
 package update
 
 import (
-	"os"
-	"strings"
+	"bytes"
+	"fmt"
 	"testing"
 
 	"github.com/github/gh-cli/api"
-	"github.com/github/gh-cli/command"
 )
 
-func TestRunWhileCheckingForUpdate(t *testing.T) {
-	originalVersion := command.Version
-	command.Version = "v0.0.0"
-	defer func() {
-		command.Version = originalVersion
-	}()
-
-	http := &api.FakeHTTP{}
-	jsonFile, _ := os.Open("../test/fixtures/latestRelease.json")
-	defer jsonFile.Close()
-	http.StubResponse(200, jsonFile)
-
-	client := api.NewClient(api.ReplaceTripper(http))
-	alertMsg := *UpdateMessage(client)
-
-	if !strings.Contains(alertMsg, command.Version) {
-		t.Errorf("expected: \"%v\" to contain \"%v\"", alertMsg, command.Version)
+func TestCheckForUpdate(t *testing.T) {
+	scenarios := []struct {
+		Name           string
+		CurrentVersion string
+		LatestVersion  string
+		LatestURL      string
+		ExpectsResult  bool
+	}{
+		{
+			Name:           "latest is newer",
+			CurrentVersion: "v0.0.1",
+			LatestVersion:  "v1.0.0",
+			LatestURL:      "https://www.spacejam.com/archive/spacejam/movie/jam.htm",
+			ExpectsResult:  true,
+		},
+		{
+			Name:           "current is prerelease",
+			CurrentVersion: "v1.0.0-pre.1",
+			LatestVersion:  "v1.0.0",
+			LatestURL:      "https://www.spacejam.com/archive/spacejam/movie/jam.htm",
+			ExpectsResult:  true,
+		},
+		{
+			Name:           "latest is current",
+			CurrentVersion: "v1.0.0",
+			LatestVersion:  "v1.0.0",
+			LatestURL:      "https://www.spacejam.com/archive/spacejam/movie/jam.htm",
+			ExpectsResult:  false,
+		},
+		{
+			Name:           "latest is older",
+			CurrentVersion: "v0.10.0-pre.1",
+			LatestVersion:  "v0.9.0",
+			LatestURL:      "https://www.spacejam.com/archive/spacejam/movie/jam.htm",
+			ExpectsResult:  false,
+		},
 	}
 
-	if !strings.Contains(alertMsg, "v1.0.0") {
-		t.Errorf("expected: \"%v\" to contain \"%v\"", alertMsg, "v1.0.0")
+	for _, s := range scenarios {
+		t.Run(s.Name, func(t *testing.T) {
+			http := &api.FakeHTTP{}
+			client := api.NewClient(api.ReplaceTripper(http))
+			http.StubResponse(200, bytes.NewBufferString(fmt.Sprintf(`{
+				"tag_name": "%s",
+				"html_url": "%s"
+			}`, s.LatestVersion, s.LatestURL)))
+
+			rel, err := CheckForUpdate(client, "OWNER/REPO", s.CurrentVersion)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if len(http.Requests) != 1 {
+				t.Fatalf("expected 1 HTTP request, got %d", len(http.Requests))
+			}
+			requestPath := http.Requests[0].URL.Path
+			if requestPath != "/repos/OWNER/REPO/releases/latest" {
+				t.Errorf("HTTP path: %q", requestPath)
+			}
+
+			if !s.ExpectsResult {
+				if rel != nil {
+					t.Fatal("expected no new release")
+				}
+				return
+			}
+			if rel == nil {
+				t.Fatal("expected to report new release")
+			}
+
+			if rel.Version != s.LatestVersion {
+				t.Errorf("Version: %q", rel.Version)
+			}
+			if rel.URL != s.LatestURL {
+				t.Errorf("URL: %q", rel.URL)
+			}
+		})
 	}
 }
