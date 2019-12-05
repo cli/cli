b19d969192777e2143f40b615fad4f6ec2fb35d7
Mislav Marohnić <mislav.marohnic@gmail.com>
2019-12-05 14:01:44 +0100

    TMP go-msi's build.bat

---
+++ wix-templates/build.bat
@@ -0,0 +1,2 @@
+candle product.wxs WixUI_HK.wxs LicenseAgreementDlg_HK.wxs
+light -ext WixUIExtension -ext WixUtilExtension -sacl -spdb  -out ../../../p/go/src/github.com/mh-cbon/go-msi/gh.msi product.wixobj WixUI_HK.wixobj LicenseAgreementDlg_HK.wixobj
