756610301eed01cb73d86f8376df01717e5de56a
Tiernan L <tierninho@github.com>
2020-01-10 07:53:16 -1000

    Swap to show

--- command/root.go
+++ command/root.go
@@ -25,7 +25,7 @@ func init() {
 
 	RootCmd.PersistentFlags().StringP("repo", "R", "", "Select another repository using the `OWNER/REPO` format")
 	RootCmd.PersistentFlags().Bool("help", false, "Show help for command")
-	RootCmd.Flags().Bool("version", false, "View gh version")
+	RootCmd.Flags().Bool("version", false, "Show gh version")
 	// TODO:
 	// RootCmd.PersistentFlags().BoolP("verbose", "V", false, "enable verbose output")
 
