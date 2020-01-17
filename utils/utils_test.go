a29715363719b785f22ef06fcd9e28052032ce0b
vilmibm <vilmibm@neongrid.space>
2020-01-17 15:56:52 -0600

    test for utils.FuzzyAgo

---
+++ utils/utils_test.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"testing"
+	"time"
+)
+
+// TODO test FuzzyAgo
+
+func TestFuzzyAgo(t *testing.T) {
+
+	cases := map[string]string{
+		"1s":       "less than a minute ago",
+		"30s":      "less than a minute ago",
+		"1m08s":    "about 1 minute ago",
+		"15m0s":    "about 15 minutes ago",
+		"59m10s":   "about 59 minutes ago",
+		"1h10m02s": "about 1 hour ago",
+		"15h0m01s": "about 15 hours ago",
+		"30h10m":   "about 1 day ago",
+		"50h":      "about 2 days ago",
+	}
+
+	for duration, expected := range cases {
+		d, e := time.ParseDuration(duration)
+		if e != nil {
+			t.Errorf("failed to create a duration: %s", e)
+		}
+
+		fuzzy := FuzzyAgo(d)
+		if fuzzy != expected {
+			t.Errorf("unexpected fuzzy duration value: %s for %s", fuzzy, duration)
+		}
+	}
+}
