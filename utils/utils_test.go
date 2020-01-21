8de1eb693def373bc0a94a711c79353fcf8ed4ad
vilmibm <vilmibm@neongrid.space>
2020-01-21 14:25:38 -0600

    support more time units

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -8,15 +8,20 @@ import (
 func TestFuzzyAgo(t *testing.T) {
 
 	cases := map[string]string{
-		"1s":       "less than a minute ago",
-		"30s":      "less than a minute ago",
-		"1m08s":    "about 1 minute ago",
-		"15m0s":    "about 15 minutes ago",
-		"59m10s":   "about 59 minutes ago",
-		"1h10m02s": "about 1 hour ago",
-		"15h0m01s": "about 15 hours ago",
-		"30h10m":   "about 1 day ago",
-		"50h":      "about 2 days ago",
+		"1s":         "less than a minute ago",
+		"30s":        "less than a minute ago",
+		"1m08s":      "about 1 minute ago",
+		"15m0s":      "about 15 minutes ago",
+		"59m10s":     "about 59 minutes ago",
+		"1h10m02s":   "about 1 hour ago",
+		"15h0m01s":   "about 15 hours ago",
+		"30h10m":     "about 1 day ago",
+		"50h":        "about 2 days ago",
+		"720h05m":    "about 1 month ago",
+		"3000h10m":   "about 4 months ago",
+		"8760h59m":   "about 1 year ago",
+		"17601h59m":  "about 2 years ago",
+		"262800h19m": "about 30 years ago",
 	}
 
 	for duration, expected := range cases {
