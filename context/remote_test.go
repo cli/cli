8453bf679c5d0b3b0d0d626f7060e6d2ba385101
Mislav Marohnić <mislav.marohnic@gmail.com>
2020-01-28 19:51:44 +0100

    Fix parsing non-GitHub remotes
    Otherwise, the remote URL translation mechanism had crashed when
    encountering a non-Github.com git remote.

--- context/remote_test.go
+++ context/remote_test.go
@@ -2,6 +2,7 @@ package context
 
 import (
 	"errors"
+	"net/url"
 	"testing"
 
 	"github.com/cli/cli/git"
@@ -24,4 +25,35 @@ func Test_Remotes_FindByName(t *testing.T) {
 
 	_, err = list.FindByName("nonexist")
 	eq(t, err, errors.New(`no GitHub remotes found`))
+}
+
+func Test_translateRemotes(t *testing.T) {
+	publicURL, _ := url.Parse("https://github.com/monalisa/hello")
+	originURL, _ := url.Parse("http://example.com/repo")
+
+	gitRemotes := git.RemoteSet{
+		&git.Remote{
+			Name:     "origin",
+			FetchURL: originURL,
+		},
+		&git.Remote{
+			Name:     "public",
+			FetchURL: publicURL,
+		},
+	}
+
+	identityURL := func(u *url.URL) *url.URL {
+		return u
+	}
+	result := translateRemotes(gitRemotes, identityURL)
+
+	if len(result) != 1 {
+		t.Errorf("got %d results", len(result))
+	}
+	if result[0].Name != "public" {
+		t.Errorf("got %q", result[0].Name)
+	}
+	if result[0].RepoName() != "hello" {
+		t.Errorf("got %q", result[0].RepoName())
+	}
 }
