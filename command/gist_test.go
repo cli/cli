81462d1dfafb0a59f2f32045d249c4c7d7368058
wingkwong <wingkwong.code@gmail.com>
2020-02-25 13:12:01 +0800

    feat: add gist create command test case #397

---
+++ command/gist_test.go
@@ -0,0 +1,37 @@
+package command
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+)
+
+func TestGistCreate(t *testing.T) {
+	http := initFakeHTTP()
+
+	http.StubResponse(200, bytes.NewBufferString(`
+	{
+		"html_url": "https://gist.github.com/aa5a315d61ae9438b18d"
+	}
+	`))
+
+	output, err := RunCommand(gistCreateCmd, `gist create -f "../test/fixtures/gistCreate.json" -d "Gist description" -p=false`)
+	eq(t, err, nil)
+
+	bodyBytes, _ := ioutil.ReadAll(http.Requests[0].Body)
+	reqBody := struct {
+		Description string
+		Public      bool
+		Files       map[string]struct {
+			Content string
+		}
+	}{}
+
+	json.Unmarshal(bodyBytes, &reqBody)
+
+	eq(t, reqBody.Description, "Gist description")
+	eq(t, reqBody.Public, false)
+	eq(t, reqBody.Files["gistCreate.json"].Content, "{}")
+	eq(t, output.String(), "https://gist.github.com/aa5a315d61ae9438b18d\n")
+}
