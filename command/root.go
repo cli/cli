96d96847dcc2145c877a99a3c6219cffefdfdcdc
Kevin Bluer <kevin@bluer.com>
2020-05-15 20:34:04 -0500

    handling incorrect format being provided when determining base repo

--- command/root.go
+++ command/root.go
@@ -219,7 +219,10 @@ func changelogURL(version string) string {
 func determineBaseRepo(cmd *cobra.Command, ctx context.Context) (ghrepo.Interface, error) {
 	repo, err := cmd.Flags().GetString("repo")
 	if err == nil && repo != "" {
-		baseRepo, _ := ghrepo.FromFullName(repo)
+		baseRepo, err := ghrepo.FromFullName(repo)
+		if err != nil {
+			return nil, fmt.Errorf("argument error: %w", err)
+		}
 		return baseRepo, nil
 	}
 
