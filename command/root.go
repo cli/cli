8a4872bab38611904db7a58244c40c92c76cc8e0
Corey Johnson <probablycorey@gmail.com>
2020-06-22 14:07:49 -0400

    Remove global repo flag

--- command/root.go
+++ command/root.go
@@ -52,7 +52,6 @@ func init() {
 	RootCmd.AddCommand(versionCmd)
 	RootCmd.SetVersionTemplate(versionOutput)
 
-	RootCmd.PersistentFlags().StringP("repo", "R", "", "Select another repository using the `OWNER/REPO` format")
 	RootCmd.PersistentFlags().Bool("help", false, "Show help for command")
 	RootCmd.Flags().Bool("version", false, "Show gh version")
 	// TODO:
