2761739c29c7f12fb9a1f21e7f5c5bb8779c212f
Corey Johnson <probablycorey@gmail.com>
2020-06-22 15:19:33 -0400

    Correct package name

--- command/util.go
+++ command/util.go
@@ -1,8 +1,7 @@
-package Command
+package command
 
 import "github.com/spf13/cobra"
-package Command
 
 func includeRepoFlag(cmd *cobra.Command) {
-	cmd.StringP("repo", "R", "", "Select another repository using the `OWNER/REPO` format")
+	cmd.Flags().StringP("repo", "R", "", "Select another repository using the `OWNER/REPO` format")
 }
