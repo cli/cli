6cbcdf2a74e7b75b7741a3a6a733e4d3ce52920b
Ravikanth C <ravikanth@ravichaganti.com>
2020-07-06 19:33:17 +0530

    Clarify how the branch can be retained after `pr merge`

--- command/pr.go
+++ command/pr.go
@@ -107,9 +107,14 @@ var prReopenCmd = &cobra.Command{
 }
 var prMergeCmd = &cobra.Command{
 	Use:   "merge [<number> | <url> | <branch>]",
-	Short: "Merge a pull request",
 	Args:  cobra.MaximumNArgs(1),
-	RunE:  prMerge,
+	Short: "Merge a pull request",
+	Long: `Merge a pull request by providing the pull request number or url or the branch.
+
+By default, the source branch gets deleted after the pull request merge is complete.
+
+If the source branch needs be retained, '--delete-branch=false' should be specified.`,
+	RunE: prMerge,
 }
 var prReadyCmd = &cobra.Command{
 	Use:   "ready [<number> | <url> | <branch>]",
