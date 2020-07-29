9cbcab2872a1bc607e1df1e9eeb9c465801c4dbb
vilmibm <vilmibm@neongrid.space>
2020-07-29 15:45:38 -0500

    support stubbing SurveyAskOne

--- pkg/prompt/stubber.go
+++ pkg/prompt/stubber.go
@@ -9,16 +9,39 @@ import (
 )
 
 type AskStubber struct {
-	Asks  [][]*survey.Question
-	Count int
-	Stubs [][]*QuestionStub
+	Asks     [][]*survey.Question
+	AskOnes  []*survey.Prompt
+	Count    int
+	OneCount int
+	Stubs    [][]*QuestionStub
+	StubOnes []*PromptStub
 }
 
 // TODO ADAPT THIS ALL TO SUPPORT THE NEW SURVEYASKONE
 
 func InitAskStubber() (*AskStubber, func()) {
 	origSurveyAsk := SurveyAsk
+	origSurveyAskOne := SurveyAskOne
 	as := AskStubber{}
+
+	SurveyAskOne = func(p survey.Prompt, response interface{}, opts ...survey.AskOpt) error {
+		as.AskOnes = append(as.AskOnes, &p)
+		count := as.OneCount
+		as.OneCount += 1
+		if count > len(as.StubOnes) {
+			panic(fmt.Sprintf("more asks than stubs. most recent call: %v", p))
+		}
+		stubbedPrompt := as.StubOnes[count]
+		if stubbedPrompt.Default {
+			defaultValue := reflect.ValueOf(p).Elem().FieldByName("Default")
+			_ = core.WriteAnswer(response, "", defaultValue)
+		} else {
+			_ = core.WriteAnswer(response, "", stubbedPrompt.Value)
+		}
+
+		return nil
+	}
+
 	SurveyAsk = func(qs []*survey.Question, response interface{}, opts ...survey.AskOpt) error {
 		as.Asks = append(as.Asks, qs)
 		count := as.Count
@@ -46,14 +69,32 @@ func InitAskStubber() (*AskStubber, func()) {
 	}
 	teardown := func() {
 		SurveyAsk = origSurveyAsk
+		SurveyAskOne = origSurveyAskOne
 	}
 	return &as, teardown
+}
+
+type PromptStub struct {
+	Value   interface{}
+	Default bool
 }
 
 type QuestionStub struct {
 	Name    string
 	Value   interface{}
 	Default bool
+}
+
+func (as *AskStubber) StubOne(value interface{}) {
+	as.StubOnes = append(as.StubOnes, &PromptStub{
+		Value: value,
+	})
+}
+
+func (as *AskStubber) StubOneDefault() {
+	as.StubOnes = append(as.StubOnes, &PromptStub{
+		Default: true,
+	})
 }
 
 func (as *AskStubber) Stub(stubbedQuestions []*QuestionStub) {
