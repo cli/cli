a90b62fb6c497b74826d9961010136dd77f474a7
zamasu <zamasu@gmail.com>
2020-10-11 17:42:11 +0900

    Fix parsing labels in paginated IssueList

--- api/queries_issue_test.go
+++ api/queries_issue_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"reflect"
 	"testing"
 
 	"github.com/cli/cli/internal/ghrepo"
@@ -66,5 +67,86 @@ func TestIssueList(t *testing.T) {
 	_ = json.Unmarshal(bodyBytes, &reqBody)
 	if endCursor := reqBody.Variables["endCursor"].(string); endCursor != "ENDCURSOR" {
 		t.Errorf("expected %q, got %q", "ENDCURSOR", endCursor)
+	}
+}
+
+func TestIssueList_pagination(t *testing.T) {
+	http := &httpmock.Registry{}
+	client := NewClient(ReplaceTripper(http))
+	http.StubResponse(200, bytes.NewBufferString(`
+	{ "data": { "repository": {
+		"hasIssuesEnabled": true,
+		"issues": {
+			"nodes": [
+				{
+					"title": "issue1",
+					"labels": { "nodes": [ { "name": "bug" } ], "totalCount": 1 },
+					"assignees": { "nodes": [ { "login": "user1" } ], "totalCount": 1 }
+				}
+			],
+			"pageInfo": {
+				"hasNextPage": true,
+				"endCursor": "ENDCURSOR"
+			},
+			"totalCount": 2
+		}
+	} } }
+	`))
+	http.StubResponse(200, bytes.NewBufferString(`
+	{ "data": { "repository": {
+		"hasIssuesEnabled": true,
+		"issues": {
+			"nodes": [
+				{
+					"title": "issue2",
+					"labels": { "nodes": [ { "name": "enhancement" } ], "totalCount": 1 },
+					"assignees": { "nodes": [ { "login": "user2" } ], "totalCount": 1 }
+				}
+			],
+			"pageInfo": {
+				"hasNextPage": false,
+				"endCursor": "ENDCURSOR"
+			},
+			"totalCount": 2
+		}
+	} } }
+	`))
+	repo := ghrepo.New("OWNER", "REPO")
+
+	want := &IssuesAndTotalCount{
+		Issues: []Issue{
+			{
+				Title: "issue1",
+				Assignees: struct {
+					Nodes      []struct{ Login string }
+					TotalCount int
+				}{[]struct{ Login string }{{"user1"}}, 1},
+				Labels: struct {
+					Nodes      []struct{ Name string }
+					TotalCount int
+				}{[]struct{ Name string }{{"bug"}}, 1},
+			},
+			{
+				Title: "issue2",
+				Assignees: struct {
+					Nodes      []struct{ Login string }
+					TotalCount int
+				}{[]struct{ Login string }{{"user2"}}, 1},
+				Labels: struct {
+					Nodes      []struct{ Name string }
+					TotalCount int
+				}{[]struct{ Name string }{{"enhancement"}}, 1},
+			},
+		},
+		TotalCount: 2,
+	}
+
+	got, err := IssueList(client, repo, "", nil, "", 0, "", "", "")
+	if err != nil {
+		t.Errorf("IssueList() error = %v", err)
+		return
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("IssueList() = %v, want %v", got, want)
 	}
 }
