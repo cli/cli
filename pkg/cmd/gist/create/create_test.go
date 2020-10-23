a8c340b7258db45316500310cc582dfff0394b77
Christopher Oswald <chrisozzynp@gmail.com>
2020-10-22 19:13:13 -0700

    Support for --web when using gist create
    Proposal to close #2071
    
    I have not worked with Go prior to this so please smite me down with the
    wisdom of a million Golang gods if I'm doing something terribly wrong.
    
    I also added a test to gist/create for the added web arg.
    
    Pretty much referenced the implementation from pr/create.

--- pkg/cmd/gist/create/create_test.go
+++ pkg/cmd/gist/create/create_test.go
@@ -3,6 +3,7 @@ package create
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/cli/cli/test"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -254,6 +255,26 @@ func Test_createRun(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "web arg",
+			opts: &CreateOptions{
+				WebMode:   true,
+				Filenames: []string{fixtureFile},
+			},
+			wantOut:    "Opening gist.github.com/aa5a315d61ae9438b18d in your browser.\n",
+			wantStderr: "- Creating gist fixture.txt\n✓ Created gist fixture.txt\n",
+			wantErr:    false,
+			wantParams: map[string]interface{}{
+				"description": "",
+				"updated_at":  "0001-01-01T00:00:00Z",
+				"public":      false,
+				"files": map[string]interface{}{
+					"fixture.txt": map[string]interface{}{
+						"content": "{}",
+					},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		reg := &httpmock.Registry{}
@@ -270,6 +291,14 @@ func Test_createRun(t *testing.T) {
 		io, stdin, stdout, stderr := iostreams.Test()
 		tt.opts.IO = io
 
+		cs, cmdTeardown := test.InitCmdStubber()
+
+		if tt.name == "web arg" {
+			defer cmdTeardown()
+
+			cs.Stub("")
+		}
+
 		t.Run(tt.name, func(t *testing.T) {
 			stdin.WriteString(tt.stdin)
 
@@ -285,6 +314,11 @@ func Test_createRun(t *testing.T) {
 			assert.Equal(t, tt.wantOut, stdout.String())
 			assert.Equal(t, tt.wantStderr, stderr.String())
 			assert.Equal(t, tt.wantParams, reqBody)
+
+			if tt.name == "web arg" {
+				assert.Equal(t, cs.Calls[0].Args[1], "https://gist.github.com/aa5a315d61ae9438b18d")
+			}
+
 			reg.Verify(t)
 		})
 	}
