20beaa1851ed2c04d7ff6ba5070453019d4b3276
Sam Coe <samcoe@users.noreply.github.com>
2020-12-01 11:44:14 -0500

    Properly check env auth tokens in CheckAuth

--- pkg/cmdutil/auth_check_test.go
+++ pkg/cmdutil/auth_check_test.go
@@ -1,6 +1,7 @@
 package cmdutil
 
 import (
+	"os"
 	"testing"
 
 	"github.com/cli/cli/internal/config"
@@ -8,21 +9,34 @@ import (
 )
 
 func Test_CheckAuth(t *testing.T) {
+	orig_GITHUB_TOKEN := os.Getenv("GITHUB_TOKEN")
+	t.Cleanup(func() {
+		os.Setenv("GITHUB_TOKEN", orig_GITHUB_TOKEN)
+	})
+
 	tests := []struct {
 		name     string
 		cfg      func(config.Config)
+		envToken bool
 		expected bool
 	}{
 		{
 			name:     "no hosts",
 			cfg:      func(c config.Config) {},
+			envToken: false,
 			expected: false,
+		},
+		{name: "no hosts, env auth token",
+			cfg:      func(c config.Config) {},
+			envToken: true,
+			expected: true,
 		},
 		{
 			name: "host, no token",
 			cfg: func(c config.Config) {
 				_ = c.Set("github.com", "oauth_token", "")
 			},
+			envToken: false,
 			expected: false,
 		},
 		{
@@ -30,12 +44,19 @@ func Test_CheckAuth(t *testing.T) {
 			cfg: func(c config.Config) {
 				_ = c.Set("github.com", "oauth_token", "a token")
 			},
+			envToken: false,
 			expected: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.envToken {
+				os.Setenv("GITHUB_TOKEN", "TOKEN")
+			} else {
+				os.Setenv("GITHUB_TOKEN", "")
+			}
+
 			cfg := config.NewBlankConfig()
 			tt.cfg(cfg)
 			result := CheckAuth(cfg)
