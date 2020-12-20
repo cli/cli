511beb4352973805482b28e29d3de11890f694f9
Evan Sosenko <razorx@evansosenko.com>
2020-12-20 11:47:16 -0600

    Add test for remote sort order

--- pkg/cmd/factory/remote_resolver_test.go
+++ pkg/cmd/factory/remote_resolver_test.go
@@ -41,6 +41,39 @@ func Test_remoteResolver(t *testing.T) {
 	assert.Equal(t, "upstream", remotes[1].Name)
 }
 
+func Test_remoteResolverPriority(t *testing.T) {
+	rr := &remoteResolver{
+		readRemotes: func() (git.RemoteSet, error) {
+			return git.RemoteSet{
+				git.NewRemote("origin", "https://github.com/origin-owner/repo.git"),
+				git.NewRemote("github", "https://github.com/gh-owner/gh-repo.git"),
+				git.NewRemote("custom", "https://github.com/foo/bar.git"),
+				git.NewRemote("upstream", "https://github.com/upstream-owner/repo.git"),
+			}, nil
+		},
+		getConfig: func() (config.Config, error) {
+			return config.NewFromString(heredoc.Doc(`
+				hosts:
+				  example.org:
+				    oauth_token: GHETOKEN
+			`)), nil
+		},
+		urlTranslator: func(u *url.URL) *url.URL {
+			return u
+		},
+	}
+
+	resolver := rr.Resolver("")
+	remotes, err := resolver()
+	require.NoError(t, err)
+	require.Equal(t, 4, len(remotes))
+
+	assert.Equal(t, "custom", remotes[0].Name)
+	assert.Equal(t, "origin", remotes[1].Name)
+	assert.Equal(t, "github", remotes[2].Name)
+	assert.Equal(t, "upstream", remotes[3].Name)
+}
+
 func Test_remoteResolverOverride(t *testing.T) {
 	rr := &remoteResolver{
 		readRemotes: func() (git.RemoteSet, error) {
