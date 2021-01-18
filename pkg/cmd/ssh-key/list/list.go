4a2cc8d2a467a5ce57a36de9475bd6325627291e
Mislav Marohnić <mislav@github.com>
2021-01-18 17:23:54 +0100

    Simplify `ssh-key list`
    Do not require nor request `read:public_key` scope by default.

--- pkg/cmd/ssh-key/list/list.go
+++ pkg/cmd/ssh-key/list/list.go
@@ -1,29 +1,21 @@
 package list
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
-	"github.com/cli/cli/internal/ghinstance"
-	"github.com/cli/cli/utils"
-
-	"github.com/MakeNowJust/heredoc"
-	"github.com/cli/cli/api"
-	"github.com/cli/cli/internal/config"
 	"github.com/cli/cli/pkg/cmdutil"
 	"github.com/cli/cli/pkg/iostreams"
+	"github.com/cli/cli/utils"
 	"github.com/spf13/cobra"
 )
 
 // ListOptions struct for list command
 type ListOptions struct {
-	HTTPClient func() (*http.Client, error)
 	IO         *iostreams.IOStreams
-	Config     func() (config.Config, error)
-
-	ListMsg []string
+	HTTPClient func() (*http.Client, error)
 }
 
 // NewCmdList creates a command for list all SSH Keys
@@ -31,23 +23,12 @@ func NewCmdList(f *cmdutil.Factory, runF func(*ListOptions) error) *cobra.Comman
 	opts := &ListOptions{
 		HTTPClient: f.HttpClient,
 		IO:         f.IOStreams,
-		Config:     f.Config,
-
-		ListMsg: []string{},
 	}
 
 	cmd := &cobra.Command{
 		Use:   "list",
+		Short: "Lists SSH keys in a GitHub account",
 		Args:  cobra.ExactArgs(0),
-		Short: "Lists currently added ssh keys",
-		Long: heredoc.Doc(`Lists currently added ssh keys.
-
-			This interactive command lists all SSH keys associated with your account
-		`),
-		Example: heredoc.Doc(`
-			$ gh ssh-key list
-			# => lists all ssh keys associated with your account
-		`),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if runF != nil {
 				return runF(opts)
@@ -60,93 +41,56 @@ func NewCmdList(f *cmdutil.Factory, runF func(*ListOptions) error) *cobra.Comman
 }
 
 func listRun(opts *ListOptions) error {
-	apiClient, err := opts.getAPIClient()
+	apiClient, err := opts.HTTPClient()
 	if err != nil {
-		opts.printTerminal()
 		return err
 	}
 
-	err = opts.hasMinimumScopes(apiClient)
+	sshKeys, err := userKeys(apiClient, "")
 	if err != nil {
-		opts.printTerminal()
-		return err
-	}
-
-	type keys struct {
-		Title string
-		Key   string
-	}
-
-	type result []keys
-
-	rs := result{}
-	body := bytes.NewBufferString("")
-
-	err = apiClient.REST(ghinstance.Default(), "GET", "user/keys", body, &rs)
-	if err != nil {
-		opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("%s: Got %s", utils.RedX(), err))
-		opts.printTerminal()
-		return err
-	}
-
-	for _, r := range rs {
-		opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("%s %s: %s \n    %s: %s", utils.Cyan("✹"), utils.Bold("Name"), r.Title, utils.Bold("SSH-KEY"), r.Key))
-	}
-
-	opts.printTerminal()
-
-	return nil
-}
-
-func (opts *ListOptions) getAPIClient() (*api.Client, error) {
-	httpClient, err := opts.HTTPClient()
-	if err != nil {
-		opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("%s: %s", utils.RedX(), err))
-		return nil, err
-	}
-	return api.NewClientFromHTTP(httpClient), nil
-}
-
-func (opts *ListOptions) hasMinimumScopes(apiClient *api.Client) error {
-	cfg, err := opts.Config()
-	if err != nil {
-		opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("%s: %s", utils.RedX(), err))
-		return err
-	}
-
-	hostname := ghinstance.Default()
-
-	_, tokenSource, _ := cfg.GetWithSource(hostname, "oauth_token")
-
-	// TODO: Implement tests for this case when CheckWriteable function checks filesystem permissions
-	tokenIsWriteable := cfg.CheckWriteable(hostname, "oauth_token") == nil
-
-	err = apiClient.HasMinimumScopes(hostname)
-
-	if err != nil {
-		var missingScopes *api.MissingScopesError
-		if errors.As(err, &missingScopes) {
-			opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("%s: %s", utils.RedX(), err))
-			if tokenIsWriteable {
-				opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("- To request missing scopes, run: %s %s", utils.Bold("gh auth refresh -h"), hostname))
-			}
-		} else {
-			opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("%s: authentication failed", utils.RedX()))
-			opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("- The %s token in %s is no longer valid.", utils.Bold(hostname), utils.Bold(tokenSource)))
-			if tokenIsWriteable {
-				opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("- To re-authenticate, run: %s %s", utils.Bold("gh auth login -h"), utils.Bold(hostname)))
-				opts.ListMsg = append(opts.ListMsg, fmt.Sprintf("- To forget about this host, run: %s %s", utils.Bold("gh auth logout -h"), utils.Bold(hostname)))
-			}
+		if errors.Is(err, scopesError) {
+			cs := opts.IO.ColorScheme()
+			fmt.Fprint(opts.IO.ErrOut, "Error: insufficient OAuth scopes to list SSH keys\n")
+			fmt.Fprintf(opts.IO.ErrOut, "Run the following to grant scopes: %s\n", cs.Bold("gh auth refresh -s read:public_key"))
+			return cmdutil.SilentError
 		}
 		return err
 	}
 
-	return nil
+	if len(sshKeys) == 0 {
+		fmt.Fprintln(opts.IO.ErrOut, "No SSH keys present in GitHub account.")
+		return cmdutil.SilentError
+	}
+
+	t := utils.NewTablePrinter(opts.IO)
+	cs := opts.IO.ColorScheme()
+	now := time.Now()
+
+	for _, sshKey := range sshKeys {
+		t.AddField(sshKey.Title, nil, nil)
+		t.AddField(sshKey.Key, truncateMiddle, nil)
+
+		createdAt := sshKey.CreatedAt.Format(time.RFC3339)
+		if t.IsTTY() {
+			createdAt = utils.FuzzyAgoAbbr(now, sshKey.CreatedAt)
+		}
+		t.AddField(createdAt, nil, cs.Gray)
+		t.EndRow()
+	}
+
+	return t.Render()
 }
 
-func (opts *ListOptions) printTerminal() {
-	stderr := opts.IO.ErrOut
-	for _, line := range opts.ListMsg {
-		fmt.Fprintf(stderr, "  %s\n", line)
+func truncateMiddle(maxWidth int, t string) string {
+	if len(t) <= maxWidth {
+		return t
 	}
+
+	ellipsis := "..."
+	if maxWidth < len(ellipsis)+2 {
+		return t[0:maxWidth]
+	}
+
+	halfWidth := (maxWidth - len(ellipsis)) / 2
+	return t[0:halfWidth] + ellipsis + t[len(t)-halfWidth:]
 }
--- pkg/cmd/ssh-key/list/list_test.go
+++ pkg/cmd/ssh-key/list/list_test.go
@@ -1,288 +1,130 @@
 package list
 
 import (
-	"bytes"
-	"errors"
+	"fmt"
 	"net/http"
-	"reflect"
 	"testing"
+	"time"
 
-	"github.com/cli/cli/internal/config"
-	"github.com/cli/cli/pkg/cmdutil"
+	"github.com/MakeNowJust/heredoc"
 	"github.com/cli/cli/pkg/httpmock"
 	"github.com/cli/cli/pkg/iostreams"
-	"github.com/google/shlex"
 )
 
-func TestCmdList(t *testing.T) {
-	io, _, _, _ := iostreams.Test()
-	io.SetStdoutTTY(true)
-	io.SetStdinTTY(true)
-	io.SetStderrTTY(true)
-
-	httpFunc := func() (*http.Client, error) { return nil, nil }
-	configFunc := func() (config.Config, error) { return nil, nil }
-
-	type input struct {
-		cli        string
-		httpClient func() (*http.Client, error)
-		io         *iostreams.IOStreams
-		config     func() (config.Config, error)
-	}
-
-	tests := []struct {
-		name  string
-		input input
-		wants ListOptions
-	}{
-		{
-			name: "no arguments",
-			input: input{
-				cli:        "",
-				httpClient: httpFunc,
-				io:         io,
-				config:     configFunc,
-			},
-			wants: ListOptions{
-				HTTPClient: httpFunc,
-				Config:     configFunc,
-				IO:         io,
-				ListMsg:    []string{},
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			f := &cmdutil.Factory{
-				HttpClient: tt.input.httpClient,
-				Config:     tt.input.config,
-				IOStreams:  tt.input.io,
-			}
-
-			argv, err := shlex.Split(tt.input.cli)
-			if err != nil {
-				t.Errorf(`Split() = got %v`, err)
-			}
-
-			var gotOpts *ListOptions
-			cmd := NewCmdList(f, func(opts *ListOptions) error {
-				gotOpts = opts
-				return nil
-			})
-
-			cmd.SetArgs(argv)
-			cmd.SetIn(&bytes.Buffer{})
-			cmd.SetOut(&bytes.Buffer{})
-			cmd.SetErr(&bytes.Buffer{})
-
-			_, err = cmd.ExecuteC()
-			if err != nil {
-				t.Errorf(`ExecuteC() = got %v`, err)
-			}
-
-			if reflect.ValueOf(tt.wants.HTTPClient).Pointer() != reflect.ValueOf(gotOpts.HTTPClient).Pointer() {
-				t.Errorf(`HTTPClient has wrong values`)
-			}
-
-			if reflect.ValueOf(tt.wants.Config).Pointer() != reflect.ValueOf(gotOpts.Config).Pointer() {
-				t.Errorf(`Config has wrong values`)
-			}
-
-			if reflect.ValueOf(tt.wants.IO).Pointer() != reflect.ValueOf(gotOpts.IO).Pointer() {
-				t.Errorf(`IO has wrong values`)
-			}
-
-			if !reflect.DeepEqual(tt.wants.ListMsg, gotOpts.ListMsg) {
-				t.Errorf(`ListMsg has wrong values: want %v, got %v`, tt.wants.ListMsg, gotOpts.ListMsg)
-			}
-		})
-	}
-}
-
 func TestListRun(t *testing.T) {
-	type input struct {
-		httpStubs       func(*httpmock.Registry)
-		configError     bool
-		httpClientError bool
-		hasOauthToken   bool
-		wantErr         bool
-	}
-
 	tests := []struct {
-		name  string
-		input input
-		want  []string
+		name       string
+		opts       ListOptions
+		isTTY      bool
+		wantStdout string
+		wantStderr string
+		wantErr    bool
 	}{
 		{
-			name: "name and corresponding ssh key",
-			input: input{
-				func(reg *httpmock.Registry) {
-					reg.Register(
-						httpmock.REST("GET", "user/keys"),
-						httpmock.StringResponse(`[{"id":1234,"key":"ssh-rsa AAAABbBB123","title":"Mac"}]`),
-					)
-					reg.Register(
-						httpmock.REST("GET", ""),
-						httpmock.ScopesResponder("repo,read:org,read:public_key"),
-					)
-				},
-				false,
-				false,
-				true,
-				false,
-			},
-			want: []string{"✹ Name: Mac \n    SSH-KEY: ssh-rsa AAAABbBB123"},
-		},
-		{
-			name: "config error",
-			input: input{
-				func(reg *httpmock.Registry) {
-					reg.Register(
-						httpmock.REST("GET", "user/keys"),
-						httpmock.StringResponse(""),
-					)
-					reg.Register(
-						httpmock.REST("GET", ""),
-						httpmock.ScopesResponder("repo,read:org,read:public_key"),
-					)
-				},
-				true,
-				false,
-				true,
-				true,
-			},
-			want: []string{"X: Config error"},
-		},
-		{
-			name: "http client error",
-			input: input{
-				func(reg *httpmock.Registry) {
-					reg.Register(
-						httpmock.REST("GET", "user/keys"),
-						httpmock.StringResponse(""),
-					)
-					reg.Register(
-						httpmock.REST("GET", ""),
-						httpmock.ScopesResponder("repo,read:org,read:public_key"),
-					)
-				},
-				false,
-				true,
-				true,
-				true,
-			},
-			want: []string{"X: HttpClient error"},
-		},
-		{
-			name: "not found on api.github.com/user/keys",
-			input: input{
-				func(reg *httpmock.Registry) {
-					reg.Register(
-						httpmock.REST("GET", "user/keys"),
-						httpmock.StatusStringResponse(http.StatusNotFound, `{"message": "Not Found", "documentation_url": "url"}`),
-					)
-					reg.Register(
-						httpmock.REST("GET", ""),
-						httpmock.ScopesResponder("repo,read:org,read:public_key"),
-					)
-				},
-				false,
-				false,
-				true,
-				true,
-			},
-			want: []string{"X: Got HTTP 404 (https://api.github.com/user/keys)"},
-		},
-		{
-			name: "missing scope",
-			input: input{
-				func(reg *httpmock.Registry) {
-					reg.Register(
-						httpmock.REST("GET", "user/keys"),
-						httpmock.StringResponse(`[{"id":1234,"key":"ssh-rsa AAAABbBB123","title":"Mac"}]`),
-					)
-					reg.Register(
-						httpmock.REST("GET", ""),
-						httpmock.ScopesResponder(""),
-					)
-				},
-				false,
-				false,
-				true,
-				true,
-			},
-			want: []string{
-				"X: missing required scope 'repo';missing required scope 'read:org';missing required scope 'read:public_key'",
-				"- To request missing scopes, run: gh auth refresh -h github.com",
-			},
-		},
-		{
-			name: "authentication failed",
-			input: input{
-				func(reg *httpmock.Registry) {
-					reg.Register(
-						httpmock.REST("GET", "user/keys"),
-						httpmock.StringResponse(`[{"id":1234,"key":"ssh-rsa AAAABbBB123","title":"Mac"}]`),
-					)
-					reg.Register(
-						httpmock.REST("GET", ""),
-						httpmock.StatusStringResponse(http.StatusNotFound, `{"message": "Not Found", "documentation_url": "url"}`),
-					)
-				},
-				false,
-				false,
-				true,
-				true,
-			},
-			want: []string{
-				"X: authentication failed",
-				"- The github.com token in ~/.config/gh/hosts.yml is no longer valid.",
-				"- To re-authenticate, run: gh auth login -h github.com",
-				"- To forget about this host, run: gh auth logout -h github.com",
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			reg := &httpmock.Registry{}
-			tt.input.httpStubs(reg)
-
-			io, _, _, _ := iostreams.Test()
-			io.SetStdoutTTY(true)
-			io.SetStdinTTY(true)
-			io.SetStderrTTY(true)
-
-			opts := ListOptions{
+			name: "list tty",
+			opts: ListOptions{
 				HTTPClient: func() (*http.Client, error) {
-					if tt.input.httpClientError {
-						return nil, errors.New("HttpClient error")
-					}
+					createdAt := time.Now().Add(time.Duration(-24) * time.Hour)
+					reg := &httpmock.Registry{}
+					reg.Register(
+						httpmock.REST("GET", "user/keys"),
+						httpmock.StringResponse(fmt.Sprintf(`[
+							{
+								"id": 1234,
+								"key": "ssh-rsa AAAABbBB123",
+								"title": "Mac",
+								"created_at": "%[1]s"
+							},
+							{
+								"id": 5678,
+								"key": "ssh-rsa EEEEEEEK247",
+								"title": "hubot@Windows",
+								"created_at": "%[1]s"
+							}
+						]`, createdAt.Format(time.RFC3339))),
+					)
 					return &http.Client{Transport: reg}, nil
 				},
-				IO: io,
-				Config: func() (config.Config, error) {
-					if tt.input.configError {
-						return nil, errors.New("Config error")
-					}
-					cfg := config.NewBlankConfig()
-					if tt.input.hasOauthToken {
-						err := cfg.Set("github.com", "oauth_token", "abc123")
-						if err != nil {
-							return nil, err
-						}
-					}
-					return cfg, nil
+			},
+			isTTY: true,
+			wantStdout: heredoc.Doc(`
+				Mac            ssh-rsa AAAABbBB123  1d
+				hubot@Windows  ssh-rsa EEEEEEEK247  1d
+			`),
+			wantStderr: "",
+		},
+		{
+			name: "list non-tty",
+			opts: ListOptions{
+				HTTPClient: func() (*http.Client, error) {
+					createdAt, _ := time.Parse(time.RFC3339, "2020-08-31T15:44:24+02:00")
+					reg := &httpmock.Registry{}
+					reg.Register(
+						httpmock.REST("GET", "user/keys"),
+						httpmock.StringResponse(fmt.Sprintf(`[
+							{
+								"id": 1234,
+								"key": "ssh-rsa AAAABbBB123",
+								"title": "Mac",
+								"created_at": "%[1]s"
+							},
+							{
+								"id": 5678,
+								"key": "ssh-rsa EEEEEEEK247",
+								"title": "hubot@Windows",
+								"created_at": "%[1]s"
+							}
+						]`, createdAt.Format(time.RFC3339))),
+					)
+					return &http.Client{Transport: reg}, nil
 				},
-			}
+			},
+			isTTY: false,
+			wantStdout: heredoc.Doc(`
+				Mac	ssh-rsa AAAABbBB123	2020-08-31T15:44:24+02:00
+				hubot@Windows	ssh-rsa EEEEEEEK247	2020-08-31T15:44:24+02:00
+			`),
+			wantStderr: "",
+		},
+		{
+			name: "no keys",
+			opts: ListOptions{
+				HTTPClient: func() (*http.Client, error) {
+					reg := &httpmock.Registry{}
+					reg.Register(
+						httpmock.REST("GET", "user/keys"),
+						httpmock.StringResponse(`[]`),
+					)
+					return &http.Client{Transport: reg}, nil
+				},
+			},
+			wantStdout: "",
+			wantStderr: "No SSH keys present in GitHub account.\n",
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			io, _, stdout, stderr := iostreams.Test()
+			io.SetStdoutTTY(tt.isTTY)
+			io.SetStdinTTY(tt.isTTY)
+			io.SetStderrTTY(tt.isTTY)
+
+			opts := tt.opts
+			opts.IO = io
 
 			err := listRun(&opts)
-			if err != nil && !tt.input.wantErr {
+			if (err != nil) != tt.wantErr {
 				t.Errorf("linRun() return error: %v", err)
+				return
 			}
-			if !reflect.DeepEqual(opts.ListMsg, tt.want) {
-				t.Errorf("linRun() = want %v, got %v", tt.want, opts.ListMsg)
+
+			if stdout.String() != tt.wantStdout {
+				t.Errorf("wants stdout %q, got %q", tt.wantStdout, stdout.String())
+			}
+			if stderr.String() != tt.wantStderr {
+				t.Errorf("wants stderr %q, got %q", tt.wantStderr, stderr.String())
 			}
 		})
 	}
