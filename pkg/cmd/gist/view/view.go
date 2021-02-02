907524f459135df0abb6180d37973ac754a10e25
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2021-02-02 19:17:23 +0300

    add --files to list filenames in gist (#2885)

--- pkg/cmd/gist/view/view.go
+++ pkg/cmd/gist/view/view.go
@@ -19,10 +19,11 @@ type ViewOptions struct {
 	IO         *iostreams.IOStreams
 	HttpClient func() (*http.Client, error)
 
-	Selector string
-	Filename string
-	Raw      bool
-	Web      bool
+	Selector  string
+	Filename  string
+	Raw       bool
+	Web       bool
+	ListFiles bool
 }
 
 func NewCmdView(f *cmdutil.Factory, runF func(*ViewOptions) error) *cobra.Command {
@@ -51,6 +52,7 @@ func NewCmdView(f *cmdutil.Factory, runF func(*ViewOptions) error) *cobra.Comman
 
 	cmd.Flags().BoolVarP(&opts.Raw, "raw", "r", false, "Print raw instead of rendered gist contents")
 	cmd.Flags().BoolVarP(&opts.Web, "web", "w", false, "Open gist in the browser")
+	cmd.Flags().BoolVarP(&opts.ListFiles, "files", "", false, "Display filenames list from the gist")
 	cmd.Flags().StringVarP(&opts.Filename, "filename", "f", "", "Display a single file from the gist")
 
 	return cmd
@@ -126,7 +128,7 @@ func viewRun(opts *ViewOptions) error {
 
 	cs := opts.IO.ColorScheme()
 
-	if gist.Description != "" {
+	if gist.Description != "" && !opts.ListFiles {
 		fmt.Fprintf(opts.IO.Out, "%s\n\n", cs.Bold(gist.Description))
 	}
 
@@ -136,6 +138,13 @@ func viewRun(opts *ViewOptions) error {
 		filenames = append(filenames, fn)
 	}
 	sort.Strings(filenames)
+
+	if opts.ListFiles {
+		for _, fn := range filenames {
+			fmt.Fprintln(opts.IO.Out, fn)
+		}
+		return nil
+	}
 
 	for i, fn := range filenames {
 		if showFilenames {
