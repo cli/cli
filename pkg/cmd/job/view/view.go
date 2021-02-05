2a1c601244c8f57d66b6529a33a7d20510261e84
vilmibm <vilmibm@github.com>
2021-02-05 14:40:41 -0800

    reintroduce linter error

--- pkg/cmd/job/view/view.go
+++ pkg/cmd/job/view/view.go
@@ -239,10 +239,9 @@ func promptForJob(opts ViewOptions, client *api.Client, repo ghrepo.Interface, r
 		Options:  candidates,
 		PageSize: 10,
 	}, &selected)
-
-	if err != nil {
-		return "", err
-	}
+	// if err != nil {
+	// 	return "", err
+	// }
 
 	return fmt.Sprintf("%d", jobs[selected].ID), nil
 }
