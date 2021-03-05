5b3dfbf445d7cc80d2f6acd7b067c72998625529
vilmibm <vilmibm@github.com>
2021-03-05 15:58:46 -0600

    fetch workflows

--- pkg/cmd/workflow/list/list.go
+++ pkg/cmd/workflow/list/list.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/cli/cli/api"
 	"github.com/cli/cli/internal/ghrepo"
 	"github.com/cli/cli/pkg/cmdutil"
 	"github.com/cli/cli/pkg/iostreams"
@@ -60,5 +61,75 @@ func NewCmdList(f *cmdutil.Factory, runF func(*ListOptions) error) *cobra.Comman
 }
 
 func listRun(opts *ListOptions) error {
+	repo, err := opts.BaseRepo()
+	if err != nil {
+		return fmt.Errorf("could not determine base repo: %w", err)
+	}
+
+	httpClient, err := opts.HttpClient()
+	if err != nil {
+		return fmt.Errorf("could not create http client: %w", err)
+	}
+	client := api.NewClientFromHTTP(httpClient)
+
+	workflows, err := getWorkflows(client, repo, opts.Limit)
+	if err != nil {
+		return fmt.Errorf("could not get workflows: %w", err)
+	}
+
+	out := opts.IO.Out
+
+	for _, workflow := range workflows {
+		fmt.Fprintf(out, "%s\n", workflow.Name)
+	}
+
 	return nil
+}
+
+type Workflow struct {
+	Name string
+}
+
+type WorkflowsPayload struct {
+	Workflows []Workflow
+}
+
+func getWorkflows(client *api.Client, repo ghrepo.Interface, limit int) ([]Workflow, error) {
+	perPage := limit
+	page := 1
+	if limit > 100 {
+		perPage = 100
+	}
+
+	workflows := []Workflow{}
+
+	for len(workflows) < limit {
+		var result WorkflowsPayload
+
+		path := fmt.Sprintf("repos/%s/actions/workflows?per_page=%d&page=%d", ghrepo.FullName(repo), perPage, page)
+
+		err := client.REST(repo.RepoHost(), "GET", path, nil, &result)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(result.Workflows) == 0 {
+			break
+		}
+
+		for _, workflow := range result.Workflows {
+			workflows = append(workflows, workflow)
+			if len(workflows) == limit {
+				break
+			}
+		}
+
+		if len(result.Workflows) < perPage {
+			break
+		}
+
+		page++
+	}
+
+	return workflows, nil
 }
