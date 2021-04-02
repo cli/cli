238a371477070ede7d7359c5643b6e6cff9c5f85
vilmibm <vilmibm@neongrid.space>
2021-04-02 10:58:33 -0500

    no need to hide this

--- pkg/cmd/run/rerun/rerun.go
+++ pkg/cmd/run/rerun/rerun.go
@@ -30,10 +30,9 @@ func NewCmdRerun(f *cmdutil.Factory, runF func(*RerunOptions) error) *cobra.Comm
 	}
 
 	cmd := &cobra.Command{
-		Use:    "rerun [<run-id>]",
-		Short:  "Rerun a given run",
-		Args:   cobra.MaximumNArgs(1),
-		Hidden: true,
+		Use:   "rerun [<run-id>]",
+		Short: "Rerun a given run",
+		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// support `-R, --repo` override
 			opts.BaseRepo = f.BaseRepo
