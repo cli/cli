b586d517786282065737f3eb8d9dc2ca126c814c
Tobias Klauser <tklauser@distanz.ch>
2021-04-28 18:44:36 +0200

    Use golang.org/x/term
    The golang.org/x/crypto/ssh/terminal package is deprecated and merely a
    wrapper around golang.org/x/term. Use the latter directly.

--- pkg/iostreams/iostreams.go
+++ pkg/iostreams/iostreams.go
@@ -17,7 +17,7 @@ import (
 	"github.com/mattn/go-colorable"
 	"github.com/mattn/go-isatty"
 	"github.com/muesli/termenv"
-	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/term"
 )
 
 type IOStreams struct {
@@ -332,7 +332,7 @@ func isCygwinTerminal(w io.Writer) bool {
 
 func terminalSize(w io.Writer) (int, int, error) {
 	if f, isFile := w.(*os.File); isFile {
-		return terminal.GetSize(int(f.Fd()))
+		return term.GetSize(int(f.Fd()))
 	}
 	return 0, 0, fmt.Errorf("%v is not a file", w)
 }
