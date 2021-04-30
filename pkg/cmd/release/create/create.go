5fda78ce875874c63c3c29a2d27e3a9228510fd9
Amirnateghi1 <65473516+Amirnateghi1@users.noreply.github.com>
2021-04-30 10:57:00 +0430

    Revert "add some more examples for release create"

--- pkg/cmd/release/create/create.go
+++ pkg/cmd/release/create/create.go
@@ -78,19 +78,13 @@ func NewCmdCreate(f *cmdutil.Factory, runF func(*CreateOptions) error) *cobra.Co
 			git, push the tag to GitHub, then run this command.
 		`, "`"),
 		Example: heredoc.Doc(`
-			Interactively create a release
-			$ gh release create v1.2.3
-
-			Non-interactively create a release
-			$ gh release create v1.2.3 --notes "bugfix release"
-
-			Use release notes from a file
+			# use release notes from a file
 			$ gh release create v1.2.3 -F changelog.md
 
-			Upload all tarballs in a directory as release assets
+			# upload all tarballs in a directory as release assets
 			$ gh release create v1.2.3 ./dist/*.tgz
 
-			Upload a release asset with a display label
+			# upload a release asset with a display label
 			$ gh release create v1.2.3 '/path/to/asset.zip#My display label'
 		`),
 		Args: cmdutil.MinimumArgs(1, "could not create: no tag name provided"),
