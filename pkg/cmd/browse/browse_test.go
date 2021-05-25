24abf29acb22697dff3ba6fcacccb1835e53e675
bchadwic <bchadwick@mail.greenriver.edu>
2021-05-24 21:16:37 -0700

    created a test file

---
+++ pkg/cmd/browse/browse_test.go
@@ -0,0 +1,12 @@
+package browse
+
+import (
+	"testing"
+)
+
+func TestNewCmdBrowse(t *testing.T) {
+	// TODO test the use of the api using "gh browse"
+	//		instead of opening multiple browsers for each test,
+	//		we can test the http code sent back after calling a site
+
+}
