51ac0e35f772e92f1939d15e1957ecf69083a2b8
vilmibm <vilmibm@neongrid.space>
2021-06-11 12:08:49 -0500

    REVERT ME pin forked survey

--- pkg/prompt/prompt.go
+++ pkg/prompt/prompt.go
@@ -18,13 +18,26 @@ var Confirm = func(prompt string, result *bool) error {
 		Message: prompt,
 		Default: true,
 	}
-	return SurveyAskOne(p, result)
+	opts := []survey.AskOpt{}
+	opts = append(opts, func(options *survey.AskOptions) error {
+		options.PromptConfig.ShowCursor = true
+		return nil
+	})
+	return SurveyAskOne(p, result, opts...)
 }
 
 var SurveyAskOne = func(p survey.Prompt, response interface{}, opts ...survey.AskOpt) error {
+	opts = append(opts, func(options *survey.AskOptions) error {
+		options.PromptConfig.ShowCursor = true
+		return nil
+	})
 	return survey.AskOne(p, response, opts...)
 }
 
 var SurveyAsk = func(qs []*survey.Question, response interface{}, opts ...survey.AskOpt) error {
+	opts = append(opts, func(options *survey.AskOptions) error {
+		options.PromptConfig.ShowCursor = true
+		return nil
+	})
 	return survey.Ask(qs, response, opts...)
 }
