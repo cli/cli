b43f78bc196f1fd6a9e4a1ba0e63a9d3ad861740
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-07-02 23:06:50 +0200

    completions: auto-complete `--repo` values
    Looking at the locally-registered remotes, we have a pretty good idea
    what `--repo` values are available. Let's complete them.
    
    Helped by Nate Smith and Mislav Marohnić.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- pkg/cmdutil/repo_override.go
+++ pkg/cmdutil/repo_override.go
@@ -2,6 +2,8 @@ package cmdutil
 
 import (
 	"os"
+	"sort"
+	"strings"
 
 	"github.com/cli/cli/internal/ghrepo"
 	"github.com/spf13/cobra"
@@ -19,6 +21,30 @@ func executeParentHooks(cmd *cobra.Command, args []string) error {
 
 func EnableRepoOverride(cmd *cobra.Command, f *Factory) {
 	cmd.PersistentFlags().StringP("repo", "R", "", "Select another repository using the `[HOST/]OWNER/REPO` format")
+	_ = cmd.RegisterFlagCompletionFunc("repo", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		var results []string
+		remotes, err := f.Remotes()
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveError
+		}
+		config, err := f.Config()
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveError
+		}
+		gh_host,err := config.DefaultHost()
+		if err != nil {
+			return nil, cobra.ShellCompDirectiveError
+		}
+
+		for _, remote := range remotes.FilterByHosts([]string{gh_host}) {
+			repo := remote.RepoOwner() + "/" + remote.RepoName()
+			if strings.HasPrefix(repo, toComplete) {
+				results = append(results, repo)
+			}
+		}
+		sort.Strings(results)
+		return results, cobra.ShellCompDirectiveNoFileComp
+	})
 
 	cmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
 		if err := executeParentHooks(cmd, args); err != nil {
