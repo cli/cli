d506a97419e4f1e2e3f35746bb0426d42fd598de
Mislav Marohnić <git@mislav.net>
2021-07-15 16:10:03 +0200

    Increase ssh command timeout and improve error message
    - My `github/github` codespace failed to start within 10s
    - Output more precise error message

--- cmd/ghcs/ssh.go
+++ cmd/ghcs/ssh.go
@@ -111,8 +111,8 @@ func SSH(sshProfile string) error {
 			time.Sleep(1 * time.Second)
 		}
 
-		if retries == 10 {
-			return errors.New("Failed to start codespace")
+		if retries == 30 {
+			return errors.New("timed out while waiting for the codespace to start")
 		}
 
 		codespace, err = apiClient.GetCodespace(ctx, token, codespace.OwnerLogin, codespace.Name)
