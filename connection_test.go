3a2ade23a4a154eb327c097214784aa95bd138c9
Jose Garcia <josebalius@github.com>
2021-07-28 13:52:30 +0000

    Connection test

--- connection_test.go
+++ connection_test.go
@@ -31,3 +31,11 @@ func TestConnectionInvalid(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestConnectionURI(t *testing.T) {
+	conn := Connection{"sess-id", "sess-token", "sas", "sb://endpoint/.net/liveshare"}
+	uri := conn.uri("connect")
+	if uri != "wss://endpoint/.net:443/$hc/liveshare?sb-hc-action=connect&sb-hc-token=sas" {
+		t.Errorf("uri is not correct, got: '%v'", uri)
+	}
+}
