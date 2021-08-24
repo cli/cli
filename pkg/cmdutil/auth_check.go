6cf6bb4a44453f0f03bd987b73ea1c8e734a6a3f
Cuong Manh Le <cuong.manhle.vn@gmail.com>
2021-08-25 01:35:49 +0700

    Disable auth for cobra shell request completion hidden commands
    Those hidden command are used by the shell completion scripts, but they
    are not disabled auth check. Thus, the shell completion does not work
    even the completion setup was done properly.
    
    Fixes #4188

--- pkg/cmdutil/auth_check.go
+++ pkg/cmdutil/auth_check.go
@@ -34,9 +34,11 @@ func CheckAuth(cfg config.Config) bool {
 }
 
 func IsAuthCheckEnabled(cmd *cobra.Command) bool {
-	if cmd.Name() == "help" {
+	switch cmd.Name() {
+	case "help", cobra.ShellCompRequestCmd, cobra.ShellCompNoDescRequestCmd:
 		return false
 	}
+
 	for c := cmd; c.Parent() != nil; c = c.Parent() {
 		if c.Annotations != nil && c.Annotations["skipAuthCheck"] == "true" {
 			return false
