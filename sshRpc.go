18ab421b0846e7a152d431646b12702f14b26ba9
Edmundo Gonzalez <51725820+edgonmsft@users.noreply.github.com>
2021-08-26 23:04:05 +0000

    Rename to SSHServer

--- sshRpc.go
+++ sshRpc.go
@@ -5,26 +5,26 @@ import (
 	"errors"
 )
 
-type SshRpc struct {
+type SSHServer struct {
 	client *Client
 }
 
-func NewSSHRpc(client *Client) (*SshRpc, error) {
+func NewSSHServer(client *Client) (*SSHServer, error) {
 	if !client.hasJoined() {
 		return nil, errors.New("client must join before creating server")
 	}
-	return &SshRpc{client: client}, nil
+	return &SSHServer{client: client}, nil
 }
 
-type SshServerStartResult struct {
+type SSHServerStartResult struct {
 	Result     bool   `json:"result"`
 	ServerPort string `json:"serverPort"`
 	User       string `json:"user"`
 	Message    string `json:"message"`
 }
 
-func (s *SshRpc) StartRemoteServer(ctx context.Context) (SshServerStartResult, error) {
-	var response SshServerStartResult
+func (s *SSHServer) StartRemoteServer(ctx context.Context) (SSHServerStartResult, error) {
+	var response SSHServerStartResult
 
 	if err := s.client.rpc.do(ctx, "ISshServerHostService.startRemoteServer", []string{}, &response); err != nil {
 		return response, err
