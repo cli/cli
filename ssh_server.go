497b45e4e2e41c2fca55f61995a289a6e62022e6
Jose Garcia <josebalius@github.com>
2021-09-14 23:57:40 +0000

    ssh server docs

--- ssh_server.go
+++ ssh_server.go
@@ -4,14 +4,20 @@ import (
 	"context"
 )
 
+// A SSHServer handles starting the remote SSH server.
+// If there is no SSH server available it installs one.
 type SSHServer struct {
 	session *Session
 }
 
+// SSHServer returns a new SSHServer from the LiveShare Session.
 func (session *Session) SSHServer() *SSHServer {
 	return &SSHServer{session: session}
 }
 
+// SSHServerStartResult contains whether or not the start of the SSH server was
+// successful. If it succeeded the server port and user is included. If it failed,
+// it contains an explanation message.
 type SSHServerStartResult struct {
 	Result     bool   `json:"result"`
 	ServerPort string `json:"serverPort"`
@@ -19,6 +25,7 @@ type SSHServerStartResult struct {
 	Message    string `json:"message"`
 }
 
+// StartRemoteServer starts or install the remote SSH server and returns the result.
 func (s *SSHServer) StartRemoteServer(ctx context.Context) (*SSHServerStartResult, error) {
 	var response SSHServerStartResult
 
