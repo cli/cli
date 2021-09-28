d58013011034cba1f5f8c99475463b2ee034dd6c
tahsinchouhan <86396691+tahsinchouhan@users.noreply.github.com>
2021-09-28 18:27:10 +0530

    Create gh-cli-linux.sh

---
+++ docs/gh-cli-linux.sh
@@ -0,0 +1,5 @@
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+
+sudo apt install gh
