23d41ddb0d0e204a6816e51d4a296ae68fd360bd
Jose Garcia <jose.balius@gmail.com>
2021-09-30 11:09:59 -0400

    Format files

--- cmd/ghcs/ports.go
+++ cmd/ghcs/ports.go
@@ -12,8 +12,8 @@ import (
 	"strings"
 
 	"github.com/cli/cli/v2/cmd/ghcs/output"
-	"github.com/cli/cli/v2/internal/codespaces/api"
 	"github.com/cli/cli/v2/internal/codespaces"
+	"github.com/cli/cli/v2/internal/codespaces/api"
 	"github.com/cli/cli/v2/internal/liveshare"
 	"github.com/muhammadmuzzammil1998/jsonc"
 	"github.com/spf13/cobra"
