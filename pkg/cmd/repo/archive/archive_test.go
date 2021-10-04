0788805fdc702da45249863ed4687b25394112e2
Parth Patel <ppatil91099@gmail.com>
2021-10-04 14:34:18 -0400

    added api tests ( not passing )

--- pkg/cmd/repo/archive/archive_test.go
+++ pkg/cmd/repo/archive/archive_test.go
@@ -2,11 +2,14 @@ package archive
 
 import (
 	"bytes"
+	"fmt"
+	"net/http"
 	"testing"
 
-	//"github.com/cli/cli/v2/pkg/httpmock"
-	//"github.com/cli/cli/v2/internal/ghrepo"
+	"github.com/MakeNowJust/heredoc"
+	"github.com/cli/cli/v2/internal/ghrepo"
 	"github.com/cli/cli/v2/pkg/cmdutil"
+	"github.com/cli/cli/v2/pkg/httpmock"
 	"github.com/cli/cli/v2/pkg/iostreams"
 	"github.com/google/shlex"
 	"github.com/stretchr/testify/assert"
@@ -66,6 +69,78 @@ func TestNewCmdArchive(t *testing.T) {
 			
 			assert.NoError(t, err)
 			assert.Equal(t, tt.output.RepoArg, gotOpts.RepoArg)
+		})
+	}
+}
+
+func Test_ArchiveRun(t *testing.T)  {
+	tests := []struct {
+		name       string
+		opts       *ArchiveOptions
+		repoName   string
+		stdoutTTY  bool
+		wantOut    string
+		wantStderr string
+		wantErr    bool
+	}{
+		{
+			name: "OWNER/REPO",
+			wantOut: heredoc.Doc(`
+				✓ Archived repository OWNER/REPO
+				`),
+		},
+		{
+			name: "OWNER/REPO", 
+			wantOut: heredoc.Doc(`
+				! Repository OWNER/REPO is already archived
+				`),
+		},
+	}
+
+	for _, tt := range tests {
+		if tt.opts == nil {
+			tt.opts = &ArchiveOptions{}
+		}
+
+		if tt.repoName == "" {
+			tt.repoName = "OWNER/REPO"
+		}
+
+		tt.opts.BaseRepo = func() (ghrepo.Interface, error) {
+			repo, _ := ghrepo.FromFullName(tt.repoName)
+			return repo, nil
+		}
+
+		reg := &httpmock.Registry{}
+		reg.Register(
+			httpmock.GraphQL(`query archiveRepository\b`),
+			httpmock.StringResponse(`
+		{ "data": {
+			"repository": {
+			"description": "OWNER/REPO"
+		} } }`))
+		reg.Register(
+			httpmock.REST("GET", fmt.Sprintf("repos/%s/readme", tt.repoName)),
+			httpmock.StringResponse(`
+		{ "name": "readme.md",
+		"content": "IyB0cnVseSBjb29sIHJlYWRtZSBjaGVjayBpdCBvdXQ="}`))
+
+		tt.opts.HttpClient = func() (*http.Client, error) {
+			return &http.Client{Transport: reg}, nil
+		}
+
+		io, _, stdout, stderr := iostreams.Test()
+		tt.opts.IO = io
+
+		t.Run(tt.name, func(t *testing.T) {
+			io.SetStdoutTTY(tt.stdoutTTY)
+
+			if err := archiveRun(tt.opts); (err != nil) != tt.wantErr {
+				t.Errorf("archiveRun() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			assert.Equal(t, tt.wantStderr, stderr.String())
+			assert.Equal(t, tt.wantOut, stdout.String())
+			reg.Verify(t)
 		})
 	}
 }
