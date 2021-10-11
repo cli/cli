cedbbe3c2ae569f0f7a011b83930a1930145ee5a
Mateusz Urbanek <echelon.div@gmail.com>
2021-10-11 19:32:40 +0100

    Add limit flag to codespace list (#4453)
    Co-authored-by: Mislav Marohnić <mislav@github.com>

--- pkg/cmd/codespace/list.go
+++ pkg/cmd/codespace/list.go
@@ -6,28 +6,35 @@ import (
 	"os"
 
 	"github.com/cli/cli/v2/pkg/cmd/codespace/output"
+	"github.com/cli/cli/v2/pkg/cmdutil"
 	"github.com/spf13/cobra"
 )
 
 func newListCmd(app *App) *cobra.Command {
 	var asJSON bool
+	var limit int
 
 	listCmd := &cobra.Command{
 		Use:   "list",
 		Short: "List your codespaces",
 		Args:  noArgsConstraint,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return app.List(cmd.Context(), asJSON)
+			if limit < 1 {
+				return &cmdutil.FlagError{Err: fmt.Errorf("invalid limit: %v", limit)}
+			}
+
+			return app.List(cmd.Context(), asJSON, limit)
 		},
 	}
 
 	listCmd.Flags().BoolVar(&asJSON, "json", false, "Output as JSON")
+	listCmd.Flags().IntVarP(&limit, "limit", "L", 30, "Maximum number of codespaces to list")
 
 	return listCmd
 }
 
-func (a *App) List(ctx context.Context, asJSON bool) error {
-	codespaces, err := a.apiClient.ListCodespaces(ctx)
+func (a *App) List(ctx context.Context, asJSON bool, limit int) error {
+	codespaces, err := a.apiClient.ListCodespaces(ctx, limit)
 	if err != nil {
 		return fmt.Errorf("error getting codespaces: %w", err)
 	}
