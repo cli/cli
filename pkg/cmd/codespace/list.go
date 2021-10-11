94044c9d3c25eba0908ed41e513198d1e1c9fe06
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2021-10-11 07:38:05 +0200

    feat(codespaces): added limit flag to list subcommand

--- pkg/cmd/codespace/list.go
+++ pkg/cmd/codespace/list.go
@@ -11,25 +11,24 @@ import (
 
 func newListCmd(app *App) *cobra.Command {
 	var asJSON bool
-	var limit int16
+	var limit int
 
 	listCmd := &cobra.Command{
 		Use:   "list",
 		Short: "List your codespaces",
 		Args:  noArgsConstraint,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return app.List(cmd.Context(), asJSON)
+			return app.List(cmd.Context(), asJSON, limit)
 		},
 	}
 
 	listCmd.Flags().BoolVar(&asJSON, "json", false, "Output as JSON")
-	listCmd.Flags().Int16VarP(&limit, "limit", "L", 30, "Maximum number of codespaces to list")
-	// take a look at pkg/cmd/repo/list.go:103
+	listCmd.Flags().IntVarP(&limit, "limit", "L", 30, "Maximum number of codespaces to list")
 
 	return listCmd
 }
 
-func (a *App) List(ctx context.Context, asJSON bool) error {
+func (a *App) List(ctx context.Context, asJSON bool, limit int) error {
 	codespaces, err := a.apiClient.ListCodespaces(ctx)
 	if err != nil {
 		return fmt.Errorf("error getting codespaces: %w", err)
@@ -37,7 +36,10 @@ func (a *App) List(ctx context.Context, asJSON bool) error {
 
 	table := output.NewTable(os.Stdout, asJSON)
 	table.SetHeader([]string{"Name", "Repository", "Branch", "State", "Created At"})
-	for _, apiCodespace := range codespaces {
+	for i, apiCodespace := range codespaces {
+		if i == limit {
+			break
+		}
 		cs := codespace{apiCodespace}
 		table.Append([]string{
 			cs.Name,
