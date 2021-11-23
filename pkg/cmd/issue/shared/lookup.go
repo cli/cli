1eb790cacd67a7860e248688c6f7e4773f3510d1
Mislav Marohnić <mislav@github.com>
2021-11-23 16:40:14 +0100

    Improve `issue comment` re: overfetching, handling PRs
    - `issue comment` no longer fetches all issue fields and thus avoids the
      problem when loading failed due to token not having access to projects
    
    - `issue comment` now accepts either issue or pull number as argument.

--- pkg/cmd/issue/shared/lookup.go
+++ pkg/cmd/issue/shared/lookup.go
@@ -2,6 +2,7 @@ package shared
 
 import (
 	"fmt"
+	"net/http"
 	"net/url"
 	"regexp"
 	"strconv"
@@ -11,6 +12,8 @@ import (
 	"github.com/cli/cli/v2/internal/ghrepo"
 )
 
+// IssueFromArg loads an issue with all its fields.
+// Deprecated: use IssueFromArgWithFields instead.
 func IssueFromArg(apiClient *api.Client, baseRepoFn func() (ghrepo.Interface, error), arg string) (*api.Issue, ghrepo.Interface, error) {
 	issueNumber, baseRepo := issueMetadataFromURL(arg)
 
@@ -30,7 +33,31 @@ func IssueFromArg(apiClient *api.Client, baseRepoFn func() (ghrepo.Interface, er
 		}
 	}
 
-	issue, err := issueFromNumber(apiClient, baseRepo, issueNumber)
+	issue, err := api.IssueByNumber(apiClient, baseRepo, issueNumber)
+	return issue, baseRepo, err
+}
+
+// IssueFromArgWithFields loads an issue or pull request with the specified fields.
+func IssueFromArgWithFields(httpClient *http.Client, baseRepoFn func() (ghrepo.Interface, error), arg string, fields []string) (*api.Issue, ghrepo.Interface, error) {
+	issueNumber, baseRepo := issueMetadataFromURL(arg)
+
+	if issueNumber == 0 {
+		var err error
+		issueNumber, err = strconv.Atoi(strings.TrimPrefix(arg, "#"))
+		if err != nil {
+			return nil, nil, fmt.Errorf("invalid issue format: %q", arg)
+		}
+	}
+
+	if baseRepo == nil {
+		var err error
+		baseRepo, err = baseRepoFn()
+		if err != nil {
+			return nil, nil, fmt.Errorf("could not determine base repo: %w", err)
+		}
+	}
+
+	issue, err := findIssueOrPR(httpClient, baseRepo, issueNumber, fields)
 	return issue, baseRepo, err
 }
 
@@ -56,6 +83,34 @@ func issueMetadataFromURL(s string) (int, ghrepo.Interface) {
 	return issueNumber, repo
 }
 
-func issueFromNumber(apiClient *api.Client, repo ghrepo.Interface, issueNumber int) (*api.Issue, error) {
-	return api.IssueByNumber(apiClient, repo, issueNumber)
+func findIssueOrPR(httpClient *http.Client, repo ghrepo.Interface, number int, fields []string) (*api.Issue, error) {
+	type response struct {
+		Repository struct {
+			Issue *api.Issue
+		}
+	}
+
+	query := fmt.Sprintf(`
+	query IssueByNumber($owner: String!, $repo: String!, $number: Int!) {
+		repository(owner: $owner, name: $repo) {
+			issue: issueOrPullRequest(number: $number) {
+				...on Issue{%[1]s}
+				...on PullRequest{%[1]s}
+			}
+		}
+	}`, api.PullRequestGraphQL(fields))
+
+	variables := map[string]interface{}{
+		"owner":  repo.RepoOwner(),
+		"repo":   repo.RepoName(),
+		"number": number,
+	}
+
+	var resp response
+	client := api.NewClientFromHTTP(httpClient)
+	if err := client.GraphQL(repo.RepoHost(), query, variables, &resp); err != nil {
+		return nil, err
+	}
+
+	return resp.Repository.Issue, nil
 }
