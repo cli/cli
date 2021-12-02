aef1a572481b1ac26e52de43bd6ed80d6e1499c5
Mislav Marohnić <mislav@github.com>
2021-12-02 17:28:23 +0100

    fix

--- pkg/cmd/auth/setupgit/setupgit_test.go
+++ pkg/cmd/auth/setupgit/setupgit_test.go
@@ -1,14 +1,11 @@
 package setupgit
 
 import (
-	"bytes"
 	"fmt"
 	"testing"
 
 	"github.com/cli/cli/v2/internal/config"
-	"github.com/cli/cli/v2/pkg/cmdutil"
 	"github.com/cli/cli/v2/pkg/iostreams"
-	"github.com/google/shlex"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -19,54 +16,6 @@ type mockGitConfigurer struct {
 
 func (gf *mockGitConfigurer) Setup(hostname, username, authToken string) error {
 	return gf.setupErr
-}
-
-func Test_NewCmdSetupGit(t *testing.T) {
-	tests := []struct {
-		name     string
-		cli      string
-		wants    SetupGitOptions
-		wantsErr bool
-	}{
-		{
-			name:  "no arguments",
-			cli:   "",
-			wants: SetupGitOptions{Hostname: ""},
-		},
-		{
-			name:  "hostname argument",
-			cli:   "--hostname whatever",
-			wants: SetupGitOptions{Hostname: "whatever"},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			f := &cmdutil.Factory{}
-
-			argv, err := shlex.Split(tt.cli)
-			assert.NoError(t, err)
-
-			var gotOpts *SetupGitOptions
-			cmd := NewCmdSetupGit(f, func(opts *SetupGitOptions) error {
-				gotOpts = opts
-				return nil
-			})
-
-			// TODO cobra hack-around
-			cmd.Flags().BoolP("help", "x", false, "")
-
-			cmd.SetArgs(argv)
-			cmd.SetIn(&bytes.Buffer{})
-			cmd.SetOut(&bytes.Buffer{})
-			cmd.SetErr(&bytes.Buffer{})
-
-			_, err = cmd.ExecuteC()
-			assert.NoError(t, err)
-
-			assert.Equal(t, tt.wants.Hostname, gotOpts.Hostname)
-		})
-	}
 }
 
 func Test_setupGitRun(t *testing.T) {
