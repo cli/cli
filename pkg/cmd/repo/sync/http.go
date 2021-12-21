1260023547c6b975ed9d068b13cb1f5659f49928
Mislav Marohnić <mislav@github.com>
2021-12-21 16:44:27 +0100

    repo sync: Use the new merge-upstream API if available
    This helps avoid the errors when a new commit from upstream is not yet
    available in the fork, resulting in a HTTP 404 when trying to update the
    ref in the fork.
    
    If the merge-upstream API is unavailable (404) or errors out (409/422),
    the functionality falls back to the previous functionality of manually
    updating refs. This ensures that `--force` still has effect.

--- pkg/cmd/repo/sync/http.go
+++ pkg/cmd/repo/sync/http.go
@@ -3,7 +3,9 @@ package sync
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"net/http"
 
 	"github.com/cli/cli/v2/api"
 	"github.com/cli/cli/v2/internal/ghrepo"
@@ -25,6 +27,39 @@ func latestCommit(client *api.Client, repo ghrepo.Interface, branch string) (com
 	path := fmt.Sprintf("repos/%s/%s/git/refs/heads/%s", repo.RepoOwner(), repo.RepoName(), branch)
 	err := client.REST(repo.RepoHost(), "GET", path, nil, &response)
 	return response, err
+}
+
+type upstreamMergeErr struct{ error }
+
+var upstreamMergeUnavailableErr = upstreamMergeErr{errors.New("upstream merge API is unavailable")}
+
+func triggerUpstreamMerge(client *api.Client, repo ghrepo.Interface, branch string) (string, error) {
+	var payload bytes.Buffer
+	if err := json.NewEncoder(&payload).Encode(map[string]interface{}{
+		"branch": branch,
+	}); err != nil {
+		return "", err
+	}
+
+	var response struct {
+		Message    string `json:"message"`
+		MergeType  string `json:"merge_type"`
+		BaseBranch string `json:"base_branch"`
+	}
+	path := fmt.Sprintf("repos/%s/%s/merge-upstream", repo.RepoOwner(), repo.RepoName())
+	var httpErr api.HTTPError
+	if err := client.REST(repo.RepoHost(), "POST", path, &payload, &response); err != nil {
+		if errors.As(err, &httpErr) {
+			switch httpErr.StatusCode {
+			case http.StatusUnprocessableEntity, http.StatusConflict:
+				return "", upstreamMergeErr{errors.New(httpErr.Message)}
+			case http.StatusNotFound:
+				return "", upstreamMergeUnavailableErr
+			}
+		}
+		return "", err
+	}
+	return response.BaseBranch, nil
 }
 
 func syncFork(client *api.Client, repo ghrepo.Interface, branch, SHA string, force bool) error {
