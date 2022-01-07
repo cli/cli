840f93b00c5ce38fb3d863aa3a9c28469193db22
nate smith <vilmibm@github.com>
2022-01-07 12:33:22 -0600

    allow debugging output

--- script/sign-windows-executable.sh
+++ script/sign-windows-executable.sh
@@ -7,14 +7,14 @@ curl \
   -H "Authorization: token $DESKTOP_CERT_TOKEN" \
   -H "Accept: application/vnd.github.v3.raw" \
   --output windows-certificate.pfx \
-  --silent \
+  #--silent \
   https://api.github.com/repos/desktop/desktop-secrets/contents/windows-certificate.pfx
 
 PROGRAM_NAME="GitHub CLI"
 
 # Convert private key to the expected format
 openssl pkcs12 -in windows-certificate.pfx -nocerts -nodes -out private-key.pem  -passin pass:${GITHUB_CERT_PASSWORD}
-openssl rsa -in private-key.pem -outform PVK -pvk-none -out private-key.pvk 2>/dev/null # Always writes to STDERR
+openssl rsa -in private-key.pem -outform PVK -pvk-none -out private-key.pvk # 2>/dev/null # Always writes to STDERR
 
 # Convert certificate chain into the expected format
 openssl pkcs12 -in windows-certificate.pfx -nokeys -nodes -out certificate.pem -passin pass:${GITHUB_CERT_PASSWORD}
@@ -26,5 +26,5 @@ signcode \
   -n $PROGRAM_NAME \
   -t http://timestamp.digicert.com \
   -a sha256 \
-$EXECUTABLE_PATH \
-1> /dev/null # STDOUT a little bit chatty here, with multiple lines
+$EXECUTABLE_PATH # \
+# 1 > /dev/null # STDOUT a little bit chatty here, with multiple lines
