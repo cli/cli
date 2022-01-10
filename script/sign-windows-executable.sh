907a306441f00fd3c6161cff8ad3b40c805bed4e
nate smith <vilmibm@github.com>
2022-01-10 15:51:32 -0600

    try and get a clearer error

--- script/sign-windows-executable.sh
+++ script/sign-windows-executable.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 EXECUTABLE_PATH=$1
 
@@ -9,8 +10,11 @@ curl \
   --output windows-certificate.pfx \
   https://api.github.com/repos/desktop/desktop-secrets/contents/windows-certificate.pfx
 
-openssl pkcs12 -in windows-certificate.pfx -nocerts -nodes -out private-key.pem  -passin pass:${GITHUB_CERT_PASSWORD}
-openssl pkcs12 -in windows-certificate.pfx -nokeys -nodes -out certificate.pem -passin pass:${GITHUB_CERT_PASSWORD}
+stat windows-certificate.pfx
+file windows-certificate.pfx
+
+openssl pkcs12 -in windows-certificate.pfx -nocerts -nodes -out private-key.pem  -passin pass:${GITHUB_CERT_PASSWORD} || echo "no bueno 1"
+openssl pkcs12 -in windows-certificate.pfx -nokeys -nodes -out certificate.pem -passin pass:${GITHUB_CERT_PASSWORD} || echo "no bueno 2"
 
 osslsigncode sign \
   -certs certificate.pem \
@@ -18,9 +22,15 @@ osslsigncode sign \
   -n "GitHub CLI" \
   -t http://timestamp.digicert.com \
   -in $EXECUTABLE_PATH \
-  -out gh_signed.exe
+  -out gh_signed.exe || echo "no bueno 3"
+
+stat gh_signed.exe
+file gh_signed.exe
 
 # Oddly, there can be a delay before the file is *actually* available - wait for it
-while [ ! -f gh_signed.exe ]; do sleep 1; done;
+sleep 10
+# this was forcing the action to time out which makes debugging harder. 30
+# seconds isn't ideal but it's better than 3 minutes.
+#while [ ! -f gh_signed.exe ]; do sleep 1; done;
 
 mv gh_signed.exe $EXECUTABLE_PATH
