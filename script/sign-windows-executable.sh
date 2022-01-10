c6ad33c4addc133f751bb31a6ceccc794b8387f3
nate smith <vilmibm@github.com>
2022-01-10 16:15:51 -0600

    testing for race condition

--- script/sign-windows-executable.sh
+++ script/sign-windows-executable.sh
@@ -16,16 +16,18 @@ file windows-certificate.pfx
 openssl pkcs12 -in windows-certificate.pfx -nocerts -nodes -out private-key.pem  -passin pass:${GITHUB_CERT_PASSWORD} || echo "no bueno 1"
 openssl pkcs12 -in windows-certificate.pfx -nokeys -nodes -out certificate.pem -passin pass:${GITHUB_CERT_PASSWORD} || echo "no bueno 2"
 
+foo="$(date +%s)"
+
 osslsigncode sign \
   -certs certificate.pem \
   -key private-key.pem \
   -n "GitHub CLI" \
   -t http://timestamp.digicert.com \
   -in $EXECUTABLE_PATH \
-  -out gh_signed.exe || echo "no bueno 3"
+  -out gh_signed-${foo}.exe || echo "no bueno 3"
 
-stat gh_signed.exe
-file gh_signed.exe
+stat gh_signed-${foo}.exe
+file gh_signed-${foo}.exe
 
 # Oddly, there can be a delay before the file is *actually* available - wait for it
 sleep 10
@@ -33,4 +35,4 @@ sleep 10
 # seconds isn't ideal but it's better than 3 minutes.
 #while [ ! -f gh_signed.exe ]; do sleep 1; done;
 
-mv gh_signed.exe $EXECUTABLE_PATH
+mv gh_signed-{foo}.exe $EXECUTABLE_PATH
