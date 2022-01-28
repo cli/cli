8178016c902ea294000d7ef6a58945ca874b8283
Audree Steinberg <audreee@github.com>
2022-01-28 13:21:15 -0800

    Add new line

--- cmd/gh/main.go
+++ cmd/gh/main.go
@@ -157,7 +157,7 @@ func mainRun() exitCode {
 				if errors.As(err, &execError) {
 					return exitCode(execError.ExitCode())
 				}
-				fmt.Fprintf(stderr, "failed to run extension: %s", err)
+				fmt.Fprintf(stderr, "failed to run extension: %s", err, "\n")
 				return exitError
 			} else if found {
 				return exitOK
