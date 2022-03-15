f94a1a2bd49b7e6c7b747032b43214f45a4c7d45
Charlie Andrews <cwndrws@github.com>
2022-03-15 17:12:16 -0400

    Add test for disallowing opening vs code for codespace with pending op
    I also refactored the existing vs code test a bit to work with the new
    use of `getOrChooseCodespace`.

--- pkg/cmd/codespace/code_test.go
+++ pkg/cmd/codespace/code_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"testing"
 
+	"github.com/cli/cli/v2/internal/codespaces/api"
 	"github.com/cli/cli/v2/pkg/cmdutil"
+	"github.com/cli/cli/v2/pkg/iostreams"
 )
 
 func TestApp_VSCode(t *testing.T) {
@@ -41,12 +43,56 @@ func TestApp_VSCode(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			b := &cmdutil.TestBrowser{}
 			a := &App{
-				browser: b,
+				browser:   b,
+				apiClient: testCodeApiMock(),
 			}
 			if err := a.VSCode(context.Background(), tt.args.codespaceName, tt.args.useInsiders); (err != nil) != tt.wantErr {
 				t.Errorf("App.VSCode() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			b.Verify(t, tt.wantURL)
 		})
+	}
+}
+
+func TestPendingOperationDisallowsCode(t *testing.T) {
+	app := testingLogsApp()
+
+	if err := app.VSCode(context.Background(), "disabledCodespace", false); err != nil {
+		if err.Error() != "codespace is disabled while it has a pending operation: Some pending operation" {
+			t.Errorf("expected pending operation error, but got: %v", err)
+		}
+	} else {
+		t.Error("expected pending operation error, but got nothing")
+	}
+}
+
+func testingCodeApp() *App {
+	io, _, _, _ := iostreams.Test()
+	return NewApp(io, nil, testCodeApiMock(), nil)
+}
+
+func testCodeApiMock() *apiClientMock {
+	user := &api.User{Login: "monalisa"}
+	testingCodespace := &api.Codespace{
+		Name: "monalisa-cli-cli-abcdef",
+	}
+	disabledCodespace := &api.Codespace{
+		Name:                           "disabledCodespace",
+		PendingOperation:               true,
+		PendingOperationDisabledReason: "Some pending operation",
+	}
+	return &apiClientMock{
+		GetCodespaceFunc: func(_ context.Context, name string, _ bool) (*api.Codespace, error) {
+			if name == "disabledCodespace" {
+				return disabledCodespace, nil
+			}
+			return testingCodespace, nil
+		},
+		GetUserFunc: func(_ context.Context) (*api.User, error) {
+			return user, nil
+		},
+		AuthorizedKeysFunc: func(_ context.Context, _ string) ([]byte, error) {
+			return []byte{}, nil
+		},
 	}
 }
