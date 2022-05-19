705cb80f4ed214c0967f2a4b500cbc46adf8305e
Ariel Deitcher <mntlty@github.com>
2022-05-19 07:43:04 -0700

    update help text

--- pkg/cmd/pr/merge/merge.go
+++ pkg/cmd/pr/merge/merge.go
@@ -74,6 +74,11 @@ func NewCmdMerge(f *cmdutil.Factory, runF func(*MergeOptions) error) *cobra.Comm
 
 			Without an argument, the pull request that belongs to the current branch
 			is selected.
+
+			When targeting a branch that requires a merge queue, no merge strategy is required.
+			If required checks have not yet passed, AutoMerge will be enabled.
+			If required checks have passed, the pull request will be added to the merge queue.
+			To bypass a merge queue and merge directly, pass the '--admin' flag.
     	`),
 		Args: cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
