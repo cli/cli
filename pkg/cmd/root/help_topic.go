621cd4153e5ccb8ba0ec95a43c22078655acd748
Matthew Runo <74583+inktomi@users.noreply.github.com>
2022-08-15 15:25:39 -0700

    Fixes #6024.
    Adding documentation for exit codes used by 'gh'

--- pkg/cmd/root/help_topic.go
+++ pkg/cmd/root/help_topic.go
@@ -138,6 +138,10 @@ var HelpTopics = map[string]map[string]string{
 			
 			- If a command fails for any reason, the exit code will be 1
 
+			- If a command is running but gets cancelled, the exit code will be 2
+
+			- If a command encounters an authentication issue, the exit code will be 4
+
 			NOTE: It is possible that a particular command may have more exit codes, so it is a good 
 			practice to check documentation for the command if you are relying on exit codes to 
 			control some behavior. 
