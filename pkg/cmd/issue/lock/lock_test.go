7a5ed081858e59ad98c71e4c80f82ca91845bab2
chemotaxis <chemotaxis@users.noreply.github.com>
2022-08-23 23:24:42 -0400

    Test reasonsMap construction
    Make sure reasons were added in the correct order.

--- pkg/cmd/issue/lock/lock_test.go
+++ pkg/cmd/issue/lock/lock_test.go
@@ -1,1 +1,16 @@
 package lock
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReasons(t *testing.T) {
+	assert.Equal(t, len(reasons), len(reasonsApi))
+
+	for _, reason := range reasons {
+		assert.Equal(t, strings.ToUpper(reason), string(*reasonsMap[reason]))
+	}
+}
