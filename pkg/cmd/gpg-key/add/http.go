b8a77b57fbc8e9e18597062a58acef55e07cd067
BakaFT <bakaft@outlook.com>
2022-10-28 19:40:39 +0800

    More user-friendly error message on `gh gpg-key add`

--- pkg/cmd/gpg-key/add/http.go
+++ pkg/cmd/gpg-key/add/http.go
@@ -49,6 +49,15 @@ func gpgKeyUpload(httpClient *http.Client, hostname string, keyFile io.Reader) e
 		if errors.As(err, &httpError) && isDuplicateError(&httpError) {
 			return nil
 		}
+		if errors.As(err, &httpError) && isKeyInvalidError(&httpError) {
+			if !isGpgKeyArmored(keyBytes) {
+				return errors.New("it seems that the GPG key is not armored.\n" +
+					"please try to find your GPG key ID using:\n" +
+					"\tgpg --list-keys\n" +
+					"and use command below to add it to your accont:\n" +
+					"\tgpg --armor --export <GPG key ID> | gh gpg-key add -")
+			}
+		}
 		return err
 	}
 
@@ -63,4 +72,15 @@ func gpgKeyUpload(httpClient *http.Client, hostname string, keyFile io.Reader) e
 func isDuplicateError(err *api.HTTPError) bool {
 	return err.StatusCode == 422 && len(err.Errors) == 2 &&
 		err.Errors[0].Field == "key_id" && err.Errors[0].Message == "key_id already exists"
+}
+
+func isKeyInvalidError(err *api.HTTPError) bool {
+	return err.StatusCode == 422 && len(err.Errors) == 1 &&
+		err.Errors[0].Field == "" && err.Errors[0].Message == "We got an error doing that."
+}
+
+func isGpgKeyArmored(keyBytes []byte) bool {
+	buf := make([]byte, 36)
+	copy(buf, keyBytes)
+	return bytes.Equal(buf, []byte("-----BEGIN PGP PUBLIC KEY BLOCK-----"))
 }
