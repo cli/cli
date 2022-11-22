6e169615e2a40a0c2a9336272eace369b3cac995
Paul Forness <50929905+ptforness@users.noreply.github.com>
2022-11-21 20:24:47 -0500

    Undo accidental line removal

--- pkg/cmd/alias/list/list_test.go
+++ pkg/cmd/alias/list/list_test.go
@@ -45,6 +45,7 @@ func TestAliasList(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			cfg := config.NewFromString(tt.config)
+
 			ios, _, stdout, stderr := iostreams.Test()
 			ios.SetStdoutTTY(tt.isTTY)
 			ios.SetStdinTTY(tt.isTTY)
