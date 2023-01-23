875b68d8138e36874c5e1cbebf9880adad2c04be
Anupam Kumar <atirikt@github.com>
2023-01-23 05:52:06 +0000

    shared/root level changes  gh cli for variables

---
+++ pkg/cmd/variable/shared/shared.go
@@ -0,0 +1,555 @@
+package shared
+
+import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/cli/cli/v2/api"
+	"github.com/cli/cli/v2/internal/config"
+	"github.com/cli/cli/v2/internal/ghinstance"
+	"github.com/cli/cli/v2/internal/ghrepo"
+	"github.com/cli/cli/v2/internal/text"
+	"github.com/cli/cli/v2/pkg/iostreams"
+)
+
+type Visibility string
+
+const (
+	All      = "all"
+	Private  = "private"
+	Selected = "selected"
+)
+
+type App string
+
+const (
+	Actions = "actions"
+	Unknown = "unknown"
+)
+
+func (app App) String() string {
+	return string(app)
+}
+
+func (app App) Title() string {
+	return text.Title(app.String())
+}
+
+type VariableEntity string
+
+const (
+	Repository   = "repository"
+	Organization = "organization"
+	Environment  = "environment"
+)
+
+type VariablePayload struct {
+	Name         string  `json:"name,omitempty"`
+	Value        string  `json:"value,omitempty"`
+	Visibility   string  `json:"visibility,omitempty"`
+	Repositories []int64 `json:"selected_repository_ids,omitempty"`
+	KeyID        string  `json:"key_id"`
+}
+
+type PubKey struct {
+	ID  string `json:"key_id"`
+	Key string
+}
+
+type repoNamesResult struct {
+	Ids []int64
+	Err error
+}
+
+type PostPatchOptions struct {
+	HttpClient func() (*http.Client, error)
+	IO         *iostreams.IOStreams
+	Config     func() (config.Config, error)
+	BaseRepo   func() (ghrepo.Interface, error)
+
+	RandomOverride func() io.Reader
+
+	VariableName    string
+	OrgName         string
+	EnvName         string
+	Body            string
+	Visibility      string
+	RepositoryNames []string
+	CsvFile         string
+	Application     string
+	Prompter        iprompter
+}
+
+type ListOptions struct {
+	HttpClient func() (*http.Client, error)
+	IO         *iostreams.IOStreams
+	Config     func() (config.Config, error)
+	BaseRepo   func() (ghrepo.Interface, error)
+
+	OrgName     string
+	EnvName     string
+	Application string
+	Page        int
+	PerPage     int
+	Name        string
+}
+
+type Variable struct {
+	Name             string     `json:"name"`
+	Value            string     `json:"value"`
+	UpdatedAt        time.Time  `json:"updated_at"`
+	Visibility       Visibility `json:"visibility"`
+	SelectedReposURL string     `json:"selected_repositories_url"`
+	NumSelectedRepos int
+}
+type VariablesPayload struct {
+	Variables []*Variable
+}
+
+type Repo struct {
+	Name string `json: "name"`
+}
+
+type SelectedRepos struct {
+	Repositories []*Repo
+}
+
+type httpClient interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+type iprompter interface {
+	Input(string, string) (string, error)
+	Select(string, string, []string) (int, error)
+	Confirm(string, bool) (bool, error)
+}
+
+func GetVariableEntity(orgName, envName string) (VariableEntity, error) {
+	orgSet := orgName != ""
+	envSet := envName != ""
+
+	if orgSet && envSet {
+		return "", errors.New("cannot specify multiple variable entities")
+	}
+
+	if orgSet {
+		return Organization, nil
+	}
+	if envSet {
+		return Environment, nil
+	}
+	return Repository, nil
+}
+
+func GetVariableApp(app string, entity VariableEntity) (App, error) {
+	switch strings.ToLower(app) {
+	case Actions:
+		return Actions, nil
+	default:
+		return Unknown, fmt.Errorf("invalid application: %s", app)
+	}
+}
+
+func IsSupportedVariableEntity(app App, entity VariableEntity) bool {
+	switch app {
+	case Actions:
+		return entity == Repository || entity == Organization || entity == Environment
+	default:
+		return false
+	}
+}
+
+// This does similar logic to `api.RepoNetwork`, but without the overfetching.
+func MapRepoToID(client *api.Client, host string, repositories []ghrepo.Interface) ([]int64, error) {
+	queries := make([]string, 0, len(repositories))
+	for i, repo := range repositories {
+		queries = append(queries, fmt.Sprintf(`
+			repo_%03d: repository(owner: %q, name: %q) {
+				databaseId
+			}
+		`, i, repo.RepoOwner(), repo.RepoName()))
+	}
+
+	query := fmt.Sprintf(`query MapRepositoryNames { %s }`, strings.Join(queries, ""))
+
+	graphqlResult := make(map[string]*struct {
+		DatabaseID int64 `json:"databaseId"`
+	})
+
+	if err := client.GraphQL(host, query, nil, &graphqlResult); err != nil {
+		return nil, fmt.Errorf("failed to look up repositories: %w", err)
+	}
+
+	repoKeys := make([]string, 0, len(repositories))
+	for k := range graphqlResult {
+		repoKeys = append(repoKeys, k)
+	}
+	sort.Strings(repoKeys)
+
+	result := make([]int64, len(repositories))
+	for i, k := range repoKeys {
+		result[i] = graphqlResult[k].DatabaseID
+	}
+	return result, nil
+}
+
+func getCurrentSelectedRepos(client httpClient, url string) string {
+	var selectedRepositories SelectedRepos
+	err := apiGet(client, url, &selectedRepositories)
+	if err != nil {
+		return ""
+	}
+	names := make([]string, 0)
+	for _, repo := range selectedRepositories.Repositories {
+		names = append(names, repo.Name)
+	}
+	return strings.Join(names, ",")
+}
+
+func MapRepoNamesToIDs(client *api.Client, host, defaultOwner string, repositoryNames []string) ([]int64, error) {
+	repos := make([]ghrepo.Interface, 0, len(repositoryNames))
+	for _, repositoryName := range repositoryNames {
+		var repo ghrepo.Interface
+		if strings.Contains(repositoryName, "/") || defaultOwner == "" {
+			var err error
+			repo, err = ghrepo.FromFullNameWithHost(repositoryName, host)
+			if err != nil {
+				return nil, fmt.Errorf("invalid repository name")
+			}
+		} else {
+			repo = ghrepo.NewWithHost(defaultOwner, repositoryName, host)
+		}
+		repos = append(repos, repo)
+	}
+	repositoryIDs, err := MapRepoToID(client, host, repos)
+	if err != nil {
+		return nil, fmt.Errorf("failed to look up IDs for repositories %v: %w", repositoryNames, err)
+	}
+	return repositoryIDs, nil
+}
+
+func GetRepoIds(client *api.Client, host, orgName string, repoNames []string) repoNamesResult {
+	repoNamesC := make(chan repoNamesResult, 1)
+	go func() {
+		if len(repoNames) == 0 {
+			repoNamesC <- repoNamesResult{}
+			return
+		}
+		repositoryIDs, err := MapRepoNamesToIDs(client, host, orgName, repoNames)
+		repoNamesC <- repoNamesResult{
+			Ids: repositoryIDs,
+			Err: err,
+		}
+	}()
+	return <-repoNamesC
+}
+
+func GetVariablesFromOptions(opts *PostPatchOptions, client *api.Client, host string, isUpdate bool) (map[string]VariablePayload, error) {
+	variables := make(map[string]VariablePayload)
+	if opts.CsvFile != "" {
+		var r io.Reader
+		if opts.CsvFile == "-" {
+			defer opts.IO.In.Close()
+			r = opts.IO.In
+		} else {
+			f, err := os.Open(opts.CsvFile)
+			if err != nil {
+				return nil, fmt.Errorf("failed to open env file: %w", err)
+			}
+			defer f.Close()
+			r = f
+		}
+		csvReader := csv.NewReader(r)
+		csvReader.Comment = '#'
+		csvReader.FieldsPerRecord = -1
+		records, err := csvReader.ReadAll()
+		if err != nil {
+			return nil, fmt.Errorf("error parsing csv file: %w", err)
+		}
+		if len(records) == 0 {
+			return nil, fmt.Errorf("no variables found in file")
+		}
+		for _, row := range records {
+			variable, err := getVarFromRow(opts, client, host, row, isUpdate)
+			if err != nil {
+				return nil, err
+			}
+			variables[row[0]] = variable
+		}
+		return variables, nil
+	}
+
+	values, err := getBody(opts, client, host, isUpdate)
+	if err != nil {
+		return nil, fmt.Errorf("did not understand variable body: %w", err)
+	}
+	variables[opts.VariableName] = values
+	return variables, nil
+}
+
+func getVarFromRow(opts *PostPatchOptions, client *api.Client, host string, row []string, isUpdate bool) (VariablePayload, error) {
+	rowLength := len(row)
+	index := 1
+	if rowLength < 2 {
+		return VariablePayload{}, fmt.Errorf("less than 2 records in a row in file")
+	}
+	if rowLength > 3 && opts.OrgName == "" {
+		return VariablePayload{}, fmt.Errorf("more than 3 vals in a row in file for non org variable %s", row[0])
+	}
+
+	var variable VariablePayload
+	variable.Value = row[index]
+	index++
+	if isUpdate {
+		if rowLength >= 3 && len(row[index]) > 0 {
+			variable.Name = row[index]
+		} else {
+			variable.Name = row[0]
+		}
+		index++
+	} else {
+		variable.Name = row[0]
+	}
+	if opts.OrgName != "" {
+		if !isUpdate && rowLength >= 3 {
+			variable.Visibility = row[index]
+		} else if rowLength >= 4 {
+			variable.Visibility = row[index]
+		}
+		index++
+		if variable.Visibility == Selected {
+			if rowLength < 5 {
+				// do not exit here as repositoryNames in opts may have the info.
+				log.Printf("selected visibility with no repos mentioned for variable %s", row[0])
+			}
+			repoIdRes := GetRepoIds(client, host, opts.OrgName, row[index:])
+			if repoIdRes.Err != nil {
+				return VariablePayload{}, repoIdRes.Err
+			}
+			variable.Repositories = repoIdRes.Ids
+		}
+	}
+	return variable, nil
+}
+
+func getBody(opts *PostPatchOptions, client *api.Client, host string, isUpdate bool) (VariablePayload, error) {
+	if opts.Body != "" {
+		return getVarFromRow(opts, client, host, strings.Split(opts.VariableName+","+string(opts.Body), ","), isUpdate)
+	}
+
+	values := make([]string, 0)
+	if opts.IO.CanPrompt() {
+		var currentVar Variable
+		if isUpdate {
+			currentVariablePtr, err := GetVariablesForList(&ListOptions{HttpClient: opts.HttpClient,
+				IO:          opts.IO,
+				Config:      opts.Config,
+				BaseRepo:    opts.BaseRepo,
+				OrgName:     opts.OrgName,
+				EnvName:     opts.EnvName,
+				Application: opts.Application,
+				Page:        0,
+				PerPage:     0,
+				Name:        opts.VariableName,
+			}, true)
+			if err == nil && len(currentVariablePtr) > 0 {
+				currentVar = *currentVariablePtr[0]
+			}
+		}
+		values = append(values, opts.VariableName)
+		data, err := opts.Prompter.Input("Value", currentVar.Value)
+		if err != nil {
+			return VariablePayload{}, err
+		}
+		values = append(values, data)
+		if isUpdate {
+			data, err = opts.Prompter.Input("New name", currentVar.Name)
+			if err != nil {
+				return VariablePayload{}, err
+			}
+			values = append(values, data)
+		}
+
+		if opts.OrgName != "" {
+			data, err = opts.Prompter.Input("Visibility", string(currentVar.Visibility))
+			if err != nil {
+				return VariablePayload{}, err
+			}
+			values = append(values, data)
+
+			if data == Selected {
+				data, err = opts.Prompter.Input("Repos(comma separated)", getCurrentSelectedRepos(client.HTTP(), currentVar.SelectedReposURL))
+				if err != nil {
+					return VariablePayload{}, err
+				}
+				values = append(values, strings.Split(data, ",")...)
+			}
+		}
+
+		fmt.Fprintln(opts.IO.Out)
+		return getVarFromRow(opts, client, host, values, isUpdate)
+	}
+
+	body, err := io.ReadAll(opts.IO.In)
+	if err != nil {
+		return VariablePayload{}, fmt.Errorf("failed to read from standard input: %w", err)
+	}
+
+	return getVarFromRow(opts, client, host, strings.Split(opts.VariableName+","+string(bytes.TrimRight(body, "\r\n")), ","), isUpdate)
+}
+
+func GetVariablesForList(opts *ListOptions, getSelectedRepoInfo bool) ([]*Variable, error) {
+	client, err := opts.HttpClient()
+	if err != nil {
+		return nil, fmt.Errorf("could not create http client: %w", err)
+	}
+
+	orgName := opts.OrgName
+	envName := opts.EnvName
+
+	var baseRepo ghrepo.Interface
+	if orgName == "" {
+		baseRepo, err = opts.BaseRepo()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	variableEntity, err := GetVariableEntity(orgName, envName)
+	if err != nil {
+		return nil, err
+	}
+
+	variableApp, err := GetVariableApp(opts.Application, variableEntity)
+	if err != nil || variableApp == Unknown {
+		return nil, err
+	}
+
+	if !IsSupportedVariableEntity(variableApp, variableEntity) {
+		return nil, fmt.Errorf("%s variables are not supported for %s", variableEntity, variableApp)
+	}
+
+	var variables []*Variable
+
+	switch variableEntity {
+	case Repository:
+		variables, err = getRepoVariables(client, baseRepo, variableApp, opts.Page, opts.PerPage, opts.Name)
+	case Environment:
+		variables, err = getEnvVariables(client, baseRepo, envName, opts.Page, opts.PerPage, opts.Name)
+	case Organization:
+		var cfg config.Config
+		var host string
+
+		cfg, err = opts.Config()
+		if err != nil {
+			return nil, err
+		}
+
+		host, _ = cfg.DefaultHost()
+		variables, err = getOrgVariables(client, host, orgName, getSelectedRepoInfo, variableApp, opts.Page, opts.PerPage, opts.Name)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get variables: %w", err)
+	}
+	return variables, nil
+}
+
+func getOrgVariables(client httpClient, host, orgName string, getSelectedRepoInfo bool, app App, page, perPage int, name string) ([]*Variable, error) {
+	variables, err := getVariables(client, host, fmt.Sprintf(`orgs/%s/%s/variables`, orgName, app), page, perPage, name)
+	if err != nil {
+		return nil, err
+	}
+
+	if getSelectedRepoInfo {
+		err = getSelectedRepositoryInformation(client, variables)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return variables, nil
+}
+
+func getEnvVariables(client httpClient, repo ghrepo.Interface, envName string, page, perPage int, name string) ([]*Variable, error) {
+	path := fmt.Sprintf(`repos/%s/environments/%s/variables`, ghrepo.FullName(repo), envName)
+	return getVariables(client, repo.RepoHost(), path, page, perPage, name)
+}
+
+func getRepoVariables(client httpClient, repo ghrepo.Interface, app App, page, perPage int, name string) ([]*Variable, error) {
+	return getVariables(client, repo.RepoHost(), fmt.Sprintf(`repos/%s/%s/variables`,
+		ghrepo.FullName(repo), app), page, perPage, name)
+}
+
+func getVariables(client httpClient, host, path string, page, perPage int, name string) ([]*Variable, error) {
+	var url string
+	if name != "" {
+		url = fmt.Sprintf("%s%s/%s", ghinstance.RESTPrefix(host), path, name)
+		var payload Variable
+		err := apiGet(client, url, &payload)
+		if err != nil {
+			return nil, err
+		}
+		return []*Variable{&payload}, nil
+	} else {
+		url = fmt.Sprintf("%s%s?page=%d&per_page=%d", ghinstance.RESTPrefix(host), path, page, perPage)
+		var payload VariablesPayload
+		err := apiGet(client, url, &payload)
+		if err != nil {
+			return nil, err
+		}
+		return payload.Variables, nil
+	}
+}
+
+func apiGet(client httpClient, url string, data interface{}) error {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 299 {
+		return api.HandleHTTPError(resp)
+	}
+
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getSelectedRepositoryInformation(client httpClient, variables []*Variable) error {
+	type responseData struct {
+		TotalCount int `json:"total_count"`
+	}
+
+	for _, variable := range variables {
+		if variable.SelectedReposURL == "" {
+			continue
+		}
+		var result responseData
+		if err := apiGet(client, variable.SelectedReposURL, &result); err != nil {
+			return fmt.Errorf("failed determining selected repositories for %s: %w", variable.Name, err)
+		}
+		variable.NumSelectedRepos = result.TotalCount
+	}
+
+	return nil
+}
---
+++ pkg/cmd/variable/shared/shared_test.go
@@ -0,0 +1,371 @@
+package shared
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/MakeNowJust/heredoc"
+	"github.com/cli/cli/v2/api"
+	"github.com/cli/cli/v2/internal/config"
+	"github.com/cli/cli/v2/internal/ghrepo"
+	"github.com/cli/cli/v2/pkg/httpmock"
+	"github.com/cli/cli/v2/pkg/iostreams"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_getBodyPrompt(t *testing.T) {
+	ios, _, _, _ := iostreams.Test()
+	ios.SetStdinTTY(true)
+	ios.SetStdoutTTY(true)
+
+	reg := &httpmock.Registry{}
+	defer reg.Verify(t)
+
+	opts := &PostPatchOptions{
+		HttpClient: func() (*http.Client, error) {
+			return &http.Client{Transport: reg}, nil
+		},
+		Config: func() (config.Config, error) {
+			return config.NewBlankConfig(), nil
+		},
+		BaseRepo: func() (ghrepo.Interface, error) {
+			return ghrepo.FromFullName("owner/repo")
+		},
+		IO:             ios,
+		Body:           "a variable",
+		RandomOverride: fakeRandom,
+	}
+	httpClient, _ := opts.HttpClient()
+	apiClient := api.NewClientFromHTTP(httpClient)
+
+	body, err := getBody(opts, apiClient, "owner/repo", false)
+	assert.NoError(t, err)
+	assert.Equal(t, body.Value, "a variable")
+}
+
+func TestGetVariableEntity(t *testing.T) {
+	tests := []struct {
+		name    string
+		orgName string
+		envName string
+		want    VariableEntity
+		wantErr bool
+	}{
+		{
+			name:    "org",
+			orgName: "myOrg",
+			want:    Organization,
+		},
+		{
+			name:    "env",
+			envName: "myEnv",
+			want:    Environment,
+		},
+		{
+			name: "defaults to repo",
+			want: Repository,
+		},
+		{
+			name:    "Errors if both org and env are set",
+			orgName: "myOrg",
+			envName: "myEnv",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			entity, err := GetVariableEntity(tt.orgName, tt.envName)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, entity)
+			}
+		})
+	}
+}
+
+func TestGetVariableApp(t *testing.T) {
+	tests := []struct {
+		name    string
+		app     string
+		entity  VariableEntity
+		want    App
+		wantErr bool
+	}{
+		{
+			name: "Actions",
+			app:  "actions",
+			want: Actions,
+		},
+		{
+			name:   "Defaults to error for repository",
+			app:    "",
+			entity: Repository,
+			wantErr: true,
+		},
+		{
+			name:   "Defaults to error for organization",
+			app:    "",
+			entity: Organization,
+			wantErr: true,
+		},
+		{
+			name:   "Defaults to error for environment",
+			app:    "",
+			entity: Environment,
+			wantErr: true,
+		},
+		{
+			name:    "Unknown for invalid apps",
+			app:     "invalid",
+			want:    Unknown,
+			wantErr: true,
+		},
+		{
+			name: "case insensitive",
+			app:  "ACTIONS",
+			want: Actions,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			app, err := GetVariableApp(tt.app, tt.entity)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, tt.want, app)
+		})
+	}
+}
+
+func TestIsSupportedVariableEntity(t *testing.T) {
+	tests := []struct {
+		name                string
+		app                 App
+		supportedEntities   []VariableEntity
+		unsupportedEntities []VariableEntity
+	}{
+		{
+			name: "Actions",
+			app:  Actions,
+			supportedEntities: []VariableEntity{
+				Repository,
+				Organization,
+				Environment,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, entity := range tt.supportedEntities {
+				assert.True(t, IsSupportedVariableEntity(tt.app, entity))
+			}
+
+			for _, entity := range tt.unsupportedEntities {
+				assert.False(t, IsSupportedVariableEntity(tt.app, entity))
+			}
+		})
+	}
+}
+
+func Test_getBody(t *testing.T) {
+	tests := []struct {
+		name    string
+		bodyArg string
+		want    string
+		stdin   string
+	}{
+		{
+			name:    "literal value",
+			bodyArg: "a variable",
+			want:    "a variable",
+		},
+		{
+			name:  "from stdin",
+			want:  "a variable",
+			stdin: "a variable",
+		},
+		{
+			name:  "from stdin with trailing newline character",
+			want:  "a variable",
+			stdin: "a variable\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ios, stdin, _, _ := iostreams.Test()
+
+			ios.SetStdinTTY(false)
+
+			_, err := stdin.WriteString(tt.stdin)
+			assert.NoError(t, err)
+			reg := &httpmock.Registry{}
+			defer reg.Verify(t)
+
+			opts := &PostPatchOptions{
+				HttpClient: func() (*http.Client, error) {
+					return &http.Client{Transport: reg}, nil
+				},
+				Config: func() (config.Config, error) {
+					return config.NewBlankConfig(), nil
+				},
+				BaseRepo: func() (ghrepo.Interface, error) {
+					return ghrepo.FromFullName("owner/repo")
+				},
+				IO:             ios,
+				VariableName:   "VARNAME",
+				Body:           "a variable",
+				RandomOverride: fakeRandom,
+			}
+			httpClient, _ := opts.HttpClient()
+			apiClient := api.NewClientFromHTTP(httpClient)
+
+			body, err := getBody(opts, apiClient, "owner/repo", false)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.want, body.Value)
+		})
+	}
+}
+
+func Test_getVariablesFromOptions(t *testing.T) {
+	genFile := func(s string) string {
+		f, err := os.CreateTemp("", "gh-env.*")
+		if err != nil {
+			t.Fatal(err)
+			return ""
+		}
+		defer f.Close()
+		t.Cleanup(func() {
+			_ = os.Remove(f.Name())
+		})
+		_, err = f.WriteString(s)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return f.Name()
+	}
+
+	tests := []struct {
+		name    string
+		opts    PostPatchOptions
+		isTTY   bool
+		stdin   string
+		want    map[string]string
+		wantErr bool
+	}{
+		{
+			name: "variable from arg",
+			opts: PostPatchOptions{
+				VariableName: "FOO",
+				Body:         "bar",
+				CsvFile:      "",
+			},
+			want: map[string]string{"FOO": "bar"},
+		},
+		{
+			name: "variables from stdin",
+			opts: PostPatchOptions{
+				Body:    "",
+				CsvFile: "-",
+			},
+			stdin: `FOO,bar`,
+			want:  map[string]string{"FOO": "bar"},
+		},
+		{
+			name: "variables from file",
+			opts: PostPatchOptions{
+				Body: "",
+				CsvFile: genFile(heredoc.Doc(`
+					FOO,bar
+					QUOTED,my value
+					#IGNORED, true
+				`)),
+			},
+			stdin: `FOO=bar`,
+			want: map[string]string{
+				"FOO":    "bar",
+				"QUOTED": "my value",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ios, stdin, _, _ := iostreams.Test()
+			ios.SetStdinTTY(tt.isTTY)
+			ios.SetStdoutTTY(tt.isTTY)
+			stdin.WriteString(tt.stdin)
+			opts := tt.opts
+			opts.IO = ios
+			reg := &httpmock.Registry{}
+			defer reg.Verify(t)
+
+			apiClient := api.NewClientFromHTTP(&http.Client{Transport: reg})
+
+			gotVariables, err := GetVariablesFromOptions(&opts, apiClient, "owner/repo", false)
+			if err != nil {
+				if !tt.wantErr {
+					t.Fatalf("getVariablesFromOptions() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			} else if tt.wantErr {
+				t.Fatalf("getVariablesFromOptions() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if len(gotVariables) != len(tt.want) {
+				t.Fatalf("getVariablesFromOptions() = got %d variables, want %d", len(gotVariables), len(tt.want))
+			}
+			for k, v := range gotVariables {
+				if tt.want[k] != v.Value {
+					t.Errorf("getVariablesFromOptions() %s = got %q, want %q", k, v.Value, tt.want[k])
+				}
+			}
+		})
+	}
+}
+
+type testClient func(*http.Request) (*http.Response, error)
+
+func (c testClient) Do(req *http.Request) (*http.Response, error) {
+	return c(req)
+}
+
+func Test_getVariables_pagination(t *testing.T) {
+	var requests []*http.Request
+	var client testClient = func(req *http.Request) (*http.Response, error) {
+		header := make(map[string][]string)
+		if len(requests) == 0 {
+			header["Link"] = []string{}
+		}
+		requests = append(requests, req)
+		return &http.Response{
+			Request: req,
+			Body:    io.NopCloser(strings.NewReader(`{"variables":[{},{}]}`)),
+			Header:  header,
+		}, nil
+	}
+	page, perPage := 2, 25
+	variables, err := getVariables(client, "github.com", "path/to", page, perPage, "")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(requests))
+	assert.Equal(t, 2, len(variables))
+	assert.Equal(t, fmt.Sprintf("https://api.github.com/path/to?page=%d&per_page=%d", page, perPage), requests[0].URL.String())
+}
+
+func fakeRandom() io.Reader {
+	return bytes.NewReader(bytes.Repeat([]byte{5}, 32))
+}
+
