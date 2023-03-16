c347d2e22a579a923e0ee0e15cfde963f7a876fa
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2023-03-16 12:14:49 +0100

    comment

--- internal/config/config.go
+++ internal/config/config.go
@@ -140,6 +140,7 @@ func (c *AuthConfig) Token(hostname string) (string, string) {
 	return token, source
 }
 
+// HasEnterpriseEnvToken checks whether the current env or config contains an enterprise token
 func (c *AuthConfig) HasEnterpriseEnvToken() bool {
 	token, source := ghAuth.TokenFromEnvOrConfig("")
 	isEnterpriseSource := (source == "GH_ENTERPRISE_TOKEN" || source == "GITHUB_ENTERPRISE_TOKEN")
