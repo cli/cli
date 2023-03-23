39805fa9b1a5c04eb3c88a1c3bf89f28657d1d33
Sam Coe <samcoe@users.noreply.github.com>
2023-03-23 12:17:47 +1100

    Properly handle closing files that have been writen to (#7199)

--- pkg/cmd/run/download/zip_test.go
+++ pkg/cmd/run/download/zip_test.go
@@ -11,23 +11,16 @@ import (
 
 func Test_extractZip(t *testing.T) {
 	tmpDir := t.TempDir()
-	wd, err := os.Getwd()
-	require.NoError(t, err)
-	t.Cleanup(func() { _ = os.Chdir(wd) })
+	extractPath := filepath.Join(tmpDir, "artifact")
 
 	zipFile, err := zip.OpenReader("./fixtures/myproject.zip")
 	require.NoError(t, err)
 	defer zipFile.Close()
 
-	extractPath := filepath.Join(tmpDir, "artifact")
-	err = os.MkdirAll(extractPath, 0700)
-	require.NoError(t, err)
-	require.NoError(t, os.Chdir(extractPath))
-
-	err = extractZip(&zipFile.Reader, ".")
+	err = extractZip(&zipFile.Reader, extractPath)
 	require.NoError(t, err)
 
-	_, err = os.Stat(filepath.Join("src", "main.go"))
+	_, err = os.Stat(filepath.Join(extractPath, "src", "main.go"))
 	require.NoError(t, err)
 }
 
