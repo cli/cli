d7a4a747c87746e55dfccbbe48e324480312c43a
Kevin Lee <klee03509@gmail.com>
2023-04-10 23:39:40 -0700

    Refactor unit tests to test setup and run functions separately

--- pkg/cmd/alias/imports/import_test.go
+++ pkg/cmd/alias/imports/import_test.go
@@ -2,6 +2,7 @@ package imports
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -10,48 +11,128 @@ import (
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/cli/cli/v2/internal/config"
+	"github.com/cli/cli/v2/pkg/cmd/alias/shared"
 	"github.com/cli/cli/v2/pkg/cmdutil"
 	"github.com/cli/cli/v2/pkg/extensions"
 	"github.com/cli/cli/v2/pkg/iostreams"
-	"github.com/cli/cli/v2/test"
 	"github.com/google/shlex"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestAliasImport(t *testing.T) {
+func TestNewCmdImport(t *testing.T) {
 	tests := []struct {
-		name             string
-		isTTY            bool
-		flags            []string
-		stdin            string
-		fileContents     string
-		initialConfig    string
-		expectedConfig   string
-		expectedOutLines []string
-		expectedErrLines []string
+		name       string
+		cli        string
+		wants      ImportOptions
+		wantsError string
 	}{
 		{
-			name:  "with no existing aliases",
-			isTTY: true,
-			flags: []string{},
+			name: "no filename",
+			cli:  "",
+			wants: ImportOptions{
+				Filename:          "",
+				OverwriteExisting: false,
+			},
+			wantsError: "no filename passed and nothing on STDIN",
+		},
+		{
+			name: "multiple filenames",
+			cli:  "aliases1 aliases2",
+			wants: ImportOptions{
+				Filename:          "aliases1 aliases2",
+				OverwriteExisting: false,
+			},
+			wantsError: "too many arguments",
+		},
+		{
+			name: "with flags",
+			cli:  "aliases --clobber",
+			wants: ImportOptions{
+				Filename:          "aliases",
+				OverwriteExisting: true,
+			},
+		},
+		{
+			name: "from stdin",
+			cli:  "-",
+			wants: ImportOptions{
+				Filename:          "-",
+				OverwriteExisting: false,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ios, _, _, _ := iostreams.Test()
+			ios.SetStdinTTY(true)
+			f := &cmdutil.Factory{IOStreams: ios}
+
+			argv, err := shlex.Split(tt.cli)
+			assert.NoError(t, err)
+
+			var gotOpts *ImportOptions
+			cmd := NewCmdImport(f, func(opts *ImportOptions) error {
+				gotOpts = opts
+				return nil
+			})
+			cmd.SetArgs(argv)
+			cmd.SetIn(&bytes.Buffer{})
+			cmd.SetOut(io.Discard)
+			cmd.SetErr(io.Discard)
+
+			_, err = cmd.ExecuteC()
+			if tt.wantsError != "" {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.wants.Filename, gotOpts.Filename)
+			assert.Equal(t, tt.wants.OverwriteExisting, gotOpts.OverwriteExisting)
+		})
+	}
+}
+
+func Test_importRun(t *testing.T) {
+	tmpFile := filepath.Join(t.TempDir(), "aliases")
+	importFileMsg := fmt.Sprintf(`- Importing aliases from file "%s"`, tmpFile)
+	importStdinMsg := "- Importing aliases from standard input"
+
+	tests := []struct {
+		name         string
+		opts         *ImportOptions
+		stdin        string
+		fileContents string
+		initConfig   string
+		wantConfig   string
+		wantStderr   string
+	}{
+		{
+			name: "with no existing aliases",
+			opts: &ImportOptions{
+				Filename:          tmpFile,
+				OverwriteExisting: false,
+			},
 			fileContents: heredoc.Doc(`
                 co: pr checkout
                 igrep: '!gh issue list --label="$1" | grep "$2"'
             `),
-			expectedConfig: heredoc.Doc(`
+			wantConfig: heredoc.Doc(`
                 aliases:
                     co: pr checkout
                     igrep: '!gh issue list --label="$1" | grep "$2"'
             `),
-			expectedErrLines: []string{"Importing aliases from file", "Added alias (co|igrep)"},
-			expectedOutLines: []string{},
+			wantStderr: importFileMsg + "\n✓ Added alias co\n✓ Added alias igrep\n\n",
 		},
 		{
-			name:  "with existing aliases",
-			isTTY: true,
-			flags: []string{},
+			name: "with existing aliases",
+			opts: &ImportOptions{
+				Filename:          tmpFile,
+				OverwriteExisting: false,
+			},
 			fileContents: heredoc.Doc(`
                 users: |-
                     api graphql -F name="$1" -f query='
@@ -62,12 +143,12 @@ func TestAliasImport(t *testing.T) {
                         }'
                 co: pr checkout
             `),
-			initialConfig: heredoc.Doc(`
+			initConfig: heredoc.Doc(`
                 aliases:
                     igrep: '!gh issue list --label="$1" | grep "$2"'
                 editor: vim
             `),
-			expectedConfig: heredoc.Doc(`
+			wantConfig: heredoc.Doc(`
                 aliases:
                     igrep: '!gh issue list --label="$1" | grep "$2"'
                     co: pr checkout
@@ -80,13 +161,14 @@ func TestAliasImport(t *testing.T) {
                             }'
                 editor: vim
             `),
-			expectedErrLines: []string{"Importing aliases from file", "Added alias (co|users)"},
-			expectedOutLines: []string{},
+			wantStderr: importFileMsg + "\n✓ Added alias co\n✓ Added alias users\n\n",
 		},
 		{
-			name:  "from stdin",
-			isTTY: true,
-			flags: []string{},
+			name: "from stdin",
+			opts: &ImportOptions{
+				Filename:          "-",
+				OverwriteExisting: false,
+			},
 			stdin: heredoc.Doc(`
                 co: pr checkout
                 features: |-
@@ -94,7 +176,7 @@ func TestAliasImport(t *testing.T) {
                     --label=enhancement
                 igrep: '!gh issue list --label="$1" | grep "$2"'
             `),
-			expectedConfig: heredoc.Doc(`
+			wantConfig: heredoc.Doc(`
                 aliases:
                     co: pr checkout
                     features: |-
@@ -102,178 +184,152 @@ func TestAliasImport(t *testing.T) {
                         --label=enhancement
                     igrep: '!gh issue list --label="$1" | grep "$2"'
             `),
-			expectedErrLines: []string{"Importing aliases from standard input", "Added alias (co|igrep)"},
-			expectedOutLines: []string{},
+			wantStderr: importStdinMsg + "\n✓ Added alias co\n✓ Added alias features\n✓ Added alias igrep\n\n",
 		},
 		{
-			name:  "already taken aliases",
-			isTTY: true,
-			flags: []string{},
+			name: "already taken aliases",
+			opts: &ImportOptions{
+				Filename:          tmpFile,
+				OverwriteExisting: false,
+			},
 			fileContents: heredoc.Doc(`
                 co: pr checkout -R cool/repo
                 igrep: '!gh issue list --label="$1" | grep "$2"'
             `),
-			initialConfig: heredoc.Doc(`
+			initConfig: heredoc.Doc(`
                 aliases:
                     co: pr checkout
                 editor: vim
             `),
-			expectedConfig: heredoc.Doc(`
+			wantConfig: heredoc.Doc(`
                 aliases:
                     co: pr checkout
                     igrep: '!gh issue list --label="$1" | grep "$2"'
                 editor: vim
             `),
-			expectedErrLines: []string{
-				"Importing aliases from file",
-				"Could not import alias co: already taken",
-				"Added alias igrep",
-			},
-			expectedOutLines: []string{},
+			wantStderr: importFileMsg + "\nX Could not import alias co: name already taken\n✓ Added alias igrep\n\n",
 		},
 		{
-			name:  "override aliases",
-			isTTY: true,
-			flags: []string{"--clobber"},
+			name: "override aliases",
+			opts: &ImportOptions{
+				Filename:          tmpFile,
+				OverwriteExisting: true,
+			},
 			fileContents: heredoc.Doc(`
                 co: pr checkout -R cool/repo
                 igrep: '!gh issue list --label="$1" | grep "$2"'
             `),
-			initialConfig: heredoc.Doc(`
+			initConfig: heredoc.Doc(`
                 aliases:
                     co: pr checkout
                 editor: vim
             `),
-			expectedConfig: heredoc.Doc(`
+			wantConfig: heredoc.Doc(`
                 aliases:
                     co: pr checkout -R cool/repo
                     igrep: '!gh issue list --label="$1" | grep "$2"'
                 editor: vim
             `),
-			expectedErrLines: []string{
-				"Importing aliases from file",
-				"Changed alias co",
-				"Added alias igrep",
-			},
-			expectedOutLines: []string{},
+			wantStderr: importFileMsg + "\n! Changed alias co\n✓ Added alias igrep\n\n",
 		},
 		{
-			name:  "alias is a gh command",
-			isTTY: true,
-			flags: []string{},
+			name: "alias is a gh command",
+			opts: &ImportOptions{
+				Filename:          tmpFile,
+				OverwriteExisting: false,
+			},
 			fileContents: heredoc.Doc(`
                 pr: pr checkout
                 issue: issue list
                 api: api graphql
             `),
-			expectedErrLines: []string{"Could not import alias (pr|checkout|list): already a gh command"},
-			expectedOutLines: []string{},
+			wantStderr: strings.Join(
+				[]string{
+					importFileMsg,
+					"X Could not import alias api: already a gh command",
+					"X Could not import alias issue: already a gh command",
+					"X Could not import alias pr: already a gh command\n\n",
+				},
+				"\n",
+			),
 		},
 		{
-			name:  "invalid expansion",
-			isTTY: true,
-			flags: []string{},
+			name: "invalid expansion",
+			opts: &ImportOptions{
+				Filename:          tmpFile,
+				OverwriteExisting: false,
+			},
 			fileContents: heredoc.Doc(`
                 alias1:
                 alias2: ps checkout
             `),
-			expectedErrLines: []string{"Could not import alias alias[0-9]: expansion does not correspond to a gh command"},
-			expectedOutLines: []string{},
+			wantStderr: strings.Join(
+				[]string{
+					importFileMsg,
+					"X Could not import alias alias1: expansion does not correspond to a gh command",
+					"X Could not import alias alias2: expansion does not correspond to a gh command\n\n",
+				},
+				"\n",
+			),
 		},
 	}
 
 	for _, tt := range tests {
+		err := os.WriteFile(tmpFile, []byte(tt.fileContents), 0600)
+		if err != nil {
+			t.Fatalf("could not write test file %v", err)
+		}
+		require.NoError(t, err)
+
+		ios, stdin, _, stderr := iostreams.Test()
+		ios.SetStdinTTY(true)
+		ios.SetStdoutTTY(true)
+		ios.SetStderrTTY(true)
+		tt.opts.IO = ios
+
+		readConfigs := config.StubWriteConfig(t)
+		cfg := config.NewFromString(tt.initConfig)
+		tt.opts.Config = func() (config.Config, error) {
+			return cfg, nil
+		}
+
+		f := &cmdutil.Factory{
+			IOStreams: ios,
+			Config: func() (config.Config, error) {
+				return cfg, nil
+			},
+			ExtensionManager: &extensions.ExtensionManagerMock{
+				ListFunc: func() []extensions.Extension {
+					return []extensions.Extension{}
+				},
+			},
+		}
+
+		rootCmd := &cobra.Command{}
+		prCmd := &cobra.Command{Use: "pr"}
+		prCmd.AddCommand(&cobra.Command{Use: "checkout"})
+		prCmd.AddCommand(&cobra.Command{Use: "status"})
+		rootCmd.AddCommand(prCmd)
+		issueCmd := &cobra.Command{Use: "issue"}
+		issueCmd.AddCommand(&cobra.Command{Use: "list"})
+		rootCmd.AddCommand(issueCmd)
+		apiCmd := &cobra.Command{Use: "api"}
+		apiCmd.AddCommand(&cobra.Command{Use: "graphql"})
+		rootCmd.AddCommand(apiCmd)
+
+		tt.opts.validCommand = shared.ValidCommandFunc(f, rootCmd)
+
 		t.Run(tt.name, func(t *testing.T) {
-			cli := "-"
-			if tt.stdin == "" {
-				tmpFile := filepath.Join(t.TempDir(), "aliases")
-				err := os.WriteFile(tmpFile, []byte(tt.fileContents), 0600)
-				if err != nil {
-					t.Fatalf("could not write test file %v", err)
-				}
-				cli = tmpFile
-			}
-			cli = strings.Join(append(tt.flags, cli), " ")
+			stdin.WriteString(tt.stdin)
 
-			readConfigs := config.StubWriteConfig(t)
-
-			cfg := config.NewFromString(tt.initialConfig)
-
-			output, err := runCommand(cfg, tt.isTTY, cli, tt.stdin)
+			err := importRun(tt.opts)
 			require.NoError(t, err)
 
 			mainBuf := bytes.Buffer{}
 			readConfigs(&mainBuf, io.Discard)
 
-			//nolint:staticcheck // prefer exact matchers over ExpectLines
-			test.ExpectLines(t, output.Stderr(), tt.expectedErrLines...)
-			//nolint:staticcheck // prefer exact matchers over ExpectLines
-			test.ExpectLines(t, output.String(), tt.expectedOutLines...)
-
-			assert.Equal(t, tt.expectedConfig, mainBuf.String())
+			assert.Equal(t, tt.wantStderr, stderr.String())
+			assert.Equal(t, tt.wantConfig, mainBuf.String())
 		})
 	}
-}
-
-func TestAliasImport_no_filename(t *testing.T) {
-	cfg := config.NewFromString("")
-	_, err := runCommand(cfg, true, "", "")
-	require.EqualError(t, err, "no filename passed and nothing on STDIN")
-}
-
-func TestAliasImport_multiple_filenames(t *testing.T) {
-	cfg := config.NewFromString("")
-	_, err := runCommand(cfg, true, "aliases1 aliases2", "")
-	require.EqualError(t, err, "too many arguments")
-}
-
-func runCommand(cfg config.Config, isTTY bool, cli, in string) (*test.CmdOut, error) {
-	ios, stdin, stdout, stderr := iostreams.Test()
-	ios.SetStdoutTTY(isTTY)
-	ios.SetStdinTTY(isTTY)
-	ios.SetStderrTTY(isTTY)
-	stdin.WriteString(in)
-
-	factory := &cmdutil.Factory{
-		IOStreams: ios,
-		Config: func() (config.Config, error) {
-			return cfg, nil
-		},
-		ExtensionManager: &extensions.ExtensionManagerMock{
-			ListFunc: func() []extensions.Extension {
-				return []extensions.Extension{}
-			},
-		},
-	}
-
-	cmd := NewCmdImport(factory, nil)
-
-	// fake command nesting structure needed for validCommand
-	rootCmd := &cobra.Command{}
-	rootCmd.AddCommand(cmd)
-	prCmd := &cobra.Command{Use: "pr"}
-	prCmd.AddCommand(&cobra.Command{Use: "checkout"})
-	prCmd.AddCommand(&cobra.Command{Use: "status"})
-	rootCmd.AddCommand(prCmd)
-	issueCmd := &cobra.Command{Use: "issue"}
-	issueCmd.AddCommand(&cobra.Command{Use: "list"})
-	rootCmd.AddCommand(issueCmd)
-	apiCmd := &cobra.Command{Use: "api"}
-	apiCmd.AddCommand(&cobra.Command{Use: "graphql"})
-	rootCmd.AddCommand(apiCmd)
-
-	argv, err := shlex.Split("import " + cli)
-	if err != nil {
-		return nil, err
-	}
-	rootCmd.SetArgs(argv)
-	rootCmd.SetIn(stdin)
-	rootCmd.SetOut(io.Discard)
-	rootCmd.SetErr(io.Discard)
-
-	err = rootCmd.Execute()
-	return &test.CmdOut{
-		OutBuf: stdout,
-		ErrBuf: stderr,
-	}, err
 }
