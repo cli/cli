93e1511bae2923f617d99526281c6b8918a7c3ab
John Keech <jkeech@github.com>
2023-08-08 09:32:06 -0700

    Codespaces: Use the host name from the logged in server for commands (#7795)
    * Use the host name from the logged in server for codespace commands
    
    * Fix existing tests
    
    * Add tests for server url configuration
    
    * Rename defaultApiUrl to defaultAPIURL and comment cleanup
    
    * Switch to t.Setenv in codespaces api tests
    
    * Switch to t.Setenv in other tests
    
    * Support custom server in web flows for list and create
    
    * Rename GetServerURL() to ServerURL()

--- pkg/cmd/project/item-create/item_create_test.go
+++ pkg/cmd/project/item-create/item_create_test.go
@@ -1,7 +1,6 @@
 package itemcreate
 
 import (
-	"os"
 	"testing"
 
 	"github.com/cli/cli/v2/pkg/cmd/project/shared/queries"
@@ -73,8 +72,7 @@ func TestNewCmdCreateItem(t *testing.T) {
 		},
 	}
 
-	os.Setenv("GH_TOKEN", "auth-token")
-	defer os.Unsetenv("GH_TOKEN")
+	t.Setenv("GH_TOKEN", "auth-token")
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
--- pkg/cmd/project/item-edit/item_edit_test.go
+++ pkg/cmd/project/item-edit/item_edit_test.go
@@ -1,7 +1,6 @@
 package itemedit
 
 import (
-	"os"
 	"testing"
 
 	"github.com/cli/cli/v2/pkg/cmd/project/shared/queries"
@@ -105,8 +104,7 @@ func TestNewCmdeditItem(t *testing.T) {
 		},
 	}
 
-	os.Setenv("GH_TOKEN", "auth-token")
-	defer os.Unsetenv("GH_TOKEN")
+	t.Setenv("GH_TOKEN", "auth-token")
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
