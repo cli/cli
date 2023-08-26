45868eba2b8b66d90b736fbea26c115cda079d3e
Jun Nishimura <n.junjun0303@gmail.com>
2023-08-26 18:18:30 +0900

    add unit test

--- pkg/cmd/alias/delete/delete_test.go
+++ pkg/cmd/alias/delete/delete_test.go
@@ -86,3 +86,90 @@ func TestAliasDelete(t *testing.T) {
 		})
 	}
 }
+
+func TestDeleteRun(t *testing.T) {
+	tests := []struct {
+		name               string
+		config             string
+		isTTY              bool
+		opts               *DeleteOptions
+		wantWriteCallCount int
+		wantAliases        map[string]string
+		wantStdout         string
+		wantStderr         string
+		wantErrMsg         string
+	}{
+		{
+			name: "delete one alias",
+			config: heredoc.Doc(`
+				aliases:
+				  il: issue list
+				  co: pr checkout
+			`),
+			isTTY: true,
+			opts: &DeleteOptions{
+				Name: "co",
+				All:  false,
+			},
+			wantAliases: map[string]string{
+				"il": "issue list",
+			},
+			wantWriteCallCount: 1,
+			wantStdout:         "",
+			wantStderr:         "✓ Deleted alias co; was pr checkout\n",
+			wantErrMsg:         "",
+		},
+		{
+			name: "delete all aliases with all flag",
+			config: heredoc.Doc(`
+				aliases:
+				  il: issue list
+				  co: pr checkout
+			`),
+			isTTY: true,
+			opts: &DeleteOptions{
+				All: true,
+			},
+			wantWriteCallCount: 2,
+			wantAliases:        map[string]string{},
+			wantStdout:         "",
+			wantStderr:         "✓ Deleted alias il; was issue list\n✓ Deleted alias co; was pr checkout\n",
+			wantErrMsg:         "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ios, _, _, _ := iostreams.Test()
+			ios.SetStdinTTY(tt.isTTY)
+			ios.SetStdoutTTY(tt.isTTY)
+			ios.SetStderrTTY(tt.isTTY)
+			tt.opts.IO = ios
+
+			cfg := config.NewFromString(tt.config)
+			tt.opts.Config = func() (config.Config, error) {
+				return cfg, nil
+			}
+
+			err := deleteRun(tt.opts)
+			if tt.wantErrMsg != "" {
+				assert.EqualError(t, err, tt.wantErrMsg)
+				writeCalls := cfg.WriteCalls()
+				assert.Equal(t, 0, len(writeCalls))
+			} else {
+				assert.NoError(t, err)
+				writeCalls := cfg.WriteCalls()
+				assert.Equal(t, tt.wantWriteCallCount, len(writeCalls))
+			}
+
+			aliasCfg := cfg.Aliases()
+			aliases := aliasCfg.All()
+			for name, expansion := range aliases {
+				wantExpansion, ok := tt.wantAliases[name]
+				if !ok {
+					t.Errorf("expected not to have alias '%s', but it was registered", name)
+				}
+				assert.Equal(t, wantExpansion, expansion)
+			}
+		})
+	}
+}
