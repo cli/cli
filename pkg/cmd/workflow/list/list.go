330ad32c316e727fa46c7c425b0523ea24525de5
Raj Hawaldar <rajhawaldar.in@gmail.com>
2023-08-31 01:49:18 +0530

    Add JSON output flags to workflow list command (#7902)

--- pkg/cmd/workflow/list/list.go
+++ pkg/cmd/workflow/list/list.go
@@ -6,10 +6,10 @@ import (
 
 	"github.com/cli/cli/v2/api"
 	"github.com/cli/cli/v2/internal/ghrepo"
+	"github.com/cli/cli/v2/internal/tableprinter"
 	"github.com/cli/cli/v2/pkg/cmd/workflow/shared"
 	"github.com/cli/cli/v2/pkg/cmdutil"
 	"github.com/cli/cli/v2/pkg/iostreams"
-	"github.com/cli/cli/v2/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -19,11 +19,17 @@ type ListOptions struct {
 	IO         *iostreams.IOStreams
 	HttpClient func() (*http.Client, error)
 	BaseRepo   func() (ghrepo.Interface, error)
-
-	PlainOutput bool
+	Exporter   cmdutil.Exporter
 
 	All   bool
 	Limit int
+}
+
+var workflowFields = []string{
+	"id",
+	"name",
+	"path",
+	"state",
 }
 
 func NewCmdList(f *cmdutil.Factory, runF func(*ListOptions) error) *cobra.Command {
@@ -42,9 +48,6 @@ func NewCmdList(f *cmdutil.Factory, runF func(*ListOptions) error) *cobra.Comman
 			// support `-R, --repo` override
 			opts.BaseRepo = f.BaseRepo
 
-			terminal := opts.IO.IsStdoutTTY() && opts.IO.IsStdinTTY()
-			opts.PlainOutput = !terminal
-
 			if opts.Limit < 1 {
 				return cmdutil.FlagErrorf("invalid limit: %v", opts.Limit)
 			}
@@ -58,8 +61,8 @@ func NewCmdList(f *cmdutil.Factory, runF func(*ListOptions) error) *cobra.Comman
 	}
 
 	cmd.Flags().IntVarP(&opts.Limit, "limit", "L", defaultLimit, "Maximum number of workflows to fetch")
-	cmd.Flags().BoolVarP(&opts.All, "all", "a", false, "Also show disabled workflows")
-
+	cmd.Flags().BoolVarP(&opts.All, "all", "a", false, "Include disabled workflows")
+	cmdutil.AddJSONFlags(cmd, &opts.Exporter, workflowFields)
 	return cmd
 }
 
@@ -82,7 +85,18 @@ func listRun(opts *ListOptions) error {
 		return fmt.Errorf("could not get workflows: %w", err)
 	}
 
-	if len(workflows) == 0 {
+	var filteredWorkflows []shared.Workflow
+	if opts.All {
+		filteredWorkflows = workflows
+	} else {
+		for _, workflow := range workflows {
+			if !workflow.Disabled() {
+				filteredWorkflows = append(filteredWorkflows, workflow)
+			}
+		}
+	}
+
+	if len(filteredWorkflows) == 0 {
 		return cmdutil.NewNoResultsError("no workflows found")
 	}
 
@@ -92,17 +106,18 @@ func listRun(opts *ListOptions) error {
 		fmt.Fprintf(opts.IO.ErrOut, "failed to start pager: %v\n", err)
 	}
 
-	//nolint:staticcheck // SA1019: utils.NewTablePrinter is deprecated: use internal/tableprinter
-	tp := utils.NewTablePrinter(opts.IO)
-	cs := opts.IO.ColorScheme()
+	if opts.Exporter != nil {
+		return opts.Exporter.Write(opts.IO, filteredWorkflows)
+	}
 
-	for _, workflow := range workflows {
-		if workflow.Disabled() && !opts.All {
-			continue
-		}
-		tp.AddField(workflow.Name, nil, cs.Bold)
-		tp.AddField(string(workflow.State), nil, nil)
-		tp.AddField(fmt.Sprintf("%d", workflow.ID), nil, cs.Cyan)
+	cs := opts.IO.ColorScheme()
+	tp := tableprinter.New(opts.IO)
+	tp.HeaderRow("Name", "State", "ID")
+
+	for _, workflow := range filteredWorkflows {
+		tp.AddField(workflow.Name)
+		tp.AddField(string(workflow.State))
+		tp.AddField(fmt.Sprintf("%d", workflow.ID), tableprinter.WithColor(cs.Cyan))
 		tp.EndRow()
 	}
 
