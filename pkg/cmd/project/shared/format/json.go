7420f044807198931d58b6fe590831df1ab4e898
Sam Coe <samcoe@users.noreply.github.com>
2023-10-10 13:53:25 +0200

    Fake template field when exporting projects to json

--- pkg/cmd/project/shared/format/json.go
+++ pkg/cmd/project/shared/format/json.go
@@ -15,10 +15,13 @@ func JSONProject(project queries.Project) ([]byte, error) {
 		ShortDescription: project.ShortDescription,
 		Public:           project.Public,
 		Closed:           project.Closed,
-		Template:         project.Template,
-		Title:            project.Title,
-		ID:               project.ID,
-		Readme:           project.Readme,
+		// The Template field is hardcoded to be false due to https://github.com/cli/cli/issues/8103.
+		// The Template field does not exist on GHES 3.8 and older, once GHES 3.8 gets
+		// deprecated on 2024-03-07 we can start populating this field with actual data.
+		Template: false,
+		Title:    project.Title,
+		ID:       project.ID,
+		Readme:   project.Readme,
 		Items: struct {
 			TotalCount int `json:"totalCount"`
 		}{
@@ -50,10 +53,13 @@ func JSONProjects(projects []queries.Project, totalCount int) ([]byte, error) {
 			ShortDescription: p.ShortDescription,
 			Public:           p.Public,
 			Closed:           p.Closed,
-			Template:         p.Template,
-			Title:            p.Title,
-			ID:               p.ID,
-			Readme:           p.Readme,
+			// The Template field is hardcoded to be false due to https://github.com/cli/cli/issues/8103.
+			// The Template field does not exist on GHES 3.8 and older, once GHES 3.8 gets
+			// deprecated on 2024-03-07 we can start populating this field with actual data.
+			Template: false,
+			Title:    p.Title,
+			ID:       p.ID,
+			Readme:   p.Readme,
 			Items: struct {
 				TotalCount int `json:"totalCount"`
 			}{
--- pkg/cmd/project/shared/queries/queries.go
+++ pkg/cmd/project/shared/queries/queries.go
@@ -109,11 +109,21 @@ type Project struct {
 	ShortDescription string
 	Public           bool
 	Closed           bool
-	Template         bool
-	Title            string
-	ID               string
-	Readme           string
-	Items            struct {
+	// The Template field is commented out due to https://github.com/cli/cli/issues/8103.
+	// We released gh v2.34.0 without realizing the Template field does not exist
+	// on GHES 3.8 and older. This broke all project commands for users targeting GHES 3.8
+	// and older. In order to fix this we will no longer query the Template field until
+	// GHES 3.8 gets deprecated on 2024-03-07. This solution was simplier and quicker
+	// than adding a feature detection measure to every place this query is used.
+	// It does have the negative consequence that we have had to hardcode a false value for
+	// the Template field when outputing projects to JSON using the --format flag supported
+	// by a number of project commands. See `pkg/cmd/project/shared/format/json.go` for
+	// implementation.
+	// Template         bool
+	Title  string
+	ID     string
+	Readme string
+	Items  struct {
 		PageInfo   PageInfo
 		TotalCount int
 		Nodes      []ProjectItem
