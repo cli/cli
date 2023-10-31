b0ce9257287dad686ab64ec49398a3893e85c892
Sam Coe <samcoe@users.noreply.github.com>
2023-10-31 16:28:10 +0100

    Add auth token migration to multi account migration

--- internal/config/migration/multi_account_test.go
+++ internal/config/migration/multi_account_test.go
@@ -1,13 +1,14 @@
 package migration_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/cli/cli/v2/internal/config/migration"
+	"github.com/cli/cli/v2/internal/keyring"
 	"github.com/cli/cli/v2/pkg/httpmock"
 	"github.com/cli/go-gh/v2/pkg/config"
 	"github.com/stretchr/testify/require"
-	"github.com/zalando/go-keyring"
 )
 
 func TestMigration(t *testing.T) {
@@ -43,6 +44,60 @@ hosts:
 
 	requireKeyWithValue(t, cfg, []string{"hosts", "enterprise.com", "user"}, "user2")
 	requireKeyWithValue(t, cfg, []string{"hosts", "enterprise.com", "oauth_token"}, "yyyyyyyyyyyyyyyyyyyy")
+	requireKeyWithValue(t, cfg, []string{"hosts", "enterprise.com", "git_protocol"}, "https")
+}
+
+func TestMigrationSecureStorage(t *testing.T) {
+	cfg := config.ReadFromString(`
+hosts:
+  github.com:
+    user: userOne
+    git_protocol: ssh
+  enterprise.com:
+    user: userTwo
+    git_protocol: https
+`)
+
+	userOneToken := "userOne-token"
+	userTwoToken := "userTwo-token"
+
+	keyring.MockInit()
+	require.NoError(t, keyring.Set("gh:github.com", "", userOneToken))
+	require.NoError(t, keyring.Set("gh:enterprise.com", "", userTwoToken))
+
+	var m migration.MultiAccount
+	require.NoError(t, m.Do(cfg))
+
+	// Verify token gets stored with host and username
+	gotUserOneToken, err := keyring.Get("gh:github.com", "userOne")
+	require.NoError(t, err)
+	require.Equal(t, userOneToken, gotUserOneToken)
+
+	// Verify token still exists with only host
+	gotUserOneToken, err = keyring.Get("gh:github.com", "")
+	require.NoError(t, err)
+	require.Equal(t, userOneToken, gotUserOneToken)
+
+	// Verify token gets stored with host and username
+	gotUserTwoToken, err := keyring.Get("gh:enterprise.com", "userTwo")
+	require.NoError(t, err)
+	require.Equal(t, userTwoToken, gotUserTwoToken)
+
+	// Verify token still exists with only host
+	gotUserTwoToken, err = keyring.Get("gh:enterprise.com", "")
+	require.NoError(t, err)
+	require.Equal(t, userTwoToken, gotUserTwoToken)
+
+	// First we'll check that the data has been copied to the new structure
+	requireKeyWithValue(t, cfg, []string{"hosts", "github.com", "users", "userOne", "git_protocol"}, "ssh")
+
+	requireKeyWithValue(t, cfg, []string{"hosts", "enterprise.com", "users", "userTwo", "git_protocol"}, "https")
+
+	// Then we'll check that the old data has been left alone
+	requireKeyWithValue(t, cfg, []string{"hosts", "github.com", "user"}, "userOne")
+	requireKeyWithValue(t, cfg, []string{"hosts", "github.com", "git_protocol"}, "ssh")
+
+	requireKeyWithValue(t, cfg, []string{"hosts", "enterprise.com", "user"}, "userTwo")
 	requireKeyWithValue(t, cfg, []string{"hosts", "enterprise.com", "git_protocol"}, "https")
 }
 
@@ -91,8 +146,9 @@ func TestMigrationReturnsSuccessfullyWhenAnonymousUserExists(t *testing.T) {
 	// Simulates config that gets generated when a user logs
 	// in with a token and git protocol is not specified and
 	// secure storage is used.
+	token := "test-token"
 	keyring.MockInit()
-	require.NoError(t, keyring.Set("gh:github.com", "", "test-token"))
+	require.NoError(t, keyring.Set("gh:github.com", "", token))
 
 	cfg := config.ReadFromString(`
 hosts:
@@ -110,20 +166,31 @@ hosts:
 	m := migration.MultiAccount{Transport: reg}
 	require.NoError(t, m.Do(cfg))
 
-	require.Equal(t, "token test-token", reg.Requests[0].Header.Get("Authorization"))
+	require.Equal(t, fmt.Sprintf("token %s", token), reg.Requests[0].Header.Get("Authorization"))
 	requireKeyWithValue(t, cfg, []string{"hosts", "github.com", "user"}, "monalisa")
 	// monalisa key gets created with no value
 	users, err := cfg.Keys([]string{"hosts", "github.com", "users"})
 	require.NoError(t, err)
 	require.Equal(t, []string{"monalisa"}, users)
+
+	// Verify token gets stored with host and username
+	gotToken, err := keyring.Get("gh:github.com", "monalisa")
+	require.NoError(t, err)
+	require.Equal(t, token, gotToken)
+
+	// Verify token still exists with only host
+	gotToken, err = keyring.Get("gh:github.com", "")
+	require.NoError(t, err)
+	require.Equal(t, token, gotToken)
 }
 
 func TestMigrationReturnsSuccessfullyWhenAnonymousUserExistsAndGitProtocol(t *testing.T) {
 	// Simulates config that gets generated when a user logs
 	// in with a token and git protocol is specified and
 	// secure storage is used.
+	token := "test-token"
 	keyring.MockInit()
-	require.NoError(t, keyring.Set("gh:github.com", "", "test-token"))
+	require.NoError(t, keyring.Set("gh:github.com", "", token))
 
 	cfg := config.ReadFromString(`
 hosts:
@@ -142,9 +209,19 @@ hosts:
 	m := migration.MultiAccount{Transport: reg}
 	require.NoError(t, m.Do(cfg))
 
-	require.Equal(t, "token test-token", reg.Requests[0].Header.Get("Authorization"))
+	require.Equal(t, fmt.Sprintf("token %s", token), reg.Requests[0].Header.Get("Authorization"))
 	requireKeyWithValue(t, cfg, []string{"hosts", "github.com", "user"}, "monalisa")
 	requireKeyWithValue(t, cfg, []string{"hosts", "github.com", "users", "monalisa", "git_protocol"}, "ssh")
+
+	// Verify token gets stored with host and username
+	gotToken, err := keyring.Get("gh:github.com", "monalisa")
+	require.NoError(t, err)
+	require.Equal(t, token, gotToken)
+
+	// Verify token still exists with only host
+	gotToken, err = keyring.Get("gh:github.com", "")
+	require.NoError(t, err)
+	require.Equal(t, token, gotToken)
 }
 
 func TestMigrationReturnsSuccessfullyWhenAnonymousUserExistsAndInsecureStorage(t *testing.T) {
