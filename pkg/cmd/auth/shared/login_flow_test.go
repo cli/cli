2c72647cf774e782eb4ea88049f575074836a2d6
William Martin <williammartin@github.com>
2023-12-04 15:51:30 +0100

    Update git protocol login prompt to indicate it is host level

--- pkg/cmd/auth/shared/login_flow_test.go
+++ pkg/cmd/auth/shared/login_flow_test.go
@@ -52,7 +52,7 @@ func TestLogin_ssh(t *testing.T) {
 	pm := &prompter.PrompterMock{}
 	pm.SelectFunc = func(prompt, _ string, opts []string) (int, error) {
 		switch prompt {
-		case "What is your preferred protocol for Git operations?":
+		case "What is your preferred protocol for Git operations on this host?":
 			return prompter.IndexFor(opts, "SSH")
 		case "How would you like to authenticate GitHub CLI?":
 			return prompter.IndexFor(opts, "Paste an authentication token")
