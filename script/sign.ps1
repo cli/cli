441beb9de37b468e7d96187c1ae00908598471f8
Andy Feller <andyfeller@github.com>
2023-12-14 11:42:34 -0500

    Fix signtool invocation

--- script/sign.ps1
+++ script/sign.ps1
@@ -13,4 +13,4 @@ if ($null -eq $Env:METADATA_PATH) {
 $signtool = Resolve-Path "C:\Program Files (x86)\Windows Kits\10\bin\*\x64\signtool.exe" | Select-Object -Last 1
 Write-Host "Using signtool from $signtool"
 
-& $signtool sign /d "GitHub CLI" /fd sha256 /td sha256 /tr http://timestamp.acs.microsoft.com /v /dlib "$Env:DLIB_PATH" /dmdf "$Env:METADATA_PATH" "$args[0]"
+& $signtool sign /d "GitHub CLI" /fd sha256 /td sha256 /tr http://timestamp.acs.microsoft.com /v /dlib "$Env:DLIB_PATH" /dmdf "$Env:METADATA_PATH" $Args[0]
