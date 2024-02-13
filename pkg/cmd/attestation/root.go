2be3de6047351d62e2c51b2023c8151094260987
Meredith Lancaster <malancas@github.com>
2024-02-13 14:04:52 -0700

    update args passed to the attestation cmd
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- pkg/cmd/attestation/root.go
+++ pkg/cmd/attestation/root.go
@@ -3,25 +3,18 @@ package attestation
 import (
 	"github.com/github/gh-attestation/cmd/app"
 
-	"github.com/cli/cli/v2/pkg/cmdutil"
+	"github.com/cli/cli/v2/pkg/iostreams"
 	"github.com/spf13/cobra"
 )
 
-func NewCmdAttestation(f *cmdutil.Factory) *cobra.Command {
+func NewCmdAttestation(io *iostreams.IOStreams, version, buildDate string) *cobra.Command {
 	root := &cobra.Command{
 		Use:     "attestation",
 		Short:   "attestations",
-		// Aliases: []string{"cs"},
-		// GroupID: "core",
 	}
 
-	version := "test"
-	date := "test"
-	shortSHA := "test"
-
-	root.AddCommand(app.NewDownloadCmd(version, date))
-	root.AddCommand(app.NewVerifyCmd(version, date))
-	root.AddCommand(app.NewVersionCmd(version, date, shortSHA))
+	root.AddCommand(app.NewDownloadCmd(version, buildDate))
+	root.AddCommand(app.NewVerifyCmd(version, buildDate))
 	root.AddCommand(app.NewTUFRootVerifyCmd())
 
 	return root
