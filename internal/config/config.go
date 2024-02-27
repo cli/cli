5d72e17246db6dd4bfa4e106ae2db2c0a5dfc2c7
Bartek Pacia <barpac02@gmail.com>
2024-02-27 10:33:15 +0100

    Revert "make comments in the default config file more informative"
    This reverts commit ef96b447d7c68fba8a3076216a681594a6575670.

--- internal/config/config.go
+++ internal/config/config.go
@@ -519,11 +519,11 @@ const defaultConfigStr = `
 version: 1
 # What protocol to use when performing git operations. Supported values: ssh, https
 git_protocol: https
-# What editor gh should run when creating issues, pull requests, etc. If blank, will refer to environment variable $EDITOR.
+# What editor gh should run when creating issues, pull requests, etc. If blank, will refer to environment.
 editor:
 # When to interactively prompt. This is a global config that cannot be overridden by hostname. Supported values: enabled, disabled
 prompt: enabled
-# A pager program to send command output to, e.g. "less". If blank, will refer to environment variable $PAGER. Set to "cat" to disable the pager.
+# A pager program to send command output to, e.g. "less". Set the value to "cat" to disable the pager.
 pager:
 # Aliases allow you to create nicknames for gh commands
 aliases:
