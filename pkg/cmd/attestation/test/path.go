5172edd2b2277206149f09faafef05612ac10935
Meredith Lancaster <malancas@github.com>
2024-03-19 16:00:40 -0600

    escape backwards slash for windows files with replace
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- pkg/cmd/attestation/test/path.go
+++ pkg/cmd/attestation/test/path.go
@@ -1,13 +1,13 @@
 package test
 
 import (
-	"path/filepath"
 	"runtime"
+	"strings"
 )
 
 func NormalizeRelativePath(posixPath string) string {
 	if runtime.GOOS == "windows" {
-		return filepath.FromSlash(posixPath)
+		return strings.ReplaceAll(posixPath, "/", "\\")
 	}
 	return posixPath
 }
