9492b0380c27a30c2032d7b8f7573f995705e63b
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2024-03-19 23:52:00 +0900

    Annotation note, provide the reasons why this file is necessary

--- api/http_client_test_C0_sanitizer_go1_21.go
+++ api/http_client_test_C0_sanitizer_go1_21.go
@@ -15,10 +15,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// NOTE: This test is not working as expected. It is not able to escape the control characters. #8668
 func TestHTTPClientSanitizeJSONControlCharactersC0_go1_21(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		issue := Issue{
-			Title: "\u001B[31mRed Title\u001B[0m",
+			Title: "\u001B[31mGo 1.21 Escape Sequence Test\u001B[0m",
 			Body:  "1\u0001 2\u0002 3\u0003 4\u0004 5\u0005 6\u0006 7\u0007 8\u0008 9\t A\r\n B\u000b C\u000c D\r\n E\u000e F\u000f",
 			Author: Author{
 				ID:    "1",
@@ -45,7 +46,7 @@ func TestHTTPClientSanitizeJSONControlCharactersC0_go1_21(t *testing.T) {
 	var issue Issue
 	err = json.Unmarshal(body, &issue)
 	require.NoError(t, err)
-	assert.Equal(t, "^[[31mRed Title^[[0m", issue.Title)
+	assert.Equal(t, "^[[31mGo 1.21 Escape Sequence Test^[[0m", issue.Title)
 	assert.Equal(t, "1^A 2^B 3^C 4^D 5^E 6^F 7^G 8^H 9\t A\r\n B\v C^L D\r\n E^N F^O", issue.Body)
 	assert.Equal(t, "10^P 11^Q 12^R 13^S 14^T 15^U 16^V 17^W 18^X 19^Y 1A^Z 1B^[ 1C^\\ 1D^] 1E^^ 1F^_", issue.Author.Name)
 	assert.Equal(t, "monalisa \\u00^[", issue.Author.Login)
