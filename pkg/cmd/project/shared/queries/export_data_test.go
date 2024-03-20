b54f7a3bde50df3c31fdd68b638a0c0378a0ad58
yasunori <yasunori.kirin0418@gmail.com>
2024-03-20 20:58:51 +0900

    Include draft ID in project item list JSON (#8754)

--- pkg/cmd/project/shared/queries/export_data_test.go
+++ pkg/cmd/project/shared/queries/export_data_test.go
@@ -259,6 +259,7 @@ func TestJSONProjectDetailedItems(t *testing.T) {
 			Content: ProjectItemContent{
 				TypeName: "DraftIssue",
 				DraftIssue: DraftIssue{
+					ID:    "draftIssueId",
 					Title: "Pull Request title",
 					Body:  "a body",
 				},
@@ -270,7 +271,7 @@ func TestJSONProjectDetailedItems(t *testing.T) {
 	assert.NoError(t, err)
 	assert.JSONEq(
 		t,
-		`{"items":[{"content":{"type":"Issue","body":"a body","title":"Issue title","number":1,"repository":"cli/go-gh","url":"issue-url"},"id":"issueId"},{"content":{"type":"PullRequest","body":"a body","title":"Pull Request title","number":2,"repository":"cli/go-gh","url":"pr-url"},"id":"pullRequestId"},{"content":{"type":"DraftIssue","body":"a body","title":"Pull Request title"},"id":"draftIssueId"}],"totalCount":5}`,
+		`{"items":[{"content":{"type":"Issue","body":"a body","title":"Issue title","number":1,"repository":"cli/go-gh","url":"issue-url"},"id":"issueId"},{"content":{"type":"PullRequest","body":"a body","title":"Pull Request title","number":2,"repository":"cli/go-gh","url":"pr-url"},"id":"pullRequestId"},{"content":{"type":"DraftIssue","body":"a body","title":"Pull Request title","id":"draftIssueId"},"id":"draftIssueId"}],"totalCount":5}`,
 		string(out))
 }
 
@@ -300,6 +301,7 @@ func TestJSONProjectItem_DraftIssue_ProjectV2ItemFieldIterationValue(t *testing.
 		Content: ProjectItemContent{
 			TypeName: "DraftIssue",
 			DraftIssue: DraftIssue{
+				ID:    "draftIssueId",
 				Title: "Pull Request title",
 				Body:  "a body",
 			},
@@ -319,7 +321,7 @@ func TestJSONProjectItem_DraftIssue_ProjectV2ItemFieldIterationValue(t *testing.
 	assert.NoError(t, err)
 	assert.JSONEq(
 		t,
-		`{"items":[{"sprint":{"title":"Iteration Title","startDate":"","duration":0},"content":{"type":"DraftIssue","body":"a body","title":"Pull Request title"},"id":"draftIssueId"}],"totalCount":5}`,
+		`{"items":[{"sprint":{"title":"Iteration Title","startDate":"","duration":0},"content":{"type":"DraftIssue","body":"a body","title":"Pull Request title","id":"draftIssueId"},"id":"draftIssueId"}],"totalCount":5}`,
 		string(out))
 
 }
@@ -338,6 +340,7 @@ func TestJSONProjectItem_DraftIssue_ProjectV2ItemFieldMilestoneValue(t *testing.
 		Content: ProjectItemContent{
 			TypeName: "DraftIssue",
 			DraftIssue: DraftIssue{
+				ID:    "draftIssueId",
 				Title: "Pull Request title",
 				Body:  "a body",
 			},
@@ -357,7 +360,7 @@ func TestJSONProjectItem_DraftIssue_ProjectV2ItemFieldMilestoneValue(t *testing.
 	assert.NoError(t, err)
 	assert.JSONEq(
 		t,
-		`{"items":[{"milestone":{"title":"Milestone Title","dueOn":"","description":""},"content":{"type":"DraftIssue","body":"a body","title":"Pull Request title"},"id":"draftIssueId"}],"totalCount":5}`,
+		`{"items":[{"milestone":{"title":"Milestone Title","dueOn":"","description":""},"content":{"type":"DraftIssue","body":"a body","title":"Pull Request title","id":"draftIssueId"},"id":"draftIssueId"}],"totalCount":5}`,
 		string(out))
 
 }
