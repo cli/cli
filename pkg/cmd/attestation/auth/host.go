17b3d2a3748a3543921b74368ab9952b4edcdf06
Meredith Lancaster <malancas@github.com>
2024-04-04 08:21:27 -0600

    check for enterprise host
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- pkg/cmd/attestation/auth/host.go
+++ pkg/cmd/attestation/auth/host.go
@@ -2,16 +2,33 @@ package auth
 
 import (
 	"errors"
+	"strings"
 
 	"github.com/cli/go-gh/v2/pkg/auth"
 )
 
-var ErrUnsupportedHost = errors.New("The GH_HOST environment variable is set to a custom GitHub host. gh attestation does not currently support custom GitHub Enterprise hosts")
+var ErrGHESHost = errors.New("A GHES host was detected. gh attestation does not currently support GHES")
+
+const (
+	github                = "github.com"
+	localhost             = "github.localhost"
+	// tenancyHost is the domain name of a tenancy GitHub instance
+	tenancyHost = "ghe.com"
+)
+
+func isEnterprise(host string) bool {
+	return host != github && host != localhost && !isTenancy(host)
+}
+
+func isTenancy(host string) bool {
+	return strings.HasSuffix(host, "."+tenancyHost)
+}
 
 func IsHostSupported() error {
 	host, _ := auth.DefaultHost()
-	if host != "github.com" {
-		return ErrUnsupportedHost
+	
+	if isEnterprise(host) {
+		return ErrGHESHost
 	}
 	return nil
 }
