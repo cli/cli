7de10f0bdf55c679a86a645f31608c6f7e6480c3
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2024-04-10 19:20:20 +0100

    Add `UpdatePullRequestBranch` method
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- api/queries_pr.go
+++ api/queries_pr.go
@@ -607,6 +607,18 @@ func UpdatePullRequestReviews(client *Client, repo ghrepo.Interface, params gith
 	return err
 }
 
+func UpdatePullRequestBranch(client *Client, repo ghrepo.Interface, params githubv4.UpdatePullRequestBranchInput) error {
+	var mutation struct {
+		UpdatePullRequestBranch struct {
+			PullRequest struct {
+				ID string
+			}
+		} `graphql:"updatePullRequestBranch(input: $input)"`
+	}
+	variables := map[string]interface{}{"input": params}
+	return client.Mutate(repo.RepoHost(), "PullRequestUpdateBranch", &mutation, variables)
+}
+
 func isBlank(v interface{}) bool {
 	switch vv := v.(type) {
 	case string:
