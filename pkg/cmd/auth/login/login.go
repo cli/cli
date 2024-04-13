2c6343ad5660e0ffbb2217a794b6bff15487b614
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2024-04-13 15:47:44 +0100

    Explain `--skip-ssh-key` usage in long doc
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- pkg/cmd/auth/login/login.go
+++ pkg/cmd/auth/login/login.go
@@ -76,6 +76,10 @@ func NewCmdLogin(f *cmdutil.Factory, runF func(*LoginOptions) error) *cobra.Comm
 			The git protocol to use for git operations on this host can be set with %[1]s--git-protocol%[1]s,
 			or during the interactive prompting. Although login is for a single account on a host, setting
 			the git protocol will take effect for all users on the host.
+
+			Specifying %[1]sssh%[1]s for the git protocol will detect existing SSH keys to upload,
+			prompting to create and upload a new key if one is not found. This can be skipped with
+			%[1]s--skip-ssh-key%[1]s flag.
 		`, "`"),
 		Example: heredoc.Doc(`
 			# Start interactive setup
