1faeadad191e0d19982c7d772f48d05ccaad000c
Meredith Lancaster <malancas@github.com>
2024-04-22 13:04:39 -0600

    fetch attestation file
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- test/integration/attestation-cmd/verify-local-artifacts.sh
+++ test/integration/attestation-cmd/verify-local-artifacts.sh
@@ -8,8 +8,11 @@ ghBuildPath="$rootDir/bin/gh"
 
 sigstore02PackageFile="sigstore-2.2.0.tgz"
 sigstore02PackageURL="https://registry.npmjs.org/sigstore/-/$sigstore02PackageFile"
+sigstore02AttestationFile="sigstore-2.2.0.json"
+sigstore02AttestationURL="https://registry.npmjs.org/-/npm/v1/attestations/sigstore@2.2.0"
 
 curl -s "$sigstore02PackageURL" -o "$sigstore02PackageFile"
+curl -s "$sigstore02AttestationURL" | jq '.attestations[1].bundle' > "$sigstore02AttestationFile"
 
 sigstore03PackageFile="sigstore-3.0.0.tgz"
 sigstore03PackageURL="https://registry.npmjs.org/sigstore/-/$sigstore03PackageFile"
@@ -17,9 +20,9 @@ sigstore03PackageURL="https://registry.npmjs.org/sigstore/-/$sigstore03PackageFi
 curl -s "$sigstore03PackageURL" -o "$sigstore03PackageFile"
 
 # Verify the v0.2.0 sigstore bundle
-sigstoreBundle02="$rootDir/pkg/cmd/attestation/test/data/sigstore.js-2.2.0.bundle.json"
-echo "Testing with package $sigstore02PackageFile and attestation $sigstoreBundle02"
-if ! $ghBuildPath attestation verify "$sigstore02PackageFile" -b "$sigstoreBundle02" --digest-alg=sha512 --owner=sigstore; then
+# sigstoreBundle02="$rootDir/pkg/cmd/attestation/test/data/sigstore.js-2.2.0.bundle.json"
+#echo "Testing with package $sigstore02PackageFile and attestation $sigstoreBundle02"
+if ! $ghBuildPath attestation verify "$sigstore02PackageFile" -b "$sigstore02AttestationFile" --digest-alg=sha512 --owner=sigstore; then
     echo "Failed to verify package with a Sigstore v0.2.0 bundle"
     # cleanup test data
     rm "$sigstore02PackageFile"
