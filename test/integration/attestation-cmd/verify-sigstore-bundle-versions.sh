14cb87cefb66141898146562b961043d99f4b4e3
Meredith Lancaster <malancas@github.com>
2024-04-22 14:37:42 -0600

    rename the integration test file
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- test/integration/attestation-cmd/verify-sigstore-bundle-versions.sh
+++ test/integration/attestation-cmd/verify-sigstore-bundle-versions.sh
@@ -14,28 +14,11 @@ sigstore02AttestationURL="https://registry.npmjs.org/-/npm/v1/attestations/sigst
 curl -s "$sigstore02PackageURL" -o "$sigstore02PackageFile"
 curl -s "$sigstore02AttestationURL" | jq '.attestations[1].bundle' > "$sigstore02AttestationFile"
 
-sigstore03PackageFile="sigstore-3.0.0.tgz"
-sigstore03PackageURL="https://registry.npmjs.org/sigstore/-/$sigstore03PackageFile"
-sigstore03AttestationFile="sigstore-3.0.0.json"
-sigstore03AttestationURL="https://registry.npmjs.org/-/npm/v1/attestations/sigstore@3.0.0"
-
-curl -s "$sigstore03PackageURL" -o "$sigstore03PackageFile"
-curl -s "$sigstore03AttestationURL" | jq '.attestations[1].bundle' > "$sigstore03AttestationFile"
-
 # Verify the v0.2.0 sigstore bundle
 echo "Testing with package $sigstore02PackageFile and attestation $sigstore02AttestationFile"
 if ! $ghBuildPath attestation verify "$sigstore02PackageFile" -b "$sigstore02AttestationFile" --digest-alg=sha512 --owner=sigstore; then
     echo "Failed to verify package with a Sigstore v0.2.0 bundle"
     # cleanup test data
     rm "$sigstore02PackageFile" "$sigstore02AttestationFile"
-    exit 1
-fi
-
-# Verify the v0.3.0 sigstore bundle
-echo "Testing with package $sigstore03PackageFile and attestation $sigstore03AttestationFile"
-if ! $ghBuildPath attestation verify "$sigstore03PackageFile" -b "$sigstore03AttestationFile" --digest-alg=sha512 --owner=sigstore; then
-    echo "Failed to verify package with a Sigstore v0.3.0 bundle"
-    # cleanup test data
-    rm "$sigstore03PackageFile" "$sigstore03AttestationFile"
     exit 1
 fi
