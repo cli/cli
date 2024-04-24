3d339eb8eeec8ea9775b947da513f91162ff7a20
Meredith Lancaster <malancas@users.noreply.github.com>
2024-04-24 13:51:19 -0600

    Update pkg/cmd/attestation/verification/mock_verifier.go
    Co-authored-by: Phill MV <phillmv@github.com>

--- pkg/cmd/attestation/verification/mock_verifier.go
+++ pkg/cmd/attestation/verification/mock_verifier.go
@@ -31,7 +31,7 @@ func (v *MockSigstoreVerifier) Verify(attestations []*api.Attestation, policy ve
 			Signature: &verify.SignatureVerificationResult{
 				Certificate: &certificate.Summary{
 					Extensions: certificate.Extensions{
-						BuildSignerURI: "https://github.com/foo/bar/.github/workflows/release.yml@refs/heads/main",
+						BuildSignerURI: "https://github.com/github/example/.github/workflows/release.yml@refs/heads/main",
 					},
 				},
 			},
