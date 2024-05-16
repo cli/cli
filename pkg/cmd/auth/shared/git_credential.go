187c5016f0c5e508093b56d0f4da78b7a1404118
William Martin <williammartin@github.com>
2024-05-16 13:28:03 +0200

    Comment the git credential flow

--- pkg/cmd/auth/shared/git_credential.go
+++ pkg/cmd/auth/shared/git_credential.go
@@ -19,13 +19,20 @@ type GitCredentialFlow struct {
 }
 
 func (flow *GitCredentialFlow) Prompt(hostname string) error {
+	// First we'll fetch the credential helper that would be used for this host
 	var configuredHelperErr error
 	flow.helper, configuredHelperErr = flow.HelperConfig.ConfiguredHelper(hostname)
+	// If the helper is gh itself, then we don't need to ask the user if they want to update their git credentials
+	// because it will happen automatically by virtue of the fact that gh will return the active token.
+	//
+	// Since gh is the helper, this token may be used for git operations, so we'll additionally request the workflow
+	// scope to ensure that git push operations that include workflow changes succeed.
 	if flow.helper.IsOurs() {
 		flow.scopes = append(flow.scopes, "workflow")
 		return nil
 	}
 
+	// Prompt the user for whether they want to configure git with the newly obtained token
 	result, err := flow.Prompter.Confirm("Authenticate Git with your GitHub credentials?", true)
 	if err != nil {
 		return err
@@ -33,11 +40,24 @@ func (flow *GitCredentialFlow) Prompt(hostname string) error {
 	flow.shouldSetup = result
 
 	if flow.shouldSetup {
+		// If the user does want to configure git, we'll check the error returned from fetching the configured helper
+		// above. If the error indicates that git isn't installed, we'll return an error now to ensure that the auth
+		// flow is aborted before the user goes any further.
+		//
+		// Note that this is _slightly_ naive because there may be other reasons that fetching the configured helper
+		// fails that might cause later failures but this code has existed for a long time and I don't want to change
+		// it as part of a refactoring.
+		//
+		// Refs:
+		//  * https://git-scm.com/docs/git-config#_description
+		//  * https://github.com/cli/cli/pull/4109
 		var errNotInstalled *git.NotInstalled
 		if errors.As(err, &errNotInstalled) {
 			return configuredHelperErr
 		}
 
+		// On the other hand, if the user has requested setup we'll additionally request the workflow
+		// scope to ensure that git push operations that include workflow changes succeed.
 		flow.scopes = append(flow.scopes, "workflow")
 	}
 
