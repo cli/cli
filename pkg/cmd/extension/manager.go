deb3d57956e22c393491e73eacf7a105a6a46944
William Martin <williammartin@github.com>
2024-09-12 20:29:59 +0200

    Stub hasRosetta for tests

--- pkg/cmd/extension/manager.go
+++ pkg/cmd/extension/manager.go
@@ -771,11 +771,7 @@ func possibleDists() []string {
 	}
 }
 
-func hasRosetta() bool {
-	noRosetta := os.Getenv("GH_NO_ROSETTA")
-	if noRosetta != "" {
-		return false
-	}
+var hasRosetta = func() bool {
 	_, err := os.Stat("/Library/Apple/usr/libexec/oah/libRosettaRuntime")
 	return err == nil
 }
