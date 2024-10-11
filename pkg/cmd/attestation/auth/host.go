8109594c4ce6038ab500fb403cf8bd919d17fd95
Tyler McGoffin <jtmcg@github.com>
2024-10-11 15:21:52 -0700

    auth: Removed redundant ghauth.IsTenancy(host) check

--- pkg/cmd/attestation/auth/host.go
+++ pkg/cmd/attestation/auth/host.go
@@ -9,9 +9,7 @@ import (
 var ErrUnsupportedHost = errors.New("An unsupported host was detected. Note that gh attestation does not currently support GHES")
 
 func IsHostSupported(host string) error {
-	// Note that this check is slightly redundant as Tenancy should not be considered Enterprise
-	// but the ghinstance package has not been updated to reflect this yet.
-	if ghauth.IsEnterprise(host) && !ghauth.IsTenancy(host) {
+	if ghauth.IsEnterprise(host) {
 		return ErrUnsupportedHost
 	}
 	return nil
