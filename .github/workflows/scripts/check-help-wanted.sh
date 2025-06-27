8134fb377b4686e4f67aa79160c3b14721f7985d
Kynan Ware <47394200+BagToad@users.noreply.github.com>
2025-06-27 10:21:44 -0600

    Move help-wanted check for draft PRs into script

--- .github/workflows/scripts/check-help-wanted.sh
+++ .github/workflows/scripts/check-help-wanted.sh
@@ -15,6 +15,9 @@ fi
 # Skip if PR is from a bot or org member
 if [ "$PR_AUTHOR_TYPE" = "Bot" ] || [ "$PR_AUTHOR_ASSOCIATION" = "MEMBER" ] || [ "$PR_AUTHOR_ASSOCIATION" = "OWNER" ]; then
     echo "Skipping check for PR #$PR_URL as it is from a bot ($PR_AUTHOR_TYPE) or an org member ($PR_AUTHOR_ASSOCIATION: MEMBER/OWNER)"
+# Skip if PR is a draft
+if [ "$PR_IS_DRAFT" = "true" ]; then
+    echo "Skipping check for PR $PR_URL as it is a draft"
     exit 0
 fi
 
