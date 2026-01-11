8099b94b5e840efabe14856f8a5f70c3230fabad
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-01-11 10:03:20 +0900

    test: add a code comment to avoid confusion

--- pkg/cmd/attestation/artifact/artifact_posix_test.go
+++ pkg/cmd/attestation/artifact/artifact_posix_test.go
@@ -189,7 +189,7 @@ func TestNewDigestedArtifactWithDigestInput(t *testing.T) {
 	})
 
 	t.Run("sha512 digest input", func(t *testing.T) {
-		artifact, err := NewDigestedArtifact(&mockClient, "sha512:cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e", "sha256")
+		artifact, err := NewDigestedArtifact(&mockClient, "sha512:cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e", "sha256") // The argument `digestAlg` is ignored if `reference` is a digest reference.
 		require.NoError(t, err)
 		require.Equal(t, "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e", artifact.Digest())
 		require.Equal(t, "sha512", artifact.Algorithm())
