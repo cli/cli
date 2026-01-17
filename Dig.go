3ac4cd9bd9fc6dddcde09b47b6dce172c693a388
ตร.จังหวัดเลย <www.law.go.th@gmail.com>
2026-01-17 15:43:24 +0700

    Create Dig.go

---
+++ Dig.go
@@ -0,0 +1,232 @@
+{{Infobox
+| title = 
+| image = [[File:LGBTQ multicolored rainbow flag and Pride Month text (51908268822).jpg|200px]]
+| imagestyle = 
+| caption = WikiDiversity 2023
+| headerstyle = background:#ABCDEF
+| labelstyle = background:#ABCDEF
+| header1 = Event information
+| label2 = What
+| data2 = WikiDiversity 2023
+| label3 = Date
+| data3 = Campaign period 1 June 2023 – 30 June 2023 (UTC+7)
+| label4 = Location
+| data4 = Online
+| label5 = Host
+| data5 = Wikimedia Thailand
+}}
+
+'''Wikimedia Thailand/WikiDiversity''' เป็นกิจกรรมเกี่ยวกับ[[:w:en:gender equality|ความเท่าเทียมทางเพศ]] [[:w:en:Neurodiversity|ความหลากหลายทางระบบประสาท]] และ[[:w:en:Multiculturalism|พหุวัฒนธรรม]]ในโครงการวิกิพีเดียภาษาไทยและโครงการพี่น้องอื่น ๆ ซึ่งจะจัดขึ้นในวันที่ 1–30 มิถุนายน พ.ศ. 2566 เนื่องในเดือนแห่งความภาคภูมิใจที่เป็นการเฉลิมฉลองความภาคภูมิใจของชาว[[:w:th:เลสเบียน|เลสเบียน]] [[:w:th:เกย์|เกย์]] [[:w:th:ไบเซ็กชวล|ไบเซ็กชวล]] [[:w:th:คนข้ามเพศ|คนข้ามเพศ]] [[:w:th:เควียร์|เควียร์]] และรวมถึง[[:w:th:แอลจีบีที|ผู้ที่มีความหลากหลายทางเพศ]]อื่น ๆ ทั่วโลก
+
+กิจกรรมนี้จัดขึ้นเพื่อให้สมาชิกชุมชนได้มีโอกาสร่วมกิจกรรม สร้างสรรค์และร่วมแก้ไขพัฒนาบทความบนวิกิพีเดีย รวมถึงโครงการพี่น้องอื่น ๆ ที่เกี่ยวกับความเท่าเทียมทางเพศ ความหลากหลายทางระบบประสาท และพหุวัฒนธรรม โดยไม่ได้จำกัดเพียงหมวดหมู่ความหลากหลายทางเพศเท่านั้น เพื่อสนับสนุนการตระหนักรู้และเคารพในความแตกต่างต่อผู้อื่น
+
+== ธีม ==
+กิจกรรมนี้มีธีมหลักสองธีม ได้แก่ '''ความภาคภูมิใจในการเป็นตัวของตัวเอง''' และ '''การเสริมสร้างพลังด้วยความเห็นใจและความเท่าเทียม'''
+
+== วิธีการร่วมกิจกรรม ==
+ผู้ที่สนใจร่วมกิจกรรมสามารถแก้ไขในหมวดหมู่ที่เกี่ยวกับความเท่าเทียมทางเพศ ความหลากหลายทางระบบประสาท และพหุวัฒนธรรมบนวิกิพีเดีย วิกิตำรา วิกิซอร์ซ วิกิคำคม วิกิสนเทศ วิกิพจนานุกรม และวิกิมีเดียคอมมอนส์ ตามความถนัด โดยมีกติกาและการให้คะแนนดังต่อไปนี้
+
+=== กติกา ===
+# มีส่วนร่วมอย่างน้อย 2,000 ไบต์และไม่น้อยกว่า 300 คำ
+# การแก้ไขต้องอยู่ในช่วงวันที่ 1 - 30 มิถุนายน พ.ศ. 2566 เท่านั้น
+# บทความ คำศัพท์ ข้อมูล หรือภาพที่มีส่วนร่วมในการแก้ไขต้องเกี่ยวข้องกับความหลากหลายทางเพศ ความเท่าเทียมทางเพศ ความหลากหลายทางระบบประสาท และพหุวัฒนธรรม
+# บทความหรือไอเทมที่สร้าง ภาพที่อัปโหลด ต้องเป็นไปตามนโยบายของแต่ละโครงการ ตัวอย่างเช่น
+::* บทความที่สร้างบนวิกิพีเดียจะต้องถูกเขียนด้วยความเป็นกลาง มีแหล่งอ้างอิงที่เชื่อถือได้ ไม่มีป้ายระบุปัญหาของบทความ
+::* ภาพที่ลงบน วิกิมีเดีย คอมมอนส์ จะต้องเป็นผลงานของผู้เข้าร่วม ไม่ละเมิดลิขสิทธ์ของผู้อื่น
+:: หากทีมผู้จัดเห็นว่าบทความใดไม่ผ่านเกณฑ์ของแต่ละโครงการ บทความนั้นจะไม่ถูกนำมานับคะแนน
+:5. ผู้มีส่วนร่วมจะต้องนำชื่อบทความที่ตนแก้ไขใส่ลงในตารางที่กำหนดให้
+
+=== เกณฑ์การให้คะแนน ===
+การนับคะแนนจะกระทำโดยใช้เกณฑ์ดังต่อไปนี้
+{| class="wikitable"
+! style="text-align: center; background: Black; color:white; width:30%;"| โครงการ
+! style="text-align: center; background: Black; color:white; width:60%;"| ปริมาณที่แก้ไข
+! style="text-align: center; background: Black; color:white; width:10%;"| คะแนน
+|- style="background:#ecdbc3; color:black"
+| rowspan="3"; style="text-align: center | ''วิกิพีเดีย''<br>(1 บทความ)
+| น้อยกว่า 2,000 ไบต์
+| style="text-align: center | 5
+|- style="background:#ecdbc3; color:black"
+| 2,000 - 5,000 ไบต์ 
+| style="text-align: center | 8
+|- style="background:#ecdbc3; color:black"
+| มากกว่า 5,000 ไบต์
+| style="text-align: center | 10
+
+|- style="background:#f0e3d0; color:black"
+| rowspan="3"; style="text-align: center | ''วิกิตำรา''<br>(1 บทความ)
+| น้อยกว่า 2,000 ไบต์
+| style="text-align: center | 1
+|- style="background:#f0e3d0; color:black"
+| 2,000 - 5,000 ไบต์ 
+| style="text-align: center | 3
+|- style="background:#f0e3d0; color:black"
+| มากกว่า 5,000 ไบต์
+| style="text-align: center | 5
+
+|- style="background:#ecdbc3; color:black"
+| rowspan="3"; style="text-align: center | ''วิกิซอร์ซ''<br>(1 บทความ)
+| น้อยกว่า 2,000 ไบต์
+| style="text-align: center | 1
+|- style="background:#ecdbc3; color:black"
+| 2,000 - 5,000 ไบต์ 
+| style="text-align: center | 3
+|- style="background:#ecdbc3; color:black"
+| มากกว่า 5,000 ไบต์
+| style="text-align: center | 5
+
+|- style="background:#f0e3d0; color:black"
+| rowspan="3"; style="text-align: center | ''วิกิคำคม''<br>(1 บทความ)
+| 1 - 2 คำคม
+| style="text-align: center | 1
+|- style="background:#f0e3d0; color:black"
+| 3 - 4 คำคม
+| style="text-align: center | 3
+|- style="background:#f0e3d0; color:black"
+| 5 คำคมขึ้นไป
+| style="text-align: center | 5
+
+|- style="background:#ecdbc3; color:black"
+| style="text-align: center | ''วิกิพจนานุกรม''
+| 1 คำ
+| style="text-align: center | 3
+
+|- style="background:#f0e3d0; color:black"
+| style="text-align: center | ''วิกิสนเทศ''
+| 1 รายการ
+| style="text-align: center | 1
+
+|- style="background:#ecdbc3; color:black"
+| style="text-align: center | ''วิกิมีเดียคอมมอนส์''
+| 1 ภาพ
+| style="text-align: center | 2
+|}
+
+=== คะแนนพิเศษ ===
+# หากบทความหรือสื่อนั้น ๆ เกี่ยวข้องกับบุคคลชนชาติไทย บทความนั้นและสื่อนั้นจะมีคะแนนพิเศษอีก 5 คะแนน
+# หากภาพที่อัปโหลดถูกนำขึ้นบนบทความในโครงการใด ภาพนั้นจะมีคะแนนพิเศษเพิ่มโครงการละ 3 คะแนน
+# หากผู้เข้าร่วมเขียนบทความบนโครงการวิกิคำคมภาษาไทย วิกิตำราภาษาไทย วิกิพจนานุกรมภาษาไทย หรือวิกิสนเทศ และเชื่อมโยงหน้าเหล่านั้นไปที่วิกิพีเดียภาษาไทย บทความนั้นจะได้คะแนนพิเศษ 5 คะแนน
+#หากผู้ร่วมกิจกรรมนำแม่แบบด้านล่างไปใส่ในกล่องผู้ใช้ของหน้าผู้ใช้ตนเองบนวิกิพีเดียภาษาไทย ผู้เข้าร่วมกิจกรรมจะได้รับคะแนนพิเศษ 10 คะแนนต่อ 1 แม่แบบ
+#* [[:w:th:แม่แบบ:ผู้ใช้ สนับสนุนแอลจีบีที|แม่แบบสนับสนุนความหลากหลายทางเพศ]]
+#* [[:w:th:แม่แบบ:ผู้ใช้ สนับสนุนความหลากหลายทางระบบประสาท|แม่แบบสนับสนุนความหลากหลายทางระบบประสาท]]
+#* [[:w:th:แม่แบบ:ผู้ใช้ สนับสนุนความหลากหลายทางวัฒนธรรม|แม่แบบสนุบสนุนความหลากหลายทางวัฒนธรรม]]
+
+=== ข้อควรระวัง ===
+ผู้เข้าร่วมควรศึกษาวิธีการแก้ไขและมีส่วนร่วมอย่างถูกต้องในแต่ละโครงการก่อนแก้ไขในโครงการที่ไม่ถนัด ท่านสามารถกดลิงก์ด้านล่างเพื่อศึกษาวิธีการมีส่วนร่วมอย่างถูกต้อง
+* วิกิพีเดีย: [[:w:th:วิกิพีเดีย:คู่มือการเขียน|คู่มือการเขียน]] • [[:w:th:วิกิพีเดีย:นโยบายและแนวปฏิบัติ|นโยบายและแนวปฏิบัติ]]
+* วิกิตำรา: [[b:th:วิกิตำรา:วิกิตำราคืออะไร|วิกิตำราคืออะไร]] • [[:b:th:วิธีใช้:การแก้ไข|วิธีการแก้ไข]] • [[:b:th:วิกิตำรา:นโยบายและแนวปฏิบัติ|นโยบายและแนวปฏิบัติ]]
+* วิกิซอร์ซ: [[:s:th:วิกิซอร์ซ:วิกิซอร์ซคืออะไร|วิกิซอร์ซคืออะไร]] • [[:s:th:วิกิซอร์ซ:นโยบายและแนวปฏิบัติ|นโยบายและแนวปฏิบัติ]] • [[:s:th:วิกิซอร์ซ:นโยบายลิขสิทธิ์|นโยบายลิขสิทธิ์]]
+* วิกิคำคม: [[:q:th:วิกิคำคม:นโยบาย|นโยบาย]] • [[:q:th:วิกิคำคม:อะไรที่ไม่ใช่วิกิคำคม|อะไรที่ไม่ใช่วิกิคำคม]]
+* วิกิพจนานุกรม: [[:wikt:วิกิพจนานุกรม:นโยบายและแนวปฏิบัติ|นโยบายและแนวปฏิบัติ]] • [[:wikt:th:วิธีใช้:คู่มือในการเขียน|คู่มือในการเขียน]] • [[:wikt:ภาคผนวก:อภิธานศัพท์|อภิธานศัพท์]]
+* วิกิสนเทศ: [[:d:Wikidata:Introduction|บทนำ]] • [[:d:Wikidata:Contribute|วิธีการมีส่วนร่วม]] • [[:d:Help:Items|รายการสนเทศ]]
+* วิกิมีเดียคอมมอนส์: [[:c:Commons:Policies_and_guidelines|นโยบายและแนวปฏิบัติ]]
+หากผู้เข้าร่วมไม่ศึกษาก่อนเบื้องต้น อาจทำให้มีข้อผิดพลาดและการแจ้งลบบทความของผู้เข้าร่วม ดังนั้นการศึกษาข้อมูลโดยรวมของโครงการจะช่วยป้องกันการมีส่วนร่วมที่เสียเปล่าและการเสียกำลังใจที่จะเกิดขึ้นได้
+
+== การตัดสิน ==
+การนับคะแนนจะเริ่มหลังจากวันที่ 30 มิถุนายน พ.ศ. 2566 และจะมีการประกาศผู้ชนะภายใน 31 กรกฎาคม พ.ศ. 2566
+
+== รางวัล ==
+* โล่รางวัลสำหรับผู้ที่มีคะแนนสูงสูดสามอันดับแรก
+* ของที่ระลึก ได้แก่ เสื้อออกแบบพิเศษสำหรับกิจกรรม WikiDiversity ร่ม และกระติกน้ำ สำหรับ 30 อันดับแรก
+* ประกาศนียบัตรสำหรับผู้เข้าร่วมทุกท่าน
+
+== บทความแนะนำ ==
+=== วิกิพีเดีย ===
+หมวดหมู่ที่แนะนำจากวิกิพีเดียภาษาอังกฤษ 
+* [[w:Category:Gender equality|Gender equality]]
+* [[w:Category:Multiculturalism|Multiculturalism]]
+* [[w:Category:Neurodiversity|Neurodiversity]]
+* [[w:Outline_of_LGBT_topics|Outline of LGBT topics]]
+* [[w:Category:LGBT rights activists|LGBT rights activists]]
+* [[w:Category:Lists of LGBT-related people|Lists of LGBT-related people]]
+
+=== Wikisource ===
+ผู้เข้าร่วมสามารถดูแนวทางบทความที่สนใจได้ที่
+* [[s:Portal:LGBT]]
+* [[s:Category:LGBT|LGBT]]
+* [[s:Category:Culture|Culture]]
+
+=== Wikiquote ===
+ผู้เข้าร่วมสามารถดูแนวทางบทความที่สนใจได้ที่
+* [[q:Category:Gender_equality|Gender euality]]
+* [[q:Category:Culture|Culture]]
+* [[q:Category:Autism|Autism]]
+
+=== Wiktionary ===
+ผู้เข้าร่วมสามารถเพิ่มคำศัพท์ทางวิชาการที่เกี่ยวข้องกับกลุ่มคนที่มีความหลากหลายทางเพศ ความหลากหลายทางระบบประสาท และพหุวัฒนธรรม เช่น เพศ โรคทางระบบประสาท เป็นต้น
+
+== รายชื่อผู้เข้าร่วมกิจกรรม==
+ผู้เข้าร่วมสามารถกรอกข้อมูลได้ที่หน้า [[Wikimedia Thailand/WikiDiversity 2023/Participants]] 
+
+{{/Participants}}
+
+== สรุปผลกิจกรรม ==
+ในกิจกรรม WikiDiversity 2023 มีผู้เข้าร่วมกิจกรรมทั้งหมด 8 ท่าน ผู้ที่มีคะแนนสูงสุด 3 อันดับแรกได้แก่ [[user:วณิพก|วณิพก]] [[user:ร้อยตรี โชคดี|ร้อยตรี โชคดี]] และ [[user:Chainwit.|Chainwit.]] ผู้เข้าร่วมสามารถดูคะแนนของท่านได้ที่ตารางด่านล่างนี้ ทางผู้จัดกิจกรรมจะจัดทำของรางวัลและจัดส่งให้ผู้เข้าร่วมทุก ๆ ท่านเมื่อรางวัลและของที่ระลึกทำการผลิตเสร็จภายในสิงหาคมนี้
+
+{| class="wikitable"
+! style="text-align: center; background: Black; color:white; width:15%;"| อันดับ
+! style="text-align: center; background: Black; color:white; width:70%;"| ชื่อผู้ใช้
+! style="text-align: center; background: Black; color:white; width:15%;"| คะแนน
+|- style="background:#ecdbc3; color:black"
+| style="text-align: center"| 1
+| [[user:วณิพก]] 
+| style="text-align: center"| 284
+
+|- style="background:#f0e3d0; color:black"
+| style="text-align: center"| 2
+| [[user:ร้อยตรี โชคดี]]
+| style="text-align: center"| 156
+
+|-  style="background:#ecdbc3; color:black"
+| style="text-align: center"| 3
+| [[user:Chainwit.]]
+| style="text-align: center"| 100
+
+|- style="background:#f0e3d0; color:black"
+| style="text-align: center"| 4
+| [[user:Whitetissue]]
+| style="text-align: center"| 58
+
+|- style="background:#ecdbc3; color:black"
+| style="text-align: center"| 5
+| [[user:Thastp]]
+| style="text-align: center"| 50
+
+|- style="background:#f0e3d0; color:black"
+| style="text-align: center"| 6
+| [[user:PhakkaponP]]
+| style="text-align: center"| 30
+
+|- style="background:#ecdbc3; color:black"
+| style="text-align: center"| 7
+| [[user:Siam2019]]
+| style="text-align: center"| 29
+
+|- style="background:#f0e3d0; color:black"
+| style="text-align: center"| 8
+| [[user:OraMAAG]]
+| style="text-align: center"| 12
+|}
+
+จากการจัดกิจกรรม มีการสร้างหรือแก้ไขรวมทั้งหมด 948,384 ไบต์บน 5 โครงการด้วยกัน ได้แก่
+* วิกิพีเดีย จำนวน 57 บทความ
+* วิกิตำรา จำนวน 1 บทความ
+* วิกิพจนานุกรม จำนวน 5 คำ
+* วิกิสนเทศ จำนวน 1 รายการ
+* วิกิมีเดีย คอมมอนส์ จำนวน 1 ภาพ
+
+== ผู้สนับสนุน ==
+<table align="center" width="90%">
+<tr>
+<td align="center">[[File:Wikimedia Foundation logo - vertical.svg|100px|frameless]]<br>[http://www.wikimedia.org Wikimedia Foundation]</td>
+</tr>
+</table><br /><center>
+
+<div align="center">
+<big>ดำเนินการโดย </big><br>
+[[File:Wikimedia Thailand-Logo-BW.svg|150px]]<br>
+[https://www.facebook.com/WikimediaThailand Wikimedia Thailand Facebook]<br>
+
+ทีมผู้จัด / Organizing Team: [[w:User:Tanapatjms|Tanapatjms]] และ [[w:th:User:Eevnnap|Eevnnap]]<br>
+ที่ปรึกษา / Advisors: [[w:User:Azoma|Azoma]], [[w:User:B20180|B20180]] และ [[w:User:Taweetham|Taweetham]]<br>
+</div>
