28997470dcd9ab0a9c71402d8ce9fb40945fa0d7
Amol Yadav <amyssnipet@yahoo.com>
2026-02-01 12:22:27 +0530

    fixed: dynamic tab spaces and added args validator

--- pkg/cmd/dirs/dirs.go
+++ pkg/cmd/dirs/dirs.go
@@ -1,10 +1,9 @@
 package dirs
 
 import (
-	"fmt"
-
 	"github.com/MakeNowJust/heredoc"
 	"github.com/cli/cli/v2/internal/config"
+	"github.com/cli/cli/v2/internal/tableprinter"
 	"github.com/cli/cli/v2/pkg/cmdutil"
 	"github.com/spf13/cobra"
 )
@@ -21,11 +20,26 @@ func NewCmdDirs(f *cmdutil.Factory) *cobra.Command {
 			DATA DIR      Directory for storing downloaded data like extensions and copilot
 			STATE DIR     Directory for storing state files
 		`),
+		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Fprintf(f.IOStreams.Out, "CONFIG DIR    %s    Directory for storing configuration files\n", config.ConfigDir())
-			fmt.Fprintf(f.IOStreams.Out, "DATA DIR      %s    Directory for storing downloaded data like extensions and copilot\n", config.DataDir())
-			fmt.Fprintf(f.IOStreams.Out, "STATE DIR     %s    Directory for storing state files\n", config.StateDir())
-			return nil
+			tp := tableprinter.New(f.IOStreams, tableprinter.WithHeader("NAME", "PATH", "DESCRIPTION"))
+			
+			tp.AddField("CONFIG DIR", tableprinter.WithColor(f.IOStreams.ColorScheme().Bold))
+			tp.AddField(config.ConfigDir())
+			tp.AddField("Directory for storing configuration files")
+			tp.EndRow()
+			
+			tp.AddField("DATA DIR", tableprinter.WithColor(f.IOStreams.ColorScheme().Bold))
+			tp.AddField(config.DataDir())
+			tp.AddField("Directory for storing downloaded data like extensions and copilot")
+			tp.EndRow()
+			
+			tp.AddField("STATE DIR", tableprinter.WithColor(f.IOStreams.ColorScheme().Bold))
+			tp.AddField(config.StateDir())
+			tp.AddField("Directory for storing state files")
+			tp.EndRow()
+			
+			return tp.Render()
 		},
 	}
 
