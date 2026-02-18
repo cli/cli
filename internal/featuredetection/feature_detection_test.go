0953cf9247adff88614fb52c3a5835bc6d450557
William Martin <williammartin@github.com>
2026-02-18 16:17:25 +0100

    add missing reg.Verify

--- internal/featuredetection/feature_detection_test.go
+++ internal/featuredetection/feature_detection_test.go
@@ -655,6 +655,7 @@ func TestProjectFeatures(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			reg := &httpmock.Registry{}
+			defer reg.Verify(t)
 			httpClient := &http.Client{}
 			httpmock.ReplaceTripper(httpClient, reg)
 			for query, resp := range tt.queryResponse {
