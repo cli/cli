ab0acc6a540a3d8c67c044150f00ad87c799a814
Rylen Anil <rylen.anil@gmail.com>
2026-04-12 08:35:06 +0000

    fix: correctly filter private repo visibility
    Fixes cli/cli#12900

--- pkg/cmd/repo/list/http_test.go
+++ pkg/cmd/repo/list/http_test.go
@@ -60,6 +60,54 @@ func Test_listReposWithLanguage(t *testing.T) {
 	assert.Equal(t, `sort:updated-desc fork:true language:go user:@me`, searchData.Variables["query"])
 }
 
+func Test_listReposWithPrivateVisibilityUsesSearch(t *testing.T) {
+	reg := httpmock.Registry{}
+	defer reg.Verify(t)
+
+	var searchData struct {
+		Query     string
+		Variables map[string]interface{}
+	}
+	reg.Register(
+		httpmock.GraphQL(`query RepositoryListSearch\b`),
+		func(req *http.Request) (*http.Response, error) {
+			jsonData, err := io.ReadAll(req.Body)
+			if err != nil {
+				return nil, err
+			}
+			err = json.Unmarshal(jsonData, &searchData)
+			if err != nil {
+				return nil, err
+			}
+
+			respBody, err := os.Open("./fixtures/repoSearch.json")
+			if err != nil {
+				return nil, err
+			}
+
+			return &http.Response{
+				StatusCode: 200,
+				Request:    req,
+				Body:       respBody,
+			}, nil
+		},
+	)
+
+	client := http.Client{Transport: &reg}
+	res, err := listRepos(&client, "github.com", 10, "", FilterOptions{
+		Visibility: "private",
+	})
+	require.NoError(t, err)
+
+	assert.Equal(t, 3, res.TotalCount)
+	assert.True(t, res.FromSearch)
+	assert.Equal(t, "octocat", res.Owner)
+	assert.Equal(t, "octocat/hello-world", res.Repositories[0].NameWithOwner)
+
+	assert.Equal(t, float64(10), searchData.Variables["perPage"])
+	assert.Equal(t, `sort:updated-desc fork:true is:private user:@me`, searchData.Variables["query"])
+}
+
 func Test_searchQuery(t *testing.T) {
 	type args struct {
 		owner  string
