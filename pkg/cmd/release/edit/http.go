0580da935fecb876e27c453d3a537f776ffa1100
William Martin <williammartin@github.com>
2026-09-02 16:07:26 +0200

    Remove unnecessary 204 on release edit

--- pkg/cmd/release/edit/http.go
+++ pkg/cmd/release/edit/http.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"strconv"
 
@@ -26,26 +25,15 @@ func editRelease(httpClient *http.Client, repo ghrepo.Interface, releaseID int64
 		return nil, err
 	}
 
-	// Request rather than REST because REST returns without decoding on a 204, which would
-	// yield a zero valued release instead of the error this site reports today.
+	var newRelease shared.Release
 	// TODO(api-client-rollout)
 	// This line of code is part of a mechanical roll out of the api client.
 	// As a follow up, consider whether the api client can be injected to this call site, rather than constructed
-	resp, err := api.NewClientFromHTTP(httpClient).Request(repo.RepoHost(), http.MethodPatch, path.String(), bytes.NewBuffer(bodyBytes),
-		api.WithHeader("Content-Type", "application/json; charset=utf-8"))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	b, err := io.ReadAll(resp.Body)
-	if err != nil {
+	if err := api.NewClientFromHTTP(httpClient).REST(repo.RepoHost(), http.MethodPatch, path.String(), bytes.NewBuffer(bodyBytes), &newRelease); err != nil {
 		return nil, err
 	}
 
-	var newRelease shared.Release
-	err = json.Unmarshal(b, &newRelease)
-	return &newRelease, err
+	return &newRelease, nil
 }
 
 func remoteTagExists(httpClient *http.Client, repo ghrepo.Interface, tagName string) (bool, error) {
